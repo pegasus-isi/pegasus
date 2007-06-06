2b0d74fad9113721ebc63aa96668c35338a8f155
Gaurang Mehta <gmehta@isi.edu>
2007-06-06 09:10:57 +0000

    fixed VDS_HOME to PEGASUS_HOME

--- contrib/graph-build/build.sh
+++ contrib/graph-build/build.sh
@@ -2,4 +2,4 @@
 #
 # Constructs the most common use case
 #
-perl build.pl $VDS_HOME/build.xml > build.dot && dot -Tpng build.dot > build.png
+perl build.pl $PEGASUS_HOME/build.xml > build.dot && dot -Tpng build.dot > build.png
