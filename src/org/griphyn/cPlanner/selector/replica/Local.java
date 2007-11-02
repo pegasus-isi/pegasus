697bc1bfd9603f94a0235d016c5b53d6f0550d37
Karan Vahi <vahi@isi.edu>
2007-11-02 20:22:30 +0000

    Added a replica selector and a transfer implementation to facilitate use of native condor file transfer mechanism from submit host to remote site.
    Details can be found at
    http://vtcpc.isi.edu/bugzilla/show_bug.cgi?id=15

---
+++ src/org/griphyn/cPlanner/selector/replica/Local.java
@@ -0,0 +1,213 @@
+/**
+ * This file or a portion of this file is licensed under the terms of
+ * the Globus Toolkit Public License, found in file GTPL, or at
+ * http://www.globus.org/toolkit/download/license.html. This notice must
+ * appear in redistributions of this file, with or without modification.
+ *
+ * Redistributions of this Software, with or without modification, must
+ * reproduce the GTPL in: (1) the Software, or (2) the Documentation or
+ * some other similar material which is provided with the Software (if
+ * any).
+ *
+ * Copyright 1999-2004 University of Chicago and The University of
+ * Southern California. All rights reserved.
+ */
+package org.griphyn.cPlanner.selector.replica;
+
+import org.griphyn.cPlanner.classes.ReplicaLocation;
+
+import org.griphyn.cPlanner.selector.ReplicaSelector;
+
+import org.griphyn.cPlanner.common.LogManager;
+import org.griphyn.cPlanner.common.PegasusProperties;
+import org.griphyn.cPlanner.common.PegRandom;
+
+import org.griphyn.common.catalog.ReplicaCatalogEntry;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Vector;
+
+/**
+ * This replica selector only prefers replicas from the local host and that
+ * start with a file: URL scheme. It is useful, when you want to stagin
+ * files to a remote site from your submit host using the Condor file transfer
+ * mechanism.
+ *
+ * <p>
+ * In order to use the replica selector implemented by this class,
+ * <pre>
+ *        - the property pegasus.selector.replica must be set to value Local
+ * </pre>
+ *
+ *
+ * @see org.griphyn.cPlanner.transfer.implementation.Condor
+ *
+ * @author Karan Vahi
+ * @version $Revision$
+ */
+public class Local implements ReplicaSelector {
+
+    /**
+     * A short description of the replica selector.
+     */
+    private static String mDescription = "Local from submit host";
+
+    /**
+     * The scheme name for file url.
+     */
+    protected static final String FILE_URL_SCHEME = "file:";
+
+
+    /**
+     * The handle to the logging object that is used to log the various debug
+     * messages.
+     */
+    protected LogManager mLogger;
+
+    /**
+     * The properties object containing the properties passed to the planner.
+     */
+    protected PegasusProperties mProps;
+
+    /**
+     * The overloaded constructor, that is called by load method.
+     *
+     * @param properties the <code>PegasusProperties</code> object containing all
+     *                   the properties required by Pegasus.
+     *
+     *
+     */
+    public Local( PegasusProperties properties ){
+        mProps       = properties;
+        mLogger      = LogManager.getInstance();
+    }
+
+
+    /**
+     * Selects a random replica from all the replica's that have their
+     * site handle set to local and the pfn's start with a file url scheme.
+     *
+     * @param rl         the <code>ReplicaLocation</code> object containing all
+     *                   the pfn's associated with that LFN.
+     * @param preferredSite the preffered site for picking up the replicas.
+     *
+     * @return <code>ReplicaCatalogEntry</code> corresponding to the location selected.
+     *
+     * @see org.griphyn.cPlanner.classes.ReplicaLocation
+     */
+    public ReplicaCatalogEntry selectReplica( ReplicaLocation rl,
+                                              String preferredSite ){
+
+        ReplicaCatalogEntry rce;
+        ArrayList prefPFNs = new ArrayList();
+        int locSelected;
+        String site = null;
+
+//        mLogger.log("Selecting a pfn for lfn " + lfn + "\n amongst" + locations ,
+//                    LogManager.DEBUG_MESSAGE_LEVEL);
+
+        for ( Iterator it = rl.pfnIterator(); it.hasNext(); ) {
+            rce = ( ReplicaCatalogEntry ) it.next();
+            site = rce.getResourceHandle();
+
+            if( site == null ){
+                //skip to next replica
+                continue;
+            }
+
+            //check if has pool attribute as local, and at same time
+            //start with a file url scheme
+            if( site.equals( "local" ) && rce.getPFN().startsWith( FILE_URL_SCHEME ) ){
+                prefPFNs.add( rce );
+            }
+        }
+
+        if ( prefPFNs.isEmpty() ) {
+            //select a random location from
+            //all the matching locations
+            //in all likelihood all the urls were file urls and none
+            //were associated with the preference pool.
+            throw new RuntimeException( "Unable to select any location on local site from " +
+                                        "the list passed for lfn "  + rl.getLFN() );
+
+        } else {
+            //select a random location
+            //amongst the locations
+            //on the preference pool
+            int length = prefPFNs.size();
+            //System.out.println("No of locations found at pool " + prefPool + " are " + length);
+            locSelected = PegRandom.getInteger( length - 1 );
+            rce = ( ReplicaCatalogEntry ) prefPFNs.get( locSelected );
+
+        }
+
+        return rce;
+
+    }
+
+    /**
+     * This chooses a location amongst all the locations returned by the
+     * Replica Mechanism. If a location is found with re/pool attribute same
+     * as the preference pool, it is taken. This returns all the locations which
+     * match to the preference pool. This function is called to determine if a
+     * file does exist on the output pool or not beforehand. We need all the
+     * location to ensure that we are able to make a match if it so exists.
+     * Else a random location is selected and returned
+     *
+     * @param rl         the <code>ReplicaLocation</code> object containing all
+     *                   the pfn's associated with that LFN.
+     * @param preferredSite the preffered site for picking up the replicas.
+     *
+     * @return <code>ReplicaLocation</code> corresponding to the replicas selected.
+     *
+     * @see org.griphyn.cPlanner.classes.ReplicaLocation
+     */
+    public ReplicaLocation selectReplicas( ReplicaLocation rl,
+                                           String preferredSite ){
+
+        String lfn = rl.getLFN();
+        ReplicaLocation result = new ReplicaLocation();
+        result.setLFN( rl.getLFN() );
+
+        ReplicaCatalogEntry rce;
+        String site;
+        String ucAttrib;
+        int noOfLocs = 0;
+
+        for ( Iterator it = rl.pfnIterator(); it.hasNext(); ) {
+            noOfLocs++;
+            rce = ( ReplicaCatalogEntry ) it.next();
+            site = rce.getResourceHandle();
+
+            if ( site != null && site.equals( preferredSite )) {
+                result.addPFN( rce );
+            }
+            else if ( site == null ){
+                mLogger.log(
+                    " pool attribute not specified for the location objects" +
+                    " in the Replica Catalog", LogManager.WARNING_MESSAGE_LEVEL);
+            }
+        }
+
+        if ( result.getPFNCount() == 0 ) {
+            //means we have to choose a random location between 0 and (noOfLocs -1)
+            int locSelected = PegRandom.getInteger( noOfLocs - 1 );
+            rce = ( ReplicaCatalogEntry ) rl.getPFN(locSelected );
+            result.addPFN( rce );
+        }
+        return result;
+
+    }
+
+    /**
+     * Returns a short description of the replica selector.
+     *
+     * @return string corresponding to the description.
+     */
+    public String description(){
+        return mDescription;
+    }
+
+
+}
---
+++ src/org/griphyn/cPlanner/transfer/implementation/Condor.java
@@ -0,0 +1,374 @@
+/*
+ * This file or a portion of this file is licensed under the terms of
+ * the Globus Toolkit Public License, found in file GTPL, or at
+ * http://www.globus.org/toolkit/download/license.html. This notice must
+ * appear in redistributions of this file, with or without modification.
+ *
+ * Redistributions of this Software, with or without modification, must
+ * reproduce the GTPL in: (1) the Software, or (2) the Documentation or
+ * some other similar material which is provided with the Software (if
+ * any).
+ *
+ * Copyright 1999-2004 University of Chicago and The University of
+ * Southern California. All rights reserved.
+ */
+package org.griphyn.cPlanner.transfer.implementation;
+
+import org.griphyn.cPlanner.classes.TransferJob;
+import org.griphyn.cPlanner.classes.PlannerOptions;
+import org.griphyn.cPlanner.classes.SubInfo;
+import org.griphyn.cPlanner.classes.JobManager;
+import org.griphyn.cPlanner.classes.SiteInfo;
+import org.griphyn.cPlanner.classes.FileTransfer;
+import org.griphyn.cPlanner.classes.NameValue;
+
+import org.griphyn.cPlanner.common.PegasusProperties;
+import org.griphyn.cPlanner.common.LogManager;
+
+import org.griphyn.common.util.Separator;
+
+import org.griphyn.common.catalog.TransformationCatalogEntry;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+
+import java.io.FileWriter;
+
+import java.net.URL;
+
+/**
+ * A prototype implementation that leverages the Condor file transfer mechanism
+ * to do the transfer to the remote directory . Currently, this will only
+ * work for staging in data to a remote site from the submit host.
+ * <p>
+ * Additionally, this will only work with local replica selector that prefers
+ * file urls from the submit host for staging.
+ *
+ * <p>
+ * In order to use the transfer implementation implemented by this class,
+ * <pre>
+ *     - property <code>pegasus.transfer.stagein.impl</code> must be set to
+ *       value <code>Condor</code>.
+ *     - property <code>pegasus.selector.replica</code> must be set to value
+ *       <code>Local</code>
+ * </pre>
+ *
+ * @author Karan Vahi
+ * @version $Revision$
+ */
+public class Condor extends AbstractMultipleFTPerXFERJob {
+
+    /**
+     * The transformation namespace for the transfer job.
+     */
+    public static final String TRANSFORMATION_NAMESPACE = "pegasus";
+
+    /**
+     * The name of the underlying transformation that is queried for in the
+     * Transformation Catalog.
+     */
+    public static final String TRANSFORMATION_NAME = "true";
+
+    /**
+     * The version number for the transfer job.
+     */
+    public static final String TRANSFORMATION_VERSION = null;
+
+    /**
+     * The derivation namespace for for the transfer job.
+     */
+    public static final String DERIVATION_NAMESPACE = "pegasus";
+
+    /**
+     * The name of the underlying derivation.
+     */
+    public static final String DERIVATION_NAME = "true";
+
+    /**
+     * The derivation version number for the transfer job.
+     */
+    public static final String DERIVATION_VERSION = "1.0";
+
+    /**
+     * A short description of the transfer implementation.
+     */
+    public static final String DESCRIPTION = "Condor File Transfer Mechanism";
+
+
+    /**
+     * The overloaded constructor, that is called by the Factory to load the
+     * class.
+     *
+     * @param properties  the properties object.
+     * @param options     the options passed to the Planner.
+     */
+    public Condor( PegasusProperties properties,
+                   PlannerOptions options ) {
+        super( properties, options );
+    }
+
+
+    /**
+     * Returns a boolean indicating whether the transfer protocol being used
+     * by the implementation preserves the X Bit or not while staging.
+     *
+     * @return false
+     */
+    public boolean doesPreserveXBit() {
+        return false;
+    }
+
+    /**
+     * It constructs the arguments to the transfer executable that need to be
+     * passed to the executable referred to in this transfer mode.
+     *
+     * @param job the object containing the transfer node.
+     * @return the argument string
+     */
+    protected String generateArgumentString( TransferJob job ) {
+        return "";
+    }
+
+    /**
+     * Returns the complete name for the transformation.
+     *
+     * @return the complete name.
+     */
+    protected String getCompleteTCName(){
+        return Separator.combine( this.TRANSFORMATION_NAMESPACE,
+                                  this.TRANSFORMATION_NAME,
+                                  this.TRANSFORMATION_VERSION );
+    }
+
+
+    /**
+     * Returns the logical name of the derivation that this implementation
+     * refers to.
+     *
+     * @return the name of the derivation.
+     */
+    protected String getDerivationName() {
+        return this.DERIVATION_NAME;
+    }
+
+    /**
+     * Returns the namespace of the derivation that this implementation
+     * refers to.
+     *
+     * @return the namespace of the derivation.
+     */
+    protected String getDerivationNamespace() {
+        return this.DERIVATION_NAMESPACE;
+    }
+
+    /**
+     * Returns the version of the derivation that this implementation refers
+     * to.
+     *
+     * @return the version of the derivation.
+     */
+    protected String getDerivationVersion() {
+        return this.DERIVATION_VERSION;
+    }
+
+    /**
+     * Returns a textual description of the transfer implementation.
+     *
+     * @return a short textual description
+     */
+    public String getDescription() {
+        return Condor.DESCRIPTION;
+    }
+
+    /**
+     * Returns the environment profiles that are required for the default
+     * entry to sensibly work. There are no variables to be returned for
+     * this case.
+     *
+     * @param site the site where the job is going to run.
+     * @return an empty list
+     */
+    protected List getEnvironmentVariables( String site ) {
+        return new ArrayList( 0 );
+    }
+
+    /**
+     * Constructs a  condor file transfer job that handles multiple transfers.
+     * The job itself is a /bin/true job that currently only manages to
+     * transfer input files from the local host.
+     *
+     * @param job         the SubInfo object for the job, in relation to which
+     *                    the transfer node is being added. Either the transfer
+     *                    node can be transferring this jobs input files to
+     *                    the execution pool, or transferring this job's output
+     *                    files to the output pool.
+     * @param files       collection of <code>FileTransfer</code> objects
+     *                    representing the data files and staged executables to be
+     *                    transferred.
+     * @param execFiles   subset collection of the files parameter, that identifies
+     *                    the executable files that are being transferred.
+     * @param txJobName   the name of transfer node.
+     * @param jobClass    the job Class for the newly added job. Can be one of the
+     *                    following:
+     *                              stage-in
+     *                              stage-out
+     *                              inter-pool transfer
+     *
+     * @return  the created TransferJob.
+     */
+    public TransferJob createTransferJob( SubInfo job,
+                                          Collection files,
+                                          Collection execFiles,
+                                          String txJobName,
+                                          int jobClass ) {
+
+
+        //sanity check
+        if( jobClass != SubInfo.STAGE_IN_JOB ){
+            throw new RuntimeException( "Condor file transfer can only be used for stagein" );
+        }
+
+        TransferJob txJob = new TransferJob();
+
+        //run job always on the site where the compute job runs
+        txJob.setSiteHandle( job.getSiteHandle() );
+
+        //the non third party site for the transfer job is
+        //always the job execution site for which the transfer
+        //job is being created.
+        txJob.setNonThirdPartySite(job.getSiteHandle());
+
+        txJob.setName( txJobName );
+        txJob.setUniverse( "globus" );
+
+        txJob.setTransformation( this.TRANSFORMATION_NAMESPACE,
+                                 this.TRANSFORMATION_NAME,
+                                 this.TRANSFORMATION_VERSION );
+
+        txJob.setDerivation( this.DERIVATION_NAMESPACE,
+                             this.DERIVATION_NAMESPACE,
+                             this.DERIVATION_VERSION );
+
+        txJob.setRemoteExecutable( "/bin/true" );
+
+
+        //add input files for transfer since we are only doing for
+        //creating stagein jobs
+        for( Iterator it = files.iterator(); it.hasNext(); ){
+            FileTransfer ft = ( FileTransfer )it.next();
+            NameValue nv = ft.getSourceURL(  );
+            //sanity check first
+            if( !nv.getKey().equals( "local" ) ){
+                throw new RuntimeException( "Condor File transfer can only do stagein from local site. " +
+                                            "Unable to transfer " + ft );
+            }
+            //put the url in only if it is a file url
+            String url = nv.getValue();
+            if( url.startsWith( "file:/" ) ){
+                try{
+                    txJob.condorVariables.addIPFileForTransfer(new URL(url).
+                        getPath());
+                }
+                catch( Exception e ){
+                    throw new RuntimeException ( "Malformed source URL " + url );
+                }
+            }
+        }
+
+        //this should in fact only be set
+        // for non third party pools
+        //we first check if there entry for transfer universe,
+        //if no then go for globus
+        SiteInfo ePool = mSCHandle.getTXPoolEntry( txJob.getSiteHandle() );
+        JobManager jobmanager = ePool.selectJobManager( this.TRANSFER_UNIVERSE, true );
+        txJob.setJobManager( ( jobmanager == null) ?
+                               null :
+                               jobmanager.getInfo( JobManager.URL ) );
+
+        txJob.setJobType( jobClass );
+        txJob.setVDSSuperNode( job.jobName );
+
+        txJob.stdErr = "";
+        txJob.stdOut = "";
+
+        //the i/p and o/p files remain empty
+        //as we doing just copying urls
+        txJob.inputFiles = new HashSet();
+
+        //to get the file stat information we need to put
+        //the files as output files of the transfer job
+        txJob.outputFiles = new HashSet( files );
+
+
+        //the profile information from the pool catalog needs to be
+        //assimilated into the job.
+        txJob.updateProfiles( mSCHandle.getPoolProfile( txJob.getSiteHandle() ) );
+
+        //the profile information from the transformation
+        //catalog needs to be assimilated into the job
+        //overriding the one from pool catalog.
+ //       txJob.updateProfiles(tcEntry);
+
+        //the profile information from the properties file
+        //is assimilated overidding the one from transformation
+        //catalog.
+        txJob.updateProfiles( mProps );
+
+        //apply the priority to the transfer job
+        this.applyPriority( txJob );
+
+        //constructing the arguments to transfer script
+        //they only have to be incorporated after the
+        //profile incorporation
+        txJob.strargs = this.generateArgumentString(txJob);
+
+        if(execFiles != null){
+            //we need to add setup jobs to change the XBit
+            super.addSetXBitJobs( job, txJob, execFiles );
+        }
+
+
+        return txJob;
+    }
+
+
+    /**
+     * Retrieves the transformation catalog entry for the executable that is
+     * being used to transfer the files in the implementation.
+     *
+     * @param siteHandle the handle of the site where the transformation is
+     *   to be searched.
+     * @return the transformation catalog entry if found, else null.
+     */
+    public TransformationCatalogEntry getTransformationCatalogEntry( String siteHandle ) {
+        return null;
+    }
+
+    /**
+     * Return a boolean indicating whether the transfers to be done always in
+     * a third party transfer mode. Fix me. should say NEVER.
+     *
+     * @return boolean indicating whether to always use third party
+     *   transfers or not.
+     *
+     */
+    public boolean useThirdPartyTransferAlways() {
+        return false;
+    }
+
+    /**
+     * Writes to a FileWriter stream the stdin which goes into the magic
+     * script via standard input
+     *
+     * @param stdIn the writer to the stdin file.
+     * @param files Collection of <code>FileTransfer</code> objects
+     *   containing the information about sourceam fin and destURL's.
+     * @throws Exception
+     */
+    protected void writeJumboStdIn( FileWriter stdIn, Collection files ) throws
+        Exception {
+    }
+}
