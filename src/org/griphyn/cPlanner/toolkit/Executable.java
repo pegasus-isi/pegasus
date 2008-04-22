2f1779758af22453bccdaad866d19f8032a0843f
Karan Vahi <vahi@isi.edu>
2008-04-22 22:56:37 +0000

    Removed the stack trace print statement

--- src/org/griphyn/cPlanner/toolkit/Executable.java
+++ src/org/griphyn/cPlanner/toolkit/Executable.java
@@ -92,7 +92,6 @@ public abstract class Executable {
             //append just one elment of stack trace for each exception
             message.append( " at " ).append( cause.getStackTrace()[0] );
         }
-        e.printStackTrace();
         return message.toString();
     }
 
