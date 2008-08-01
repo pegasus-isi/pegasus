3792e74ed0f1dae45ecd765c6243b608457ecd4c
Atul Kumar <akumar@isi.edu>
2008-08-01 20:42:50 +0000

    VORS sc catalog implementation.

--- src/edu/isi/pegasus/planner/client/PegasusGetSites.java
+++ src/edu/isi/pegasus/planner/client/PegasusGetSites.java
@@ -1,3 +1,20 @@
+/*
+ * 
+ *   Copyright 2007-2008 University Of Southern California
+ * 
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ *   Unless required by applicable law or agreed to in writing,
+ *   software distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ * 
+ */
 package edu.isi.pegasus.planner.client;
 
 
@@ -8,9 +25,6 @@ import edu.isi.pegasus.planner.catalog.site.SiteFactory;
 import edu.isi.pegasus.planner.catalog.site.SiteFactoryException;
 import edu.isi.pegasus.planner.catalog.site.classes.SiteCatalogEntry;
 import edu.isi.pegasus.planner.catalog.site.classes.SiteStore;
-import edu.isi.pegasus.planner.catalog.site.classes.VORSSiteCatalogUtil;
-import edu.isi.pegasus.planner.catalog.site.classes.VORSSiteInfo;
-import edu.isi.pegasus.planner.catalog.site.classes.VORSVOInfo;
 import gnu.getopt.Getopt;
 import gnu.getopt.LongOpt;
 import java.io.File;
@@ -93,9 +107,9 @@ public class PegasusGetSites extends Executable{
     @Override
     public void printLongVersion() {
              StringBuffer text = new StringBuffer();
-        text.append( "\n" ).append( " $Id: PegasusGetSites.java 594 2008-07-18 18:50:50Z vahi $ ").
+        text.append( "\n" ).append( " $Id: PegasusGetSites.java 594 2008-08-1 18:50:50Z akumar $ ").
              append( "\n" ).append( getGVDSVersion() ).
-             append( "\n" ).append( "Usage : pegasus-get-sites -s <site> -g <grid> -o <vo> " ).
+             append( "\n" ).append( "Usage : pegasus-get-sites -source <site> -grid <grid> -vo <vo> -sc <filename> " ).
              append( "\n" ).append( " [-v] [-h]" );
 
        System.out.println( text.toString() );
@@ -104,11 +118,11 @@ public class PegasusGetSites extends Executable{
     @Override
     public void printShortVersion() {
             StringBuffer text = new StringBuffer();
-        text.append( "\n" ).append( " $Id: PegasusGetSites.java 594 2008-07-18 18:50:50Z vahi $ ").
+        text.append( "\n" ).append( " $Id: PegasusGetSites.java 594 2008-08-1 18:50:50Z akumar $ ").
              append( "\n" ).append( getGVDSVersion() ).
-             append( "\n" ).append( "Usage : pegasus-get-sites -s <site> -g <grid> -o <vo> " ).
+             append( "\n" ).append( "Usage : pegasus-get-sites -source <site> -grid <grid> -vo <vo> -sc <filename> " ).
              append( "\n" ).append( " [-v] [-h]" );
-
+        
        System.out.println( text.toString() );
     }
 
