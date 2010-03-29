4eae9750d437a5ede4cde0199e8290ff43cc5e3e
Karan Vahi <vahi@isi.edu>
2010-03-29 23:09:19 +0000

    Test for moving of packages using netbeans refactoring.

--- src/edu/isi/pegasus/planner/logsummary/JobStateParserFileFilter.java
+++ src/edu/isi/pegasus/planner/logsummary/JobStateParserFileFilter.java
@@ -13,7 +13,7 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
-package org.griphyn.logsummary;
+package edu.isi.pegasus.planner.logsummary;
 
 import java.io.File;
 import java.io.FilenameFilter;
--- src/edu/isi/pegasus/planner/logsummary/JobstateCallback.java
+++ src/edu/isi/pegasus/planner/logsummary/JobstateCallback.java
@@ -13,7 +13,7 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
-package org.griphyn.logsummary;
+package edu.isi.pegasus.planner.logsummary;
 
 import edu.isi.pegasus.common.logging.LogManagerFactory;
 import java.io.BufferedReader;
