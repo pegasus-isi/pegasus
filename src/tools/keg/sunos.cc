6c2f51d3b056ad3afdda6ccda84c4cbeecac0a3c
Jens Vöckler <voeckler@isi.edu>
2010-11-01 18:55:50 +0000

    OK, all three systems now with same kind of output. Still need to improve on the struct utsname output...

--- src/tools/keg/sunos.cc
+++ src/tools/keg/sunos.cc
@@ -13,6 +13,7 @@
  * Southern California. All rights reserved.
  */
 #include "sunos.hh"
+#include "sunos-swap.hh"
 
 static const char* RCS_ID =
   "$Id$";
@@ -34,6 +35,7 @@ static const char* RCS_ID =
 #include <sys/param.h> /* FSCALE */
 #include <kstat.h>
 
+#include <sys/loadavg.h>
 
 void
 pegasus_statfs( char* buffer, size_t capacity )
@@ -66,6 +68,65 @@ pegasus_statfs( char* buffer, size_t capacity )
       }
     }
     fclose(vfstab); 
+  }
+}
+
+static int load_flag = -2;
+static double load_avg[3]; 
+
+void
+pegasus_loadavg( char* buffer, size_t capacity )
+{
+  if ( load_flag == -2 ) {
+    load_flag = getloadavg( load_avg, 3 );
+  }
+
+  if ( load_flag >= 0 ) { 
+    char line[128];
+    snprintf( line, sizeof(line), 
+	      "Load Averages  : %.3f %.3f %.3f\n", 
+	      load_avg[0], load_avg[1], load_avg[2] ); 
+    strncat( buffer, line, capacity );     
+  }
+}
+
+static int swap_flag = -1; 
+static unsigned long long swap_total;
+static unsigned long long swap_free;
+static unsigned long long ram_total;
+static unsigned long long ram_free; 
+
+void
+pegasus_meminfo( char* buffer, size_t capacity )
+{
+  if ( ram_total == 0 ) {
+    unsigned long long pagesize = getpagesize(); 
+    long tmp; 
+    if ( (tmp=sysconf(_SC_PHYS_PAGES)) != -1 )
+      ram_total = (tmp * pagesize);
+    if ( (tmp=sysconf(_SC_AVPHYS_PAGES)) != -1 )
+      ram_free = (tmp * pagesize);
+  }
+
+  if ( ram_total > 0 ) {
+    char line[128];
+
+    snprintf( line, sizeof(line), 
+	      "Memory Usage MB: %lu total, %lu free\n",
+	      megs(ram_total), megs(ram_free) ); 
+    strncat( buffer, line, capacity ); 
+  }
+
+  if ( swap_flag == -1 ) {
+    swap_flag = gather_sunos_swap( &swap_total, &swap_free ); 
+  }
+
+  if ( swap_flag > 0 ) {
+    char line[128];
+    snprintf( line, sizeof(line),
+	      "Swap Usage   MB: %lu total, %lu free\n",
+	      megs(swap_total), megs(swap_free) );
+    strncat( buffer, line, capacity ); 
   }
 }
 
@@ -104,11 +165,6 @@ assign( void* dst, size_t size, kstat_named_t* knp )
 }
 
 struct SunOSMachineInfo {
-  uint64_t swap_total;
-  uint64_t swap_free;
-  uint64_t ram_total;
-  uint64_t ram_free; 
-
   unsigned short cpu_count;
   unsigned short cpu_online; 
 
@@ -116,33 +172,85 @@ struct SunOSMachineInfo {
   char          brand_id[20];
   char          cpu_type[20];
   char          model_name[20];
-
-  double  load[3]; 
 }; 
 
 static struct SunOSMachineInfo info; 
-static int init_flag = 1; 
+static int init_flag = -1; 
 
 static
-void
-initMachine()
+int
+initMachine( struct SunOSMachineInfo* p )
 {
-  uint64_t pagesize = getpagesize();
+  unsigned long long pagesize = getpagesize();
   kstat_ctl_t* kc; 
-  
-}
+  int result = -1; 
 
-void
-pegasus_loadavg( char* buffer, size_t capacity )
-{
-}
+  memset( p, 0, sizeof(struct SunOSMachineInfo) ); 
+  /* access kernel statistics API
+   * run /usr/sbin/kstat -p to see most of the things available.
+   */
+  if ( (kc = kstat_open()) != NULL ) {
+    kstat_t*       ksp;
+    size_t         j;
 
-void
-pegasus_meminfo( char* buffer, size_t capacity )
-{
+    // iterate over kernel statistics chain, module by module
+    for ( ksp = kc->kc_chain; ksp != NULL; ksp = ksp->ks_next ) {
+      if ( strcmp( ksp->ks_module, "cpu_info" ) == 0 ) {
+        kstat_read( kc, ksp, NULL );
+        /*
+         * module == "cpu_info"
+         */
+        p->cpu_count++;
+        for ( j=0; j < ksp->ks_ndata; ++j ) {
+          kstat_named_t* knp = ((kstat_named_t*) ksp->ks_data) + j;
+
+          if ( strcmp( knp->name, "state" ) == 0 ) {
+            if ( knp->data_type == KSTAT_DATA_CHAR &&
+                 strcmp( knp->value.c, "on-line") == 0 ||
+                 knp->data_type == KSTAT_DATA_STRING &&
+                 strcmp( KSTAT_NAMED_STR_PTR(knp), "on-line" ) == 0 )
+              p->cpu_online++;
+          } else if ( strcmp( knp->name, "clock_MHz" ) == 0 ) {
+            assign( &p->megahertz, sizeof(p->megahertz), knp );
+          } else if ( strcmp( knp->name, "brand" ) == 0 ) {
+            strncpy( p->brand_id,
+                     ( knp->data_type == KSTAT_DATA_STRING ?
+                       KSTAT_NAMED_STR_PTR(knp) : knp->value.c ),
+                     sizeof(p->brand_id) );
+          } else if ( strcmp( knp->name, "cpu_type" ) == 0 ) {
+            strncpy( p->cpu_type,
+                     ( knp->data_type == KSTAT_DATA_STRING ?
+                       KSTAT_NAMED_STR_PTR(knp) : knp->value.c ),
+                     sizeof(p->cpu_type) );
+          } else if ( strcmp( knp->name, "implementation" ) == 0 ) {
+            strncpy( p->model_name,
+                     ( knp->data_type == KSTAT_DATA_STRING ?
+                       KSTAT_NAMED_STR_PTR(knp) : knp->value.c ),
+                     sizeof(p->model_name) );
+          }
+        } /* for j */
+      } /* module == "unix" */
+    } /* for */
+
+    kstat_close(kc);
+    result = 0; 
+  }
+
+  return result; 
 }
 
 void
 pegasus_cpuinfo( char* buffer, size_t capacity )
 {
+  if ( init_flag == -1 )
+    init_flag = initMachine( &info ); 
+
+  if ( init_flag != -1 ) { 
+    char line[1024]; 
+    snprintf( line, sizeof(line), 
+	      "Processor Info.: %d x %s [%s] @ %ld\n",
+	      info.cpu_online, info.model_name, info.cpu_type, 
+	      info.megahertz ); 
+    strncat( buffer, line, capacity ); 
+  }
 }
