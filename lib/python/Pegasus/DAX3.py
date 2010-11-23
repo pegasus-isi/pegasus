6b00884064dd841cb11c3af6eb116293fdb83a63
Gideon Juve <gideon@isi.edu>
2010-11-23 22:02:28 +0000

    Improve documentation

--- lib/python/Pegasus/DAX3.py
+++ lib/python/Pegasus/DAX3.py
@@ -18,12 +18,92 @@ The classes in this module can be used to generate DAXes that can be
 read by Pegasus.
 
 The official DAX schema is here: http://pegasus.isi.edu/schema/dax-3.2.xsd
+
+Here is an example showing how to create the diamond DAX using this API:
+
+# Create ADAG object
+diamond = ADAG("diamond")
+	
+# Add input file to the DAX-level replica catalog
+a = File("f.a")
+a.addPFN(PFN("gsiftp://site.com/inputs/f.a","site"))
+diamond.addFile(a)
+	
+# Add executables to the DAX-level replica catalog
+e_preprocess = Executable(namespace="diamond", name="preprocess", version="4.0", os="linux", arch="x86_64")
+e_preprocess.addPFN(PFN("gsiftp://site.com/bin/preprocess","site"))
+diamond.addExecutable(e_preprocess)
+	
+e_findrange = Executable(namespace="diamond", name="findrange", version="4.0", os="linux", arch="x86_64")
+e_findrange.addPFN(PFN("gsiftp://site.com/bin/findrange","site"))
+diamond.addExecutable(e_findrange)
+	
+e_analyze = Executable(namespace="diamond", name="analyze", version="4.0", os="linux", arch="x86_64")
+e_analyze.addPFN(PFN("gsiftp://site.com/bin/analyze","site"))
+diamond.addExecutable(e_analyze)
+	
+# Add transformations to the DAX-level transformation catalog
+t_preprocess = Transformation(e_preprocess)
+diamond.addTransformation(t_preprocess)
+	
+t_findrange = Transformation(e_findrange)
+diamond.addTransformation(t_findrange)
+	
+t_analyze = Transformation(e_analyze)
+diamond.addTransformation(t_analyze)
+
+# Add a preprocess job
+preprocess = Job(t_preprocess)
+b1 = File("f.b1")
+b2 = File("f.b2")
+preprocess.addArguments("-a preprocess","-T60","-i",a,"-o",b1,b2)
+preprocess.uses(a, link=Link.INPUT)
+preprocess.uses(b1, link=Link.OUTPUT, transfer=True)
+preprocess.uses(b2, link=Link.OUTPUT, transfer=True)
+diamond.addJob(preprocess)
+
+# Add left Findrange job
+frl = Job(t_findrange)
+c1 = File("f.c1")
+frl.addArguments("-a findrange","-T60","-i",b1,"-o",c1)
+frl.uses(b1, link=Link.INPUT)
+frl.uses(c1, link=Link.OUTPUT, transfer=True)
+diamond.addJob(frl)
+
+# Add right Findrange job
+frr = Job(t_findrange)
+c2 = File("f.c2")
+frr.addArguments("-a findrange","-T60","-i",b2,"-o",c2)
+frr.uses(b2, link=Link.INPUT)
+frr.uses(c2, link=Link.OUTPUT, transfer=True)
+diamond.addJob(frr)
+
+# Add Analyze job
+analyze = Job(t_analyze)
+d = File("f.d")
+analyze.addArguments("-a analyze","-T60","-i",c1,c2,"-o",d)
+analyze.uses(c1, link=Link.INPUT)
+analyze.uses(c2, link=Link.INPUT)
+analyze.uses(d, link=Link.OUTPUT, transfer=True, register=True)
+diamond.addJob(analyze)
+
+# Add control-flow dependencies
+diamond.addDependency(parent=preprocess, child=frl)
+diamond.addDependency(parent=preprocess, child=frr)
+diamond.addDependency(parent=frl, child=analyze)
+diamond.addDependency(parent=frr, child=analyze)
+
+# Write the DAX to stdout
+import sys
+diamond.writeXML(sys.stdout)
+
+# Write the DAX to a file
+f = open("diamond.dax","w")
+diamond.writeXML(f)
+f.close()
 """
 
 __author__ = "Gideon Juve <juve@usc.edu>"
-__all__ = ["ADAG","DAX","DAG","Namespace","Arch","Link","When",
-		   "Transfer","OS","File","Executable","Metadata","PFN",
-		   "Profile","Transformation","Job","parse","parseString"]
 __version__ = "3.2"
 
 import datetime, pwd, os, sys
