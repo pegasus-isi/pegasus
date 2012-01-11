f9b728f858f55eb26e0c900586c96d4acb737bc0
Karan Vahi <vahi@isi.edu>
2012-01-11 01:29:08 +0000

    PM-502
    The Abstract class was not checked in.

---
+++ src/edu/isi/pegasus/common/credential/impl/Abstract.java
@@ -0,0 +1,76 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package edu.isi.pegasus.common.credential.impl;
+
+import edu.isi.pegasus.common.credential.Credential;
+
+import edu.isi.pegasus.common.logging.LogManager;
+
+import edu.isi.pegasus.planner.catalog.site.classes.SiteStore;
+
+import edu.isi.pegasus.planner.classes.PegasusBag;
+
+import edu.isi.pegasus.planner.common.PegasusProperties;
+
+
+/**
+ * An abstract base class to be used by other Credential implementations.
+ *
+ * @author Karan Vahi
+ * @version $Revision$
+ */
+public abstract class Abstract implements Credential {
+
+
+    /**
+     * The object holding all the properties pertaining to Pegasus.
+     */
+    protected PegasusProperties mProps;
+
+    /**
+     * The handle to the Site Catalog Store.
+     */
+    protected SiteStore mSiteStore;
+
+
+    /**
+     * A handle to the logging object.
+     */
+    protected LogManager mLogger;
+
+    /**
+     * The default constructor.
+     */
+    public Abstract() {
+
+    }
+
+
+
+    /**
+     * Initializes the credential implementation. Implementations require
+     * access to the logger, properties and the SiteCatalog Store.
+     *
+     * @param bag  the bag of Pegasus objects.
+     */
+    public void initialize( PegasusBag bag ){
+        mProps = bag.getPegasusProperties();
+        mSiteStore = bag.getHandleToSiteStore();
+        mLogger = bag.getLogger();
+
+    }
+
+    /**
+     * Returns the path to the credential on the submit host.
+     *
+     * @return
+     */
+    public String getPath(){
+        return this.getPath( "local" );
+    }
+
+
+}
