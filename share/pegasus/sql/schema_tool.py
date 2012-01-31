73be4b25aae62b32abaad1b6aba8db8c9961abf4
Fabio Silva <fabio@isi.edu>
2012-01-31 20:05:08 +0000

    Added the schema_upgrade tool to the Pegasus svn. This is related to JIRA issue PM-505.

---
+++ share/pegasus/sql/schema_tool.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+"""
+A simple command-line tool to query and set schema version 
+information in a stampede database.  Will also upgrade an 
+existing database to a newer schema version.
+
+Will need to be run as a user that has CREATE | ALTER TABLE 
+permissions in the database.
+
+Requires a standard SQLALchemy connection string.
+"""
+
+__rcsid__ = "$Id: schema_tool.py 29514 2012-01-23 18:22:03Z mgoode $"
+__author__ = "Monte Goode"
+
+import os
+import sys
+import logging
+import subprocess
+
+# use pegasus-config to get basic pegasus settings
+bin_dir = os.path.join(os.path.normpath(os.path.join(os.path.dirname(sys.argv[0]))), "../../../bin")
+pegasus_config = os.path.join(bin_dir, "pegasus-config") + " --python-dump"
+config = subprocess.Popen(pegasus_config, stdout=subprocess.PIPE, shell=True).communicate()[0]
+exec config
+
+from netlogger.analysis.schema.schema_check import ConnHandle, SchemaCheck
+from netlogger.nllog import OptionParser, get_logger
+
+
+def main():
+    usage = "%prog {-c | -u} connString='required' mysql_engine='optional'"
+    desc = ' '.join(__doc__.split())
+    parser = OptionParser(usage=usage, description=desc)
+    parser.add_option('-c', '--check', dest='schema_check', action='store_true',
+                        default=False, help="Perform a schema check")
+    parser.add_option('-u', '--upgrade', dest='upgrade', action='store_true',
+                        default=False, help="Upgrade database to current version.")
+    options, args = parser.parse_args(sys.argv[1:])
+    log = get_logger(__file__)
+    
+    num_modes = (0,1)[bool(options.schema_check)] + (0,1)[bool(options.upgrade)]
+    if num_modes > 1:
+        parser.error('Choose only one option flag')
+        
+    init = {}
+    for a in args:
+        k,v = a.split('=')
+        if k in ['connString', 'mysql_engine']:
+            init[k] = v
+    
+    conn = ConnHandle(**init)
+    s_check = SchemaCheck(conn.get_session())
+    
+    if options.schema_check:
+        log.info('Executing schema check')
+        s_check.check_schema()
+    elif options.upgrade:
+        log.info('Performing upgrade')
+        s_check.upgrade()
+    pass
+    
+if __name__ == '__main__':
+    main()
