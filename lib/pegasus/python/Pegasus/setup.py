867a3636dc85715ac7168e6ab811b1e687f342c8
Daniel Gunter <dkgunter@lbl.gov>
2012-02-11 16:13:13 +0000

    install scripts for Pegasus libs, on their own

---
+++ lib/pegasus/python/Pegasus/setup.py
@@ -0,0 +1,54 @@
+"""
+Installation script for Pegasus Python library
+
+Author: Dan Gunter <dkgunter@lbl.gov>
+"""
+try:
+    from setuptools import setup
+except:
+    from distutils.core import setup
+from glob import glob
+import os
+import sys
+
+VERSION = os.environ.get('PEGASUS_VERSION','trunk')
+
+# Main function
+# -------------
+
+setup(name = "Pegasus",
+      version=VERSION,
+      packages = ["monitoring",
+          "plots_stats",
+          "plots_stats.plots",
+          "plots_stats.stats",
+          "test",
+          "tools"],
+      ext_modules = [],
+      package_data = {},
+      scripts = [ ],
+      install_requires=[ ],
+      # metadata for upload to PyPI
+      author = "Pegasus Team",
+      author_email = "deelman@isi.edu",
+      maintainer = "Karan Vahi",
+      maintainer_email = "vahi@isi.edu",
+      description = "Pegasus Python library",
+      long_description = "",
+      license = "LBNL Open-Source",
+      keywords = "workflow",
+      url = "https://confluence.pegasus.isi.edu/display/pegasus/Home",
+      classifiers = [
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: No Input/Output (Daemon)",
+        "Intended Audience :: Science/Research",
+        "Intended Audience :: System Administrators",
+        "License :: Other/Proprietary License",
+        "Natural Language :: English",
+        "Operating System :: POSIX",
+        "Programming Language :: Python",
+        "Topic :: Database",
+        "Topic :: Workflow",
+        "Topic :: System :: Monitoring",
+        ],
+      )
