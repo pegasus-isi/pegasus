a392c3b436487c60ff0f3af606e98f03742e9634
Gaurang Mehta <gmehta@isi.edu>
2012-04-26 00:34:17 +0000

    adding monte's bug fixes to statistics

--- lib/pegasus/python/netlogger/version.py
+++ lib/pegasus/python/netlogger/version.py
@@ -1,12 +1,12 @@
-## Copyright (c) 2004-2009, The Regents of the University of California, 
-## through Lawrence Berkeley National Laboratory 
-## (subject to receipt of any required approvals from 
+## Copyright (c) 2004-2012, The Regents of the University of California,
+## through Lawrence Berkeley National Laboratory
+## (subject to receipt of any required approvals from
 ## the U.S. Dept. of Energy).  All rights reserved.
 """
 Software version
 """
-NL_VERSION = "(development)"
-NL_CREATE_DATE = "$Date: 2009-07-29 13:42:27 -0700 (Wed, 29 Jul 2009) $"[7:-1].strip()
-NL_COPYRIGHT = """Copyright (c) 2004-2009, The Regents of the University of California, through 
-Lawrence Berkeley National Laboratory (subject to receipt of any required 
+NL_VERSION = "4.3.0"
+NL_CREATE_DATE = "$Date$"[7:-1].strip()
+NL_COPYRIGHT = """Copyright (c) 2004-2012, The Regents of the University of California, through
+Lawrence Berkeley National Laboratory (subject to receipt of any required
 approvals from the U.S. Dept. of Energy).  All rights reserved."""
