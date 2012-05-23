0a141f0d6394c1b311e514567dea27f02bd663e6
Gideon Juve <gideon@isi.edu>
2012-05-23 16:01:24 +0000

    PMC: Try setting all memory limits
    RLIMIT_DATA didn't work, so set RLIMIT_STACK and RLIMIT_AS as well.

--- src/tools/pegasus-mpi-cluster/test/limit.py
+++ src/tools/pegasus-mpi-cluster/test/limit.py
@@ -1,6 +1,8 @@
 import resource
 
-print "Memory limits:",resource.getrlimit(resource.RLIMIT_DATA)
+print "RLIMIT_DATA:",resource.getrlimit(resource.RLIMIT_DATA)
+print "RLIMIT_STACK:",resource.getrlimit(resource.RLIMIT_STACK)
+print "RLIMIT_AS:",resource.getrlimit(resource.RLIMIT_AS)
 
-# Make sure we malloc at least 1MB
+# Make sure we allocate at least 1MB
 x = 'c' * (1024*1024)
