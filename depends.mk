e7b6e270656c272f5ff12fba88a8aa4751b573f3
Gideon Juve <gideon@isi.edu>
2012-09-19 17:16:02 +0000

    PMC: Add fdcache unit tests, update dependencies

--- depends.mk
+++ depends.mk
@@ -2,16 +2,18 @@ dag.o: dag.cpp strlib.h dag.h failure.h log.h
 engine.o: engine.cpp strlib.h dag.h failure.h log.h engine.h
 failure.o: failure.cpp failure.h
 log.o: log.cpp log.h
-master.o: master.cpp master.h engine.h dag.h failure.h protocol.h log.h \
+master.o: master.cpp master.h engine.h dag.h protocol.h failure.h log.h \
   tools.h
-pegasus-mpi-cluster.o: pegasus-mpi-cluster.cpp dag.h engine.h master.h \
-  worker.h failure.h log.h protocol.h svn.h
-protocol.o: protocol.cpp protocol.h
+pegasus-mpi-cluster.o: pegasus-mpi-cluster.cpp svn.h dag.h engine.h \
+  master.h protocol.h worker.h failure.h log.h
+protocol.o: protocol.cpp tools.h protocol.h failure.h log.h
 strlib.o: strlib.cpp strlib.h
 test-dag.o: test-dag.cpp dag.h failure.h
 test-engine.o: test-engine.cpp dag.h engine.h failure.h
+test-fdcache.o: test-fdcache.cpp master.h engine.h dag.h protocol.h \
+  failure.h log.h
 test-log.o: test-log.cpp log.h failure.h
 test-strlib.o: test-strlib.cpp strlib.h failure.h
-test-fdcache.o: test-fdcache.cpp failure.h master.h log.h
-tools.o: tools.cpp tools.h
+test-tools.o: test-tools.cpp tools.h failure.h
+tools.o: tools.cpp tools.h failure.h
 worker.o: worker.cpp strlib.h worker.h protocol.h log.h failure.h tools.h
