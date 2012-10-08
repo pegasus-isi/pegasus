8a3e4f7f367ab850c964205bd2d125d42d23626d
Rajiv Mayani <mayani@isi.edu>
2012-10-08 18:44:33 +0000

    Moving vertical table init function to script.js

--- share/pegasus/dashboard/static/js/script.js
+++ share/pegasus/dashboard/static/js/script.js
@@ -1,1 +1,18 @@
 "use strict";
+
+function verticalTableInit (selector)
+{
+	var table = $(selector);
+	table.addClass ('dataTable');
+	table.attr ('cellspacing', 0);
+	table.attr ('cellpadding', 5);
+	$(selector + '> tbody > tr > th').each (function (index)
+	{
+		$(this).addClass ('ui-state-default');
+	});
+	
+	$(selector + '> tbody > tr').each (function (index)
+	{
+		$(this).addClass ('odd');
+	});
+}
