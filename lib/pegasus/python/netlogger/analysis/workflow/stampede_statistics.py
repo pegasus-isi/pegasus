46212885f973a1152e7a84e1931df53c10dc0066
Rajiv Mayani <mayani@isi.edu>
2012-10-12 22:52:45 +0000

    Change get_total_successful_job_status to accomodate case where the workflow associated with object is not the root level workflow and expand is set to true.

--- lib/pegasus/python/netlogger/analysis/workflow/stampede_statistics.py
+++ lib/pegasus/python/netlogger/analysis/workflow/stampede_statistics.py
@@ -403,8 +403,10 @@ class StampedeStatistics(SQLAlchemyInit, DoesLogging):
         JobInstanceSub = orm.aliased(JobInstance, name='JobInstanceSub')
         sq_1 = self.session.query(func.max(JobInstanceSub.job_submit_seq).label('jss'), JobInstanceSub.job_id.label('jobid'),
                 JobInstanceSub.exitcode.label('ec'))
-        if self._expand:
+        if self._expand and self._is_root_wf:
             sq_1 = sq_1.filter(Workflow.root_wf_id == self._root_wf_id)
+        elif self._expand and not self._is_root_wf:
+            sq_1 = sq_1.filter(Workflow.root_wf_id.in_ (self._wfs))
         else:
             sq_1 = sq_1.filter(Workflow.wf_id == self._wfs[0])
         sq_1 = sq_1.filter(Workflow.wf_id == Job.wf_id)
