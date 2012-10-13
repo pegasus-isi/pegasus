08e89c268b40c7e4624e1c64f49b6470b20455b9
Gaurang Mehta <gmehta@isi.edu>
2012-10-13 02:07:18 +0000

    Adding example of using hierarchical workflows to support one file split to multiple files and determining the parallelism of child jobs on runtime using python dax generators

---
+++ share/pegasus/examples/one-to-many-dynamic-python/root-workflow.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+
+"""
+Pegasus Dax Generator example to show spliting of 
+large files without knowing the number of output files.
+The technique used here is to introduce a fake dependent 
+node that determines the number of out files created and generates a sub dax
+at runtime. This dax is then evaluated and executed on the remote host 
+just in time before execution
+
+Usage: split-hierarchical daxfile
+"""
+
+
+from Pegasus.DAX3 import *
+import sys
+import os
+
+if len(sys.argv) != 2:
+    print "Usage: %s PEGASUS_HOME" % (sys.argv[0])
+    sys.exit(1)
+
+# Create a abstract dag
+root_dax = ADAG("root-workflow")
+
+# Add input file to the DAX-level replica catalog
+a = File("input_file")
+a.addPFN(PFN("file://" + os.getcwd() + "/input.txt", "local"))
+root_dax.addFile(a)
+
+c = File("sub-workflow.dax")
+c.addPFN(PFN("file://"+os.getcwd()+"/sub-workflow.dax","local"))
+root_dax.addFile(c)
+
+# Add executables to the DAX-level replica catalog
+# In this case the binary is linux split for splitting files, sub-workflow.py to generate the diamond sub dax.
+
+e_split = Executable(namespace="linux", name="split",  os="linux", arch="x86_64", installed=True)
+e_split.addPFN(PFN("file://"+os.getenv('CLUSTER_SOFTWARE_LOCATION')+"/split", "TestCluster"))
+root_dax.addExecutable(e_split)
+    
+e_generate_diamond = Executable(namespace="diamond", name="generate",  os="linux", arch="x86_64", installed=True)
+e_generate_diamond.addPFN(PFN("file://" + os.getcwd() + "/sub-workflow.py", "local"))
+root_dax.addExecutable(e_generate_diamond)
+
+# Add a remote or local split job
+split = Job(namespace="linux", name="split")
+b = File("output.list")
+split.addArguments("-l 1",a,"output.txt.")
+split.uses(a, link=Link.INPUT)
+split.setStdout(b)
+split.uses(b, link=Link.OUTPUT)
+root_dax.addJob(split)
+
+# Add a job to analyze the output of split and generate a sub dax with correct number of parallelism based on output of previous job
+generate = Job(namespace="diamond", name="generate")
+generate.addArguments(sys.argv[1],b,os.getcwd()+"/sub-workflow.dax")
+#generate.setStdout(c)
+generate.uses(b, link=Link.INPUT)
+generate.addProfile(Profile(namespace="env",key="PYTHONPATH",value=os.environ['PYTHONPATH']))
+#generate.uses(c, link=Link.OUTPUT)
+root_dax.addJob(generate)
+
+# Add a subdax job of type DAX that takes the runtime generated sub dax file in the previous step and runs the computation.
+sub_dax = DAX(c)
+sub_dax.addArguments("--output local","--basename sub-workflow")
+root_dax.addJob(sub_dax)
+
+
+# Add control-flow dependencies
+root_dax.addDependency(Dependency(parent=split, child=generate))
+root_dax.addDependency(Dependency(parent=generate, child=sub_dax))
+
+# Write the DAX to stdout
+root_dax.writeXML(sys.stdout)
---
+++ share/pegasus/examples/one-to-many-dynamic-python/sub-workflow.py
@@ -0,0 +1,112 @@
+#!/usr/bin/env python
+
+
+"""
+Pegasus Dax Generator example to show spliting of 
+large files without knowing the number of output files.
+The technique used here is to introduce a fake dependent 
+node that determines the number of out files created and generates a sub dax
+at runtime. This dax is then evaluated and executed on the remote host 
+just in time before execution
+
+Usage: split-hierarchical daxfile
+"""
+
+from Pegasus.DAX3 import *
+import sys
+import os
+import re
+
+if len(sys.argv) != 4:
+	print "Usage: %s PEGASUS_HOME inputlistfile outputfile" % (sys.argv[0])
+	sys.exit(1)
+
+
+if not os.path.isfile(sys.argv[2]):
+	print "please provide the input list file"
+	print "Usage: %s PEGASUS_HOME inputlistfile outputfile" % (sys.argv[0])
+	sys.exit(1)	
+	
+infile = open(sys.argv[2])
+files=[]
+fileregex=re.compile("(.+)\s+(.+)")
+
+# Create a abstract dag
+diamond = ADAG("sub-workflow")
+
+	# Add executables to the DAX-level replica catalog
+	# In this case the binary is keg, which is shipped with Pegasus, so we use
+	# the remote PEGASUS_HOME to build the path.
+e_preprocess = Executable(namespace="diamond", name="preprocess", version="4.0", os="linux", arch="x86_64", installed=True)
+e_preprocess.addPFN(PFN("file://" + sys.argv[1] + "/bin/pegasus-keg", "TestCluster"))
+diamond.addExecutable(e_preprocess)
+	
+e_findrange = Executable(namespace="diamond", name="findrange", version="4.0", os="linux", arch="x86_64", installed=True)
+e_findrange.addPFN(PFN("file://" + sys.argv[1] + "/bin/pegasus-keg", "TestCluster"))
+diamond.addExecutable(e_findrange)
+	
+e_analyze = Executable(namespace="diamond", name="analyze", version="4.0", os="linux", arch="x86_64", installed=True)
+e_analyze.addPFN(PFN("file://" + sys.argv[1] + "/bin/pegasus-keg", "TestCluster"))
+diamond.addExecutable(e_analyze)
+
+for l in infile:
+	line = l.strip()
+	if not line:
+		continue
+	ifile=fileregex.split(line)
+	# Add input file to the DAX-level replica catalog
+	a = File(ifile[1])
+	a.addPFN(PFN(ifile[2],"TestCluster"))
+	diamond.addFile(a)
+		
+
+	# Add a preprocess job
+	preprocess = Job(namespace="diamond", name="preprocess", version="4.0")
+	b1 = File(ifile[1]+".b1")
+	b2 = File(ifile[1]+".b2")
+	preprocess.addArguments("-a preprocess","-T60","-i",a,"-o",b1,b2)
+	preprocess.uses(a, link=Link.INPUT)
+	preprocess.uses(b1, link=Link.OUTPUT)
+	preprocess.uses(b2, link=Link.OUTPUT)
+	diamond.addJob(preprocess)
+	
+	# Add left Findrange job
+	frl = Job(namespace="diamond", name="findrange", version="4.0")
+	c1 = File(ifile[1]+"f.c1")
+	frl.addArguments("-a findrange","-T60","-i",b1,"-o",c1)
+	frl.uses(b1, link=Link.INPUT)
+	frl.uses(c1, link=Link.OUTPUT)
+	diamond.addJob(frl)
+	
+	# Add right Findrange job
+	frr = Job(namespace="diamond", name="findrange", version="4.0")
+	c2 = File(ifile[1]+"f.c2")
+	frr.addArguments("-a findrange","-T60","-i",b2,"-o",c2)
+	frr.uses(b2, link=Link.INPUT)
+	frr.uses(c2, link=Link.OUTPUT)
+	diamond.addJob(frr)
+	
+	# Add Analyze job
+	analyze = Job(namespace="diamond", name="analyze", version="4.0")
+	d = File(ifile[1]+"f.d")
+	analyze.addArguments("-a analyze","-T60","-i",c1,c2,"-o",d)
+	analyze.uses(c1, link=Link.INPUT)
+	analyze.uses(c2, link=Link.INPUT)
+	analyze.uses(d, link=Link.OUTPUT, register=True)
+	diamond.addJob(analyze)
+	
+	# Add control-flow dependencies
+	diamond.addDependency(Dependency(parent=preprocess, child=frl))
+	diamond.addDependency(Dependency(parent=preprocess, child=frr))
+	diamond.addDependency(Dependency(parent=frl, child=analyze))
+	diamond.addDependency(Dependency(parent=frr, child=analyze))
+
+# Write the DAX to stdout
+
+outfile=open(sys.argv[3],"w")
+diamond.writeXML(outfile)
+infile.close()
+outfile.close()
+
+
+
