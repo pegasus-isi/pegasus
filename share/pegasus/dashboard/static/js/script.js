239df45cecf1e5cc1e08dfaa1e366bf937705112
Rajiv Mayani <mayani@isi.edu>
2012-10-23 20:44:07 +0000

    Ensure column headers do not get wrapped on a new line

--- share/pegasus/dashboard/static/js/script.js
+++ share/pegasus/dashboard/static/js/script.js
@@ -8,7 +8,7 @@ function verticalTableInit (selector)
 	table.attr ('cellpadding', 5);
 	$(selector + '> tbody > tr > th').each (function (index)
 	{
-		$(this).addClass ('ui-state-default');
+		$(this).addClass ('ui-state-default nowrap');
 	});
 	
 	$(selector + '> tbody > tr').each (function (index)
