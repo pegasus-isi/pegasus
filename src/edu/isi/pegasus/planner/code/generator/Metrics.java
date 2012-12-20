a3e046dcc3eb02548e78d8dce074a3a372bba1c8
Karan Vahi <vahi@isi.edu>
2012-12-20 01:04:33 +0000

    PM-678
    We now log metrics asyncronously using the Executor Server and Java
    Futures.

--- src/edu/isi/pegasus/planner/code/generator/Metrics.java
+++ src/edu/isi/pegasus/planner/code/generator/Metrics.java
@@ -24,26 +24,28 @@ import edu.isi.pegasus.common.util.Boolean;
 
 import edu.isi.pegasus.planner.classes.PegasusBag;
 
-import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.Callable;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 
 /**
- * A Metrics file generator that generates a metrics file in the submit directory
+ * Logs workflow metrics to a file in the submit directory and also sends them
+ * over a HTTP connection to a Metrics Server.
  *
- * The following metrics are logged in the metrics file
- *
- * <pre>
- * </pre>
  *
  * @author Karan Vahi
  * @version $Revision$
@@ -71,7 +73,11 @@ public class Metrics  {
      * The name of the environment variable that overrides the default server url
      */
     public static final String METRICS_SERVER_URL_ENV_VARIABLE = "PEGASUS_METRICS_SERVER";
-    
+
+    /**
+     * The timeout in seconds for sending the metrics to the server
+     */
+    public static final int METRICS_SEND_TIMEOUT  = 5;
     
     /**
      * boolean indicating whether to log metrics or not
@@ -129,7 +135,7 @@ public class Metrics  {
         this.writeOutMetricsFile( metrics );
 
         if( this.mSendMetricsToServer ){
-            sendMetricsToServer( metrics , mMetricsServerURL );
+            sendMetricsAsynchronously( metrics , mMetricsServerURL );
         }
     }
 
@@ -175,101 +181,188 @@ public class Metrics  {
      * @param metrics   the metrics to log
      * @param url       the url to send the metrics to
      */
-    private void sendMetricsToServer(PlannerMetrics metrics, String url ) throws IOException{
+    private void sendMetricsSynchronously(PlannerMetrics metrics, String url ) throws IOException{
         mLogger.log( "Planner Metrics will be sent to " + url,
                      LogManager.DEBUG_MESSAGE_LEVEL );
 
-        URL u = new URL( url );
-        HttpURLConnection connection = (HttpURLConnection) u.openConnection();
-        connection.setDoOutput( true );
-        //connection.setDoInput( true );
-        connection.setRequestMethod( "POST" );
+        SendMetrics sm = new SendMetrics( metrics, url );
 
-        connection.setRequestProperty( "Content-Type", "application/json");
+        SendMetricsResult result = sm.call();
 
-        try{
-            OutputStreamWriter out = new OutputStreamWriter(
-                                         connection.getOutputStream());
-
-            try{
-                //String payload = URLEncoder.encode( metrics.toJson(), "UTF-8") ;
-                String payload = metrics.toJson();
-                out.write(  payload );
-            }
-            finally{
-                out.close();
-            }
-
-            int responseCode = connection.getResponseCode();
-
-            if( responseCode == 202 ){
-                mLogger.log( "Metrics succesfully sent to the server", LogManager.DEBUG_MESSAGE_LEVEL );
-            }
-            else{
-                mLogger.log( "Unable to send metrics to the server " + responseCode + " " + connection.getResponseMessage(),
-                             LogManager.DEBUG_MESSAGE_LEVEL );
-            }
-
-            BufferedReader in = new BufferedReader(
-                                    new InputStreamReader(
-                                    connection.getInputStream()));
-            String result;
-            while (( result = in.readLine()) != null) {
-                System.out.println( result );
-            }
+        if( result.getCode() == 202 ){
+            mLogger.log( "Metrics succesfully sent to the server", LogManager.DEBUG_MESSAGE_LEVEL );
         }
-        finally{
-            connection.disconnect();
+        else{
+            mLogger.log( "Unable to send metrics to the server " + result,
+                             LogManager.DEBUG_MESSAGE_LEVEL );
         }
     }
 
     /**
-     * Writes out the planner metrics to the global log.
+     * Sends the planner metrics to the metrics server asynchrnously with a
+     * timeout of 5 seconds
      *
-     * @param pm  the metrics to be written out.
-     *
-     * @return boolean
+     * @param metrics   the metrics to log
+     * @param url       the url to send the metrics to
      */
-    /*
-    protected boolean writeOutMetrics( PlannerMetrics pm  ){
-        boolean result = false;
-        System.out.print( pm.toPrettyJson() );
-        if ( mProps.writeOutMetrics() ) {
-            File log = new File( mProps.getMetricsLogFile() );
+    private void sendMetricsAsynchronously( PlannerMetrics metrics, String url ){
 
+        mLogger.log( "Planner Metrics will be sent to " + url,
+                     LogManager.DEBUG_MESSAGE_LEVEL );
 
-            //do a sanity check on the directory
-            try{
-                sanityCheck( log.getParentFile() );
-                //open the log file in append mode
-                FileOutputStream fos = new FileOutputStream( log ,true );
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+  //      Future<SendMetricsResult> future =   (Future<SendMetricsResult>) executor.submit(
+ //                                                           new FutureTask<SendMetricsResult>( new SendMetrics( metrics, url ) ));
 
-                //get an exclusive lock
-                FileLock fl = fos.getChannel().lock();
-                try{
-                    mLogger.log( "Logging Planner Metrics to " + log,
+        Future<SendMetricsResult> future =   (Future<SendMetricsResult>) executor.submit( new SendMetrics( metrics, url ));
+
+        SendMetricsResult result = null;
+        try {
+            result = future.get( METRICS_SEND_TIMEOUT, TimeUnit.SECONDS )  ;
+
+        } catch (InterruptedException ex) {
+            mLogger.log( "Interrupted while sending metrics " + url, ex,
+                         LogManager.DEBUG_MESSAGE_LEVEL );
+        } catch (ExecutionException ex) {
+            mLogger.log( "Exception caught while sending metrics to server " + url, ex,
+                         LogManager.DEBUG_MESSAGE_LEVEL );
+        } catch (TimeoutException e) {
+            mLogger.log( "Sending of metrics to server timed out " + url,
+                         LogManager.DEBUG_MESSAGE_LEVEL );
+        }
+        finally{
+            executor.shutdownNow();
+        }
+
+        if( result != null ){
+            if( result.getCode() == 202 ){
+               mLogger.log( "Metrics succesfully sent to the server", LogManager.DEBUG_MESSAGE_LEVEL );
+            }
+            else{
+                mLogger.log( "Unable to send metrics to the server " + result,
                                  LogManager.DEBUG_MESSAGE_LEVEL );
-                    //write out to the planner metrics to fos
-                    fos.write( pm.toPrettyJson().getBytes() );
+            }
+        }
+
+        
+    }
+
+
+}
+    
+
+/**
+ * A Send metrics class that is used to send metrics to the metrics server
+ * using HTTP POST methods
+ *
+ * @author vahi
+ */
+class SendMetrics  implements Callable{
+
+        private PlannerMetrics mMetrics;
+
+        private String mURL;
+
+        public SendMetrics( PlannerMetrics metrics, String url ){
+            mMetrics = metrics;
+            mURL     = url;
+        }
+
+        public SendMetricsResult call () throws java.io.IOException {
+            SendMetricsResult result = this.send( mMetrics, mURL );
+            return result;
+        }
+
+        /**
+         * Sends the planner metrics to the metrics server
+         *
+         * @param metrics   the metrics to log
+         * @param url       the url to send the metrics to
+         */
+        private SendMetricsResult send(PlannerMetrics metrics, String url ) throws IOException{
+            SendMetricsResult result = new SendMetricsResult();
+            
+       
+            URL u = new URL( url );
+            HttpURLConnection connection = (HttpURLConnection) u.openConnection();
+            connection.setDoOutput( true );
+            //connection.setDoInput( true );
+            connection.setRequestMethod( "POST" );
+
+            connection.setRequestProperty( "Content-Type", "application/json");
+
+            try{
+                OutputStreamWriter out = new OutputStreamWriter(
+                                               connection.getOutputStream());
+
+                try{
+                    //String payload = URLEncoder.encode( metrics.toJson(), "UTF-8") ;
+                    String payload = metrics.toJson();
+                    out.write(  payload );
                 }
                 finally{
-                    fl.release();
-                    fos.close();
+                    out.close();
                 }
 
+                result.setCode( connection.getResponseCode() );
+                result.setResponseMessage( connection.getResponseMessage() );
+
+                /*
+                BufferedReader in = new BufferedReader(
+                                        new InputStreamReader(
+                                         connection.getInputStream()));
+                String result;
+                while (( result = in.readLine()) != null) {
+                    System.out.println( result );
+                }*/
             }
-            catch( IOException ioe ){
-                mLogger.log( "Unable to write out planner metrics ", ioe,
-                             LogManager.DEBUG_MESSAGE_LEVEL );
-                return false;
+            finally{
+                connection.disconnect();
             }
 
-            result = true;
+            return result;
         }
-        return result;
+
+}
+
+
+class SendMetricsResult {
+
+    private int mResponseCode;
+    private String mResponseMessage;
+
+    public SendMetricsResult() {
+        mResponseCode = -1;
+        mResponseMessage = "Results not retrieved yet";
     }
-    */
-   
-    
-    
+
+    public SendMetricsResult(int code, String message) {
+        mResponseCode = code;
+        mResponseMessage = message;
+    }
+
+    public void setCode(int code) {
+        this.mResponseCode = code;
+    }
+
+    public void setResponseMessage(String response) {
+        this.mResponseMessage = response;
+    }
+
+    public int getCode() {
+        return this.mResponseCode;
+    }
+
+    public String getResponseMessage() {
+        return this.mResponseMessage;
+    }
+
+    public String toString(){
+        StringBuffer sb = new StringBuffer();
+
+        sb.append(  "code = " ).append( this.mResponseCode ).
+           append(  "  " ).append( this.mResponseMessage );
+
+        return sb.toString();
+    }
 }
