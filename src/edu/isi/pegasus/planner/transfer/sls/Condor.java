4ae6a0ec97b77ea5a3eb113fe8dafbaaa392a091
Karan Vahi <vahi@isi.edu>
2013-01-10 23:30:11 +0000

    PM-676
    We want the pegasus lite case to work with creamce.

--- src/edu/isi/pegasus/planner/transfer/sls/Condor.java
+++ src/edu/isi/pegasus/planner/transfer/sls/Condor.java
@@ -287,8 +287,12 @@ public class Condor   implements SLS {
         //handle the -w option that asks kickstart to change
         //directory before launching an executable.
         String style = (String)job.vdsNS.get( Pegasus.STYLE_KEY );
-        if( style == null || !( style.equals( Pegasus.CONDOR_STYLE ) || style.equals( Pegasus.GLIDEIN_STYLE ) || style.equals( Pegasus.CONDORC_STYLE ) ) ){
-            mLogger.log( "Invalid style for the job " + job.getName(),
+        if( style == null || 
+                !( style.equals( Pegasus.CONDOR_STYLE ) || style.equals( Pegasus.GLIDEIN_STYLE ) ||
+                      style.equals( Pegasus.CONDORC_STYLE ) || style.equals( Pegasus.CREAMCE_STYLE )
+
+                  ) ){
+            mLogger.log( "Invalid style " + style + " for the job " + job.getName(),
                          LogManager.ERROR_MESSAGE_LEVEL );
             return false;
         }
