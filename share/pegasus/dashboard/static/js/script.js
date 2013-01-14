d37e9eff4d84840be81d96eec531d4bfa8994b7e
Karan Vahi <vahi@isi.edu>
2013-01-14 22:33:48 +0000

    PM-683
    Undid the change in r5968
    and implemented the change by adding a javascript function.
    
    We also have odd and level alternate coloring of the table rows.
    failed workflow - red
    successful workflow - green
    runnign workflow - move/blue?

--- share/pegasus/dashboard/static/js/script.js
+++ share/pegasus/dashboard/static/js/script.js
@@ -16,3 +16,34 @@ function verticalTableInit (selector)
 		$(this).addClass ('odd');
 	});
 }
+
+/**
+ * Display a table with based on status
+ */
+function verticalTableInitStatus (selector, status)
+{
+    var table = $(selector);
+    table.addClass ('dataTable');
+    table.attr ('cellspacing', 0);
+    table.attr ('cellpadding', 5);
+    $(selector + '> tbody > tr > th').each (function (index)
+					    {
+						$(this).addClass ('ui-state-default nowrap');
+					    });
+    
+    $(selector + '> tbody > tr').each (function (index)
+				       {
+					   if (status != null && status == 'Failed')
+					   {
+					       $(this).addClass ('failed'); // Might require change in some CSS file, probably jquery*dataTables*theme*.css file.
+					   }
+					   else if (status != null && status == 'Successful')
+                                           {
+                                               $(this).addClass ('failed');
+                                           }
+					   else
+					   {
+					       $(this).addClass ('running');
+					   }
+				       });
+}
