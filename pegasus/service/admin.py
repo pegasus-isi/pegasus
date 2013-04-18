59747cebc0f6a418684fe1c54358c297240eec36
Gideon Juve <gideon@isi.edu>
2013-04-18 11:47:58 -0700

    Add rudimentary database migration support

--- pegasus/service/admin.py
+++ pegasus/service/admin.py
@@ -1,28 +1,62 @@
+import os
 import sys
 
 from pegasus.service import db, models
+from pegasus.service.models import Schema
+
+
+def current_schema():
+    schema = db.session.query(Schema).\
+                        order_by(Schema.version.desc()).\
+                        first()
+    return schema
 
 def usage():
-    print "Usage: %s COMMAND\n" % sys.argv[0]
-    print """
-Commands:
-    help    Print this message
-    create  Create database
-    drop    Drop database
-"""
+    """Print help message"""
+    print "Usage: %s COMMAND\n" % os.path.basename(sys.argv[0])
+    print "Commands:"
+    for name, fn in COMMANDS.items():
+        print "    %-10s %s" % (name, fn.__doc__)
     exit(1)
 
-help = usage
-
 def create():
+    """Create the database"""
     print "Creating database..."
     db.create_all()
+    schema = current_schema()
+    if schema is None:
+        db.session.add(Schema(models.version))
+        db.session.commit()
+    elif schema.version < models.version:
+        print "Schema out of date. Please run migrate."
 
 def drop():
+    """Drop the database"""
     sure = raw_input("Are you sure? [y/n] ") == "y"
     if sure:
         print "Dropping database..."
         db.drop_all()
+
+def migrate():
+    """Update the database schema"""
+    schema = current_schema()
+    current = schema.version
+    latest = models.version
+    if current < latest:
+        print "Updating database from v%d to v%d..." %(current, latest)
+        #TODO Actually update the schema
+        print "NOT YET IMPLEMENTED"
+        db.session.add(Schema(models.version))
+        db.session.commit()
+    else:
+        print "Schema up to date"
+
+COMMANDS = {
+    'help': usage,
+    'create': create,
+    'drop': drop,
+    'migrate': migrate
+}
 
 def main():
     if len(sys.argv) <= 1:
@@ -32,10 +66,11 @@ def main():
         usage()
     
     command = sys.argv[1]
-    fn = globals().get(command, None)
+    fn = COMMANDS.get(command, None)
     
     if not fn:
-        usage()
+        print "No such command: %s" % command
+        exit(1)
     
     fn()
 
--- pegasus/service/models.py
+++ pegasus/service/models.py
@@ -1,18 +1,39 @@
+from datetime import datetime
+
 from pegasus.service import db
 
+#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+# IMPORTANT
+# This is the current version of the schema. It should be incremented 
+# each time a change is made to the schema and a migration script 
+# should be created to go from the old version to the new version.
+version = 1
+#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+class Schema(db.Model):
+    __tablename__ = 'schema'
+    __table_args__ = {'mysql_engine':'InnoDB'}
+
+    id = db.Column(db.Integer, primary_key=True)
+    version = db.Column(db.Integer)
+    timestamp = db.Column(db.DateTime)
+
+    def __init__(self, version):
+        self.version = version
+        self.timestamp = datetime.utcnow()
+
+    def __repr__(self):
+        return '<Schema %d %s>' % (self.version, self.timestamp)
 
 class User(db.Model):
-    __tablename__ = 'users'
+    __tablename__ = 'user'
+    __table_args__ = {'mysql_engine':'InnoDB'}
     
-    ROLE_USER = 0
-    ROLE_ADMIN = 1
-    
-    id = db.Column(db.Integer, primary_key = True)
-    username = db.Column(db.String(64), index=True, unique=True)
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True)
     hashpass = db.Column(db.String(32))
-    email = db.Column(db.String(120), index=True, unique=True)
-    role = db.Column(db.SmallInteger, default = ROLE_USER)
+    email = db.Column(db.String(120), unique=True)
     
     def __repr__(self):
-        return '<User %r>' % (self.nickname)
+        return '<User %r>' % (self.username)
 
