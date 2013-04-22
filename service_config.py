1f3dc9aee3a73582c75ac14ef079f2cf970537e6
Gideon Juve <gideon@isi.edu>
2013-04-22 10:45:03 -0700

    Update configuration options for Flask-SQLAlchemy

--- service_config.py
+++ service_config.py
@@ -16,4 +16,16 @@ SECRET_KEY = os.urandom(24)
 #SQLALCHEMY_DATABASE_URI = "mysql://pegasus:secret@127.0.0.1:3306/pegasus_service"
 SQLALCHEMY_DATABASE_URI = 'sqlite:///%s/.pegasus/workflow.db' % os.getenv('HOME')
 
+# Set to True to log SQL queries
+#SQLALCHEMY_ECHO = False
+
+# Set to change the SQLAlchemy connection pool size
+#SQLALCHEMY_POOL_SIZE = 5
+
+# Set to change the connection timeout (in seconds)
+#SQLALCHEMY_POOL_TIMEOUT = 10
+
+# Set to change how long connections remain before being recycled (in seconds)
+# Default is 2 hours for MySQL
+#SQLALCHEMY_POOL_RECYCLE = 2 * 60 * 60
 
