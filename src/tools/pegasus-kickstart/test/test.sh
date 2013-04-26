c41617e40a16f87cf57d2084d75a714f270b02bf
Gideon Juve <gideon@isi.edu>
2013-04-25 19:34:49 -0700

    kickstart: Add unit test for -I issue

--- src/tools/pegasus-kickstart/test/test.sh
+++ src/tools/pegasus-kickstart/test/test.sh
@@ -4,13 +4,16 @@ KICKSTART=../pegasus-kickstart
 
 function run_test {
     echo "Running" $KICKSTART "$@"
-    $KICKSTART "$@" > /dev/null
-    if [ $? -eq 0 ]; then
-        echo "OK"
-    fi
+    $KICKSTART "$@" >/dev/null 2>&1
+    return $?
 }
 
-run_test ./lotsofprocs.sh
-run_test -B 10000 -t ./lotsofprocs.sh
-run_test -B 10000 ./lotsofprocs.sh
+run_test ./lotsofprocs.sh && echo "OK"
+run_test -B 10000 -t ./lotsofprocs.sh && echo "OK"
+run_test -B 10000 ./lotsofprocs.sh && echo "OK"
+
+run_test -I bindate.arg && echo "OK"
+
+# It should fail if we have anything after -I fn
+run_test -I bindate.arg -B 2 || echo "OK"
 
