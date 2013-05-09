836bc37e901edeff901710af3e9cdd8548ab6fc3
Gideon Juve <gideon@isi.edu>
2013-05-08 17:42:00 -0700

    Update create, drop, migrate to use new config

--- pegasus/service/admin.py
+++ pegasus/service/admin.py
@@ -1,7 +1,20 @@
 import os
 import sys
+from optparse import OptionParser
 
-from pegasus.service import models, migrations
+from pegasus.service import models, migrations, config
+
+def parse_args(args, synopsis):
+    script = os.path.basename(sys.argv[0])
+    parser = OptionParser("%s %s" % (script, synopsis))
+
+    config.add_options(parser)
+
+    options, args = parser.parse_args(args)
+
+    config.set_options(options)
+
+    return options, args, parser
 
 def usage():
     """Print help message"""
@@ -13,6 +26,11 @@ def usage():
 
 def create(args):
     """Create the database"""
+    options, args, parser = parse_args(args, "create")
+
+    if len(args) > 0:
+        parser.error("Invalid argument")
+
     # First we check to see if it is current
     schema = migrations.current_schema()
     if schema is None:
@@ -28,6 +46,12 @@ def create(args):
 
 def drop(args):
     """Drop the database"""
+    options, args, parser = parse_args(args, "drop")
+
+    if len(args) > 0:
+        print "Invalid argument"
+        usage()
+
     sure = raw_input("Are you sure? [y/n] ") == "y"
     if sure:
         print "Dropping database..."
@@ -35,29 +59,29 @@ def drop(args):
 
 def migrate(args):
     """Update the database schema"""
-    if len(args) > 2:
-        print "Usage: migrate [version]"
-        exit(1)
-    elif len(args) == 2:
+    options, args, parser = parse_args(args, "migrate [version]")
+
+    if len(args) > 1:
+        parser.error("Invalid argument")
+    elif len(args) == 1:
         target = int(args[1])
     else:
         target = models.version
-    
+
     current = migrations.current_schema()
     if current is None:
         print "No database schema. Please run create."
         exit(1)
-    
+
     if current == target:
         print "Database schema up to date"
         exit(0)
-    
+
     print "Migrating database schema from v%d to v%d..." %(current, target)
     migrations.migrate(target)
- 
+
 
 COMMANDS = {
-    'help': usage,
     'create': create,
     'drop': drop,
     'migrate': migrate
@@ -66,16 +90,13 @@ COMMANDS = {
 def main():
     if len(sys.argv) <= 1:
         usage()
-    
-    if "-h" in sys.argv or "--help" in sys.argv:
-        usage()
-    
+
     command = sys.argv[1]
     fn = COMMANDS.get(command, None)
-    
+
     if not fn:
         print "No such command: %s" % command
         exit(1)
-    
-    fn(sys.argv[1:])
+
+    fn(sys.argv[2:])
 
