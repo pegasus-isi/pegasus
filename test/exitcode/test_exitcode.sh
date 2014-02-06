ff8f1470cb60883a7b040d37a3027f951651251f
Gideon Juve <gideon@isi.edu>
2014-02-06 14:01:48 -0800

    PM-737 pegasus-exitcode extensions
    This commit adds some new features to pegasus-exitcode:
    
    * Success messages that must appear in the output
    * Failure messages that cannot appear in the output
    * A new cluster-summary parser
    
    And it removes some unused/unnecessary features:
    
    * PegasusLite checks, which is replaces with success messages
    * The --tasks option, which was never used

--- test/exitcode/test_exitcode.sh
+++ test/exitcode/test_exitcode.sh
@@ -6,33 +6,109 @@ tests=`dirname $cwd`
 home=`dirname $tests`
 bin=$home/bin
 
+function stderr {
+    echo "$@" >&2
+}
+
 function exitcode {
     echo "Testing $2..."
     result=`$bin/pegasus-exitcode --no-rename $2 2>&1`
     rc=$?
     if [ $rc -ne $1 ]; then
-        echo "$result" >&2
-        echo "ERROR" >&2
+        stderr "$result"
+        stderr "ERROR"
         exit 1
     else
         echo "OK"
+    fi
+}
+
+function run_test {
+    echo "Testing " "$@" "..."
+    "$@"
+    rc=$?
+    if [ $rc -ne 0 ]; then
+        stderr "ERROR"
+        exit 1
+    else
+        stderr "OK"
     fi
 }
 
 function test_rename_noerrfile {
-    echo "Testing test_rename_noerrfile..."
-    result=`$bin/pegasus-exitcode ok.out 2>&1`
+    result=$($bin/pegasus-exitcode ok.out 2>&1)
     rc=$?
     mv ok.out.000 ok.out
     if [ $rc -ne 0 ]; then
-        echo "$result" >&2
-        echo "ERROR" >&2
-        exit 1
-    else
-        echo "OK"
+        stderr "$result"
+        return 1
     fi
 }
 
+function test_failure_message_zero_exit {
+    result=$($bin/pegasus-exitcode --no-rename --failure-message "Job failed" failure_message_zero_exit.out 2>&1)
+    rc=$?
+    if [ $rc -ne 1 ]; then
+        stderr "$result"
+        return 1
+    fi
+}
+
+function test_success_message_failure_message {
+    result=$($bin/pegasus-exitcode --no-rename -s "Job succeeded" -f "Job failed" success_message_failure_message.out 2>&1)
+    rc=$?
+    if [ $rc -ne 1 ]; then
+        echo "$result" >&2
+        return 1
+    fi
+}
+
+function test_success_message_missing {
+    result=$($bin/pegasus-exitcode --no-rename -s "Job succeeded" success_message_missing.out 2>&1)
+    rc=$?
+    if [ $rc -ne 1 ]; then
+        echo "$result" >&2
+        return 1
+    fi
+}
+
+function test_success_message_present {
+    result=$($bin/pegasus-exitcode --no-rename -s "Job succeeded" success_message_zero_exit.out 2>&1)
+    rc=$?
+    if [ $rc -ne 0 ]; then
+        echo "$result" >&2
+        return 1
+    fi
+}
+
+function test_success_message_nonzero_exit {
+    result=$($bin/pegasus-exitcode --no-rename --success-message "Job succeeded" success_message_nonzero_exit.out 2>&1)
+    rc=$?
+    if [ $rc -ne 1 ]; then
+        echo "$result" >&2
+        return 1
+    fi
+}
+
+function test_all_success_messages_required {
+    result=$($bin/pegasus-exitcode --no-rename -s "Job succeeded" -s "Successfully finished" success_message_zero_exit.out 2>&1)
+    rc=$?
+    if [ $rc -ne 1 ]; then
+        echo "$result" >&2
+        return 1
+    fi
+}
+
+function unit_tests {
+    echo "Running unit tests..."
+    /usr/bin/env python tests.py $bin/pegasus-exitcode
+    rc=$?
+    if [ $rc -ne 0 ]; then
+        exit 1
+    fi
+}
+
+unit_tests
 # exitcode expected_result outfile
 exitcode 0 ok.out
 exitcode 1 failed.out
@@ -53,5 +129,11 @@ exitcode 1 cluster_summary_missing.out
 exitcode 0 cluster_summary_notasks.out
 exitcode 1 cluster_summary_nosucc.out
 exitcode 0 cluster_summary_submitted.out
-test_rename_noerrfile
+run_test test_rename_noerrfile
+run_test test_failure_message_zero_exit
+run_test test_success_message_failure_message
+run_test test_success_message_missing
+run_test test_success_message_present
+run_test test_success_message_nonzero_exit
+run_test test_all_success_messages_required
 
