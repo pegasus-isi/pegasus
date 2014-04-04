3f1e76b486049d10c1953b5f26e5d240bb0941ab
Karan Vahi <vahi@isi.edu>
2014-04-03 18:58:31 -0700

    PM-743 BalancedCluster will be the default refiner for 4.4.

--- src/edu/isi/pegasus/planner/transfer/refiner/RefinerFactory.java
+++ src/edu/isi/pegasus/planner/transfer/refiner/RefinerFactory.java
@@ -45,7 +45,7 @@ public class RefinerFactory {
     /**
      * The default refiner implementation that is picked up.
      */
-    public static final String DEFAULT_REFINER_IMPLEMENTATION = "Cluster";
+    public static final String DEFAULT_REFINER_IMPLEMENTATION = "BalancedCluster";
 
     /**
      * Loads the implementing class corresponding to the value specified in the
