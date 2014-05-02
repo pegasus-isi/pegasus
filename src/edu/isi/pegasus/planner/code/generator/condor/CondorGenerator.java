11eee7ab410dfa592a0e36f16f113c434bb58f11
Karan Vahi <vahi@isi.edu>
2014-05-02 14:52:25 -0700

    PM-747 undid last commit.. erroneous statements there

--- src/edu/isi/pegasus/planner/code/generator/condor/CondorGenerator.java
+++ src/edu/isi/pegasus/planner/code/generator/condor/CondorGenerator.java
@@ -307,14 +307,6 @@ public class CondorGenerator extends Abstract {
             mGridStartFactory.initialize( mBag, dag );
             mInitializeGridStart = false;
         }
-
-        Job dummy = new Job( );
-        dummy.setName( "create_dir_blackdiamond_0_condorpool");
-        dummy.setJobType( Job.CREATE_DIR_JOB);
-        dag.add( dummy );
-        Job analyze = (Job)((GraphNode)dag.getNode( "analyze_j4" )).getContent();
-        dag.addEdge( dummy.getID(), analyze.getID());
-        dag.checkForCorruption();
         
         CodeGenerator storkGenerator = CodeGeneratorFactory.loadInstance( mBag, "Stork" );
 
