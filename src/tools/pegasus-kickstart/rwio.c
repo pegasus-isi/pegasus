5aa92a7b052c1c99b05e75d02a81c6d89239664a
Gideon Juve <gideon@isi.edu>
2014-07-01 14:44:13 -0700

    pegasus-kickstart: Formatting rwio

--- src/tools/pegasus-kickstart/rwio.c
+++ src/tools/pegasus-kickstart/rwio.c
@@ -19,144 +19,96 @@
 #include <stdio.h>
 #include <time.h>
 #include <unistd.h>
-#ifdef sun
-#include <memory.h>
-#endif
 #include <string.h>
 
 #include "rwio.h"
 
-ssize_t
-writen( int fd, const char* buffer, ssize_t n, unsigned restart )
-/* purpose: write all n bytes in buffer, if possible at all
- * paramtr: fd (IN): filedescriptor open for writing
- *          buffer (IN): bytes to write (must be at least n byte long)
- *          n (IN): number of bytes to write 
- *          restart (IN): if true, try to restart write at max that often
- * returns: n, if everything was written, or
- *          [0..n-1], if some bytes were written, but then failed,
- *          < 0, if some error occurred.
- */
-{
-  int start = 0;
-  while ( start < n ) {
-    int size = write( fd, buffer+start, n-start );
-    if ( size < 0 ) {
-      if ( restart && errno == EINTR ) { restart--; continue; }
-      return size;
-    } else {
-      start += size;
+ssize_t writen( int fd, const char* buffer, ssize_t n, unsigned restart ) {
+    /* purpose: write all n bytes in buffer, if possible at all
+     * paramtr: fd (IN): filedescriptor open for writing
+     *          buffer (IN): bytes to write (must be at least n byte long)
+     *          n (IN): number of bytes to write 
+     *          restart (IN): if true, try to restart write at max that often
+     * returns: n, if everything was written, or
+     *          [0..n-1], if some bytes were written, but then failed,
+     *          < 0, if some error occurred.
+     */
+    int start = 0;
+    while (start < n) {
+        int size = write(fd, buffer+start, n-start);
+        if (size < 0) {
+            if (restart && errno == EINTR) {
+                restart--;
+                continue;
+            }
+            return size;
+        } else {
+            start += size;
+        }
     }
-  }
-  return n;
+    return n;
 }
 
-int
-lockit( int fd, int cmd, int type )
-/* purpose: fill in POSIX lock structure and attempt lock or unlock
- * paramtr: fd (IN): which file descriptor to lock
- *          cmd (IN): F_SETLK, F_GETLK, F_SETLKW
- *          type (IN): F_WRLCK, F_RDLCK, F_UNLCK
- * warning: always locks full file ( offset=0, whence=SEEK_SET, len=0 )
- * returns: result from fcntl call
- */
-{
-  struct flock lock;
+int lockit(int fd, int cmd, int type) {
+    /* purpose: fill in POSIX lock structure and attempt lock or unlock
+     * paramtr: fd (IN): which file descriptor to lock
+     *          cmd (IN): F_SETLK, F_GETLK, F_SETLKW
+     *          type (IN): F_WRLCK, F_RDLCK, F_UNLCK
+     * warning: always locks full file ( offset=0, whence=SEEK_SET, len=0 )
+     * returns: result from fcntl call
+     */
+    struct flock lock;
 
-  /* empty all -- even non-POSIX data fields */
-  memset( &lock, 0, sizeof(lock) );
-  lock.l_type   = type;
+    /* empty all -- even non-POSIX data fields */
+    memset(&lock, 0, sizeof(lock));
+    lock.l_type = type;
 
-  /* full file */
-  lock.l_whence = SEEK_SET;
-  lock.l_start  = 0;
-  lock.l_len    = 0;
+    /* full file */
+    lock.l_whence = SEEK_SET;
+    lock.l_start = 0;
+    lock.l_len = 0;
 
-  return ( fcntl( fd, cmd, &lock ) );
+    return fcntl(fd, cmd, &lock);
 }
 
-int
-mytrylock( int fd )
-/* purpose: Try to lock the file
- * paramtr: fd (IN): open file descriptor
- * returns: -1: fatal error while locking the file, file not locked
- *           0: all backoff attempts failed, file is not locked
- *           1: file is locked
- */
-{
-  int backoff = 50; /* milliseconds, increasing */
-  int retries = 10; /* 2.2 seconds total */
+int mytrylock(int fd) {
+    /* purpose: Try to lock the file
+     * paramtr: fd (IN): open file descriptor
+     * returns: -1: fatal error while locking the file, file not locked
+     *           0: all backoff attempts failed, file is not locked
+     *           1: file is locked
+     */
+    int backoff = 50; /* milliseconds, increasing */
+    int retries = 10; /* 2.2 seconds total */
 
-  while ( lockit( fd, F_SETLK, F_WRLCK ) == -1 ) {
-    if ( errno != EACCES && errno != EAGAIN ) return -1;
-    if ( --retries == 0 ) return 0;
-    backoff += 50;
-    poll( NULL, 0, backoff );
-  }
+    while (lockit(fd, F_SETLK, F_WRLCK) == -1) {
+        if (errno != EACCES && errno != EAGAIN) return -1;
+        if (--retries == 0) return 0;
+        backoff += 50;
+        poll(NULL, 0, backoff);
+    }
 
-  return 1;
+    return 1;
 }
 
-int
-nfs_sync( int fd, unsigned idle )
-/* purpose: tries to force NFS to update the given file descriptor
- * paramtr: fd (IN): descriptor of an open file
- *          idle (IN): how many milliseconds between lock and unlock
- * seelaso: DEFAULT_SYNC_IDLE as suggested argument for idle
- * returns: 0 is ok, -1 for failure
- */
-{
-  /* lock file */
-  if ( lockit( fd, F_SETLK, F_WRLCK ) == -1 )
-    return -1;
+int nfs_sync(int fd, unsigned idle) {
+    /* purpose: tries to force NFS to update the given file descriptor
+     * paramtr: fd (IN): descriptor of an open file
+     *          idle (IN): how many milliseconds between lock and unlock
+     * seelaso: DEFAULT_SYNC_IDLE as suggested argument for idle
+     * returns: 0 is ok, -1 for failure
+     */
+    /* lock file */
+    if (lockit(fd, F_SETLK, F_WRLCK) == -1) {
+        return -1;
+    }
 
-  /* wait $idle ms */
-  if ( idle > 0 ) poll( NULL, 0, idle );
+    /* wait $idle ms */
+    if (idle > 0) {
+        poll(NULL, 0, idle);
+    }
 
-  /* unlock file */
-  return lockit( fd, F_SETLK, F_UNLCK );
+    /* unlock file */
+    return lockit(fd, F_SETLK, F_UNLCK);
 }
 
-
-/*
- * old code
- */
-#if 0
-int
-nfs_sync( int fd, unsigned idle )
-/* purpose: tries to force NFS to update the given file descriptor
- * paramtr: fd (IN): descriptor of an open file
- *          idle (IN): how many milliseconds between lock and unlock
- * returns: 0 is ok, -1 for failure
- */
-{
-#ifndef LINUX
-  /* lock file */
-  if ( lockit( fd, F_SETLK, F_WRLCK ) == -1 )
-    return -1;
-
-  /* wait 100 ms */
-  if ( idle > 0 ) poll( NULL, 0, idle );
-
-  /* unlock file */
-  return lockit( fd, F_SETLK, F_UNLCK );
-#else /* is LINUX */
-  /* how I loathe eternally broken NFS locking on Linux */
-  char src[32];
-  char dst[4096];
-  struct utimbuf utb;
-
-  /* which FD to translate */
-  snprintf( src, sizeof(src), "/proc/%d/fd/%d", getpid(), fd );
-
-  /* read symlink information */
-  if ( readlink( src, dst, sizeof(dst) ) == -1 )
-    return -1;
-
-  /* attempt an utime */
-  utb.actime = utb.modtime = time(NULL);
-  return utime( dst, &utb ); 
-#endif /* LINUX */
-}
-
-#endif /* old code #if 0 */
--- src/tools/pegasus-kickstart/rwio.h
+++ src/tools/pegasus-kickstart/rwio.h
@@ -21,48 +21,9 @@
 #define DEFAULT_SYNC_IDLE 100
 #endif
 
-extern
-ssize_t
-writen( int fd, const char* buffer, ssize_t n, unsigned restart );
-/* purpose: write all n bytes in buffer, if possible at all
- * paramtr: fd (IN): filedescriptor open for writing
- *          buffer (IN): bytes to write (must be at least n byte long)
- *          n (IN): number of bytes to write 
- *          restart (IN): if true, try to restart write at max that often
- * returns: n, if everything was written, or
- *          [0..n-1], if some bytes were written, but then failed,
- *          < 0, if some error occurred.
- */
-
-extern
-int
-lockit( int fd, int cmd, int type );
-/* purpose: fill in POSIX lock structure and attempt lock or unlock
- * paramtr: fd (IN): which file descriptor to lock
- *          cmd (IN): F_SETLK, F_GETLK, F_SETLKW
- *          type (IN): F_WRLCK, F_RDLCK, F_UNLCK
- * warning: always locks full file ( offset=0, whence=SEEK_SET, len=0 )
- * returns: result from fcntl call
- */
-
-extern
-int
-mytrylock( int fd );
-/* purpose: Try to lock the file
- * paramtr: fd (IN): open file descriptor
- * returns: -1: fatal error while locking the file, file not locked
- *           0: all backoff attempts failed, file is not locked
- *           1: file is locked
- */
-
-extern
-int
-nfs_sync( int fd, unsigned idle );
-/* purpose: tries to force NFS to update the given file descriptor
- * paramtr: fd (IN): descriptor of an open file
- *          idle (IN): how many milliseconds between lock and unlock
- * seelaso: DEFAULT_SYNC_IDLE as suggested argument for idle
- * returns: 0 is ok, -1 for failure
- */
+extern ssize_t writen(int fd, const char* buffer, ssize_t n, unsigned restart);
+extern int lockit(int fd, int cmd, int type);
+extern int mytrylock(int fd);
+extern int nfs_sync(int fd, unsigned idle);
 
 #endif /* _RWIO_H */
