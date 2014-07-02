9506a95144ab04704493043ba08c10bafd6190b2
Gideon Juve <gideon@isi.edu>
2014-07-01 21:16:17 -0700

    kickstart: limitinfo formatting

--- src/tools/pegasus-kickstart/limitinfo.c
+++ src/tools/pegasus-kickstart/limitinfo.c
@@ -13,9 +13,6 @@
  * Southern California. All rights reserved.
  */
 #include <errno.h>
-#ifdef sun
-#include <memory.h>
-#endif
 #include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
@@ -28,205 +25,196 @@
 #endif
 #endif
 
-extern
-void
-initLimitInfo(LimitInfo* limits)
-/* purpose: initializes the data structure with current limits
- * paramtr: limits (OUT): initialized memory block
- */
-{
+extern void initLimitInfo(LimitInfo* limits) {
+    /* purpose: initializes the data structure with current limits
+     * paramtr: limits (OUT): initialized memory block
+     */
 #ifdef RLIMIT_NLIMITS
-  limits->size = RLIMIT_NLIMITS;
+    limits->size = RLIMIT_NLIMITS;
 #else
-  #error "Need to write a fragment to guesstimate max# of resources"
+#error "Need to write a fragment to guesstimate max# of resources"
 #endif
-  limits->limits = (SingleLimitInfo*) calloc(sizeof(SingleLimitInfo), limits->size);
+    limits->limits = (SingleLimitInfo*) calloc(sizeof(SingleLimitInfo), limits->size);
 }
 
-extern
-void
-updateLimitInfo(LimitInfo* limits)
-{
-  int i;
+extern void updateLimitInfo(LimitInfo* limits) {
+    int i;
 
-  if (limits == NULL || limits->limits == NULL) return;
+    if (limits == NULL || limits->limits == NULL) {
+        return;
+    }
 
-  for (i=0; i<limits->size; ++i) {
-    limits->limits[i].resource = i;
-    getrlimit(i, &(limits->limits[i].limit));
-    limits->limits[i].error = errno;
-  }
+    for (i=0; i<limits->size; ++i) {
+        limits->limits[i].resource = i;
+        getrlimit(i, &(limits->limits[i].limit));
+        limits->limits[i].error = errno;
+    }
 }
 
-extern
-void
-deleteLimitInfo(LimitInfo* limits)
-/* purpose: destructor
- * paramtr: limits (IO): valid LimitInfo structure to destroy.
- */
-{
-#ifdef EXTRA_DEBUG
-  debugmsg("# deleteLimitInfo(%p)\n", limits);
-#endif
+extern void deleteLimitInfo(LimitInfo* limits) {
+    /* purpose: destructor
+     * paramtr: limits (IO): valid LimitInfo structure to destroy.
+     */
 
-  if (limits != NULL) {
-    if (limits->limits != NULL) free((void*) limits->limits);
-    memset(limits, 0, sizeof(LimitInfo));
-  }
+    if (limits != NULL) {
+        if (limits->limits != NULL) {
+            free((void*) limits->limits);
+        }
+        memset(limits, 0, sizeof(LimitInfo));
+    }
 }
 
-static
-char*
-resource2string(char* buffer, size_t capacity, int resource)
-/* purpose: converts the resource integer into a string
- * paramtr: resource (IN): resource integer
- * returns: string with name of resource, or NULL if unknown
- */
-{
-  switch (resource) {
+static char* resource2string(char* buffer, size_t capacity, int resource) {
+    /* purpose: converts the resource integer into a string
+     * paramtr: resource (IN): resource integer
+     * returns: string with name of resource, or NULL if unknown
+     */
+    switch (resource) {
 #ifdef RLIMIT_CPU
-  case RLIMIT_CPU:
-    return strncpy(buffer, "RLIMIT_CPU", capacity);
+        case RLIMIT_CPU:
+            return strncpy(buffer, "RLIMIT_CPU", capacity);
 #endif
 
 #ifdef RLIMIT_FSIZE
-  case RLIMIT_FSIZE:
-    return strncpy(buffer, "RLIMIT_FSIZE", capacity);
+        case RLIMIT_FSIZE:
+            return strncpy(buffer, "RLIMIT_FSIZE", capacity);
 #endif
 
 #ifdef RLIMIT_DATA
-  case RLIMIT_DATA:
-    return strncpy(buffer, "RLIMIT_DATA", capacity);
+        case RLIMIT_DATA:
+            return strncpy(buffer, "RLIMIT_DATA", capacity);
 #endif
 
 #ifdef RLIMIT_STACK
-  case RLIMIT_STACK:
-    return strncpy(buffer, "RLIMIT_STACK", capacity);
+        case RLIMIT_STACK:
+            return strncpy(buffer, "RLIMIT_STACK", capacity);
 #endif
 
 #ifdef RLIMIT_NOFILE
-  case RLIMIT_NOFILE:
-    return strncpy(buffer, "RLIMIT_NOFILE", capacity);
+        case RLIMIT_NOFILE:
+            return strncpy(buffer, "RLIMIT_NOFILE", capacity);
 #endif
 
 #if defined(RLIMIT_OFILE) && ! defined(RLIMIT_NOFILE)
-  case RLIMIT_OFILE:
-    return strncpy(buffer, "RLIMIT_OFILE", capacity);
+        case RLIMIT_OFILE:
+            return strncpy(buffer, "RLIMIT_OFILE", capacity);
 #endif
 
 #ifdef RLIMIT_AS
-  case RLIMIT_AS:
-    return strncpy(buffer, "RLIMIT_AS", capacity);
+        case RLIMIT_AS:
+            return strncpy(buffer, "RLIMIT_AS", capacity);
 #endif
 
 #ifdef RLIMIT_NPROC
-  case RLIMIT_NPROC:
-    return strncpy(buffer, "RLIMIT_NPROC", capacity);
+        case RLIMIT_NPROC:
+            return strncpy(buffer, "RLIMIT_NPROC", capacity);
 #endif
 
 #ifdef RLIMIT_LOCKS
-  case RLIMIT_LOCKS:
-    return strncpy(buffer, "RLIMIT_LOCKS", capacity);
+        case RLIMIT_LOCKS:
+            return strncpy(buffer, "RLIMIT_LOCKS", capacity);
 #endif
 
 #ifdef RLIMIT_SIGPENDING
-  case RLIMIT_SIGPENDING:
-    return strncpy(buffer, "RLIMIT_SIGPENDING", capacity);
+        case RLIMIT_SIGPENDING:
+            return strncpy(buffer, "RLIMIT_SIGPENDING", capacity);
 #endif
 
 #ifdef RLIMIT_MSGQUEUE
-  case RLIMIT_MSGQUEUE:
-    return strncpy(buffer, "RLIMIT_MSGQUEUE", capacity);
+        case RLIMIT_MSGQUEUE:
+            return strncpy(buffer, "RLIMIT_MSGQUEUE", capacity);
 #endif
 
 #ifdef RLIMIT_NICE
-  case RLIMIT_NICE:
-    return strncpy(buffer, "RLIMIT_NICE", capacity);
+        case RLIMIT_NICE:
+            return strncpy(buffer, "RLIMIT_NICE", capacity);
 #endif
 
 #ifdef RLIMIT_RTPRIO
-  case RLIMIT_RTPRIO:
-    return strncpy(buffer, "RLIMIT_RTPRIO", capacity);
+        case RLIMIT_RTPRIO:
+            return strncpy(buffer, "RLIMIT_RTPRIO", capacity);
 #endif
 
 #ifdef RLIMIT_VMEM
 #if RLIMIT_AS != RLIMIT_VMEM
-  case RLIMIT_VMEM:
-    return strncpy(buffer, "RLIMIT_VMEM", capacity);
+        case RLIMIT_VMEM:
+            return strncpy(buffer, "RLIMIT_VMEM", capacity);
 #endif
 #endif
 
 #ifdef RLIMIT_CORE
-  case RLIMIT_CORE:
-    return strncpy(buffer, "RLIMIT_CORE", capacity);
+        case RLIMIT_CORE:
+            return strncpy(buffer, "RLIMIT_CORE", capacity);
 #endif
 
 #ifdef RLIMIT_MEMLOCK
-  case RLIMIT_MEMLOCK:
-    return strncpy(buffer, "RLIMIT_MEMLOCK", capacity);
+        case RLIMIT_MEMLOCK:
+            return strncpy(buffer, "RLIMIT_MEMLOCK", capacity);
 #endif
 
 #ifdef RLIMIT_RSS
 #if RLIMIT_AS != RLIMIT_RSS
-  case RLIMIT_RSS:
-    return strncpy(buffer, "RLIMIT_RSS", capacity);
+        case RLIMIT_RSS:
+            return strncpy(buffer, "RLIMIT_RSS", capacity);
 #endif
 #endif
 
-  default:
-    snprintf(buffer, capacity, "RESOURCE_%d", resource);
+        default:
+            snprintf(buffer, capacity, "RESOURCE_%d", resource);
+            return buffer;
+    }
+
+    /* never reached */
+    return NULL;
+}
+
+static char* value2string(char* buffer, size_t capacity, rlim_t value) {
+    if (value == RLIM_INFINITY) {
+        strncpy(buffer, "unlimited", capacity);
+    } else {
+        sizer(buffer, capacity, sizeof(rlim_t), &value);
+    }
     return buffer;
-  }
-
-  /* never reached */
-  return NULL;
 }
 
-static
-char*
-value2string(char* buffer, size_t capacity, rlim_t value)
-{
-  if (value == RLIM_INFINITY) strncpy(buffer, "unlimited", capacity);
-  else sizer(buffer, capacity, sizeof(rlim_t), &value);
-  return buffer;
+static int formatLimit(FILE *out, size_t indent, const SingleLimitInfo* l) {
+    char id[32], value[32];
+
+    if (l->error != 0) {
+        return 0;
+    }
+
+    if (resource2string(id, sizeof(id), l->resource) == NULL) {
+        return 0;
+    }
+
+    fprintf(out, "%*s<soft id=\"%s\">%s</soft>\n", (int)indent, "", id,
+            value2string(value, sizeof(value), l->limit.rlim_cur));
+    fprintf(out, "%*s<hard id=\"%s\">%s</hard>\n", (int)indent, "", id,
+            value2string(value, sizeof(value), l->limit.rlim_max));
+
+    return 0;
 }
 
-static
-int
-formatLimit(FILE *out, size_t indent, const SingleLimitInfo* l)
-{
-  char id[32], value[32];
+int printXMLLimitInfo(FILE *out, int indent, const LimitInfo* limits) {
+    /* purpose: format the rusage record into the given stream as XML.
+     * paramtr: out (IO): the stream
+     *          indent (IN): indentation level
+     *          limits (IN): observed resource limits
+     * returns: 0 if no error
+     */
+    int i;
 
-  if (l->error != 0) return 0;
+    /* sanity check */
+    if (limits == NULL || limits->limits == NULL) {
+        return 0;
+    }
 
-  if (resource2string(id, sizeof(id), l->resource) == NULL) return 0;
+    fprintf(out, "%*s<resource>\n", indent, "");
+    for (i=0; i<limits->size; ++i) {
+        formatLimit(out, indent+2, &limits->limits[i]);
+    }
+    fprintf(out, "%*s</resource>\n", indent, "");
 
-  fprintf(out, "%*s<soft id=\"%s\">%s</soft>\n", (int)indent, "", id,
-          value2string(value, sizeof(value), l->limit.rlim_cur));
-  fprintf(out, "%*s<hard id=\"%s\">%s</hard>\n", (int)indent, "", id,
-          value2string(value, sizeof(value), l->limit.rlim_max));
-
-  return 0;
-}
-
-int
-printXMLLimitInfo(FILE *out, int indent, const LimitInfo* limits)
-/* purpose: format the rusage record into the given stream as XML.
- * paramtr: out (IO): the stream
- *          indent (IN): indentation level
- *          limits (IN): observed resource limits
- * returns: 0 if no error
- */
-{
-  int i;
-
-  /* sanity check */
-  if (limits == NULL || limits->limits == NULL) return 0;
-
-  fprintf(out, "%*s<resource>\n", indent, "");
-  for (i=0; i<limits->size; ++i)
-    formatLimit(out, indent+2, &limits->limits[i]);
-  fprintf(out, "%*s</resource>\n", indent, "");
-
-  return 0;
+    return 0;
 }
