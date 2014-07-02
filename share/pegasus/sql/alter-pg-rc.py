6ff0b80a80d2b306d5cb8f60857bc988b7873533
Rafael Ferreira da Silva <silva@rafaelsilva.isi.edu>
2014-07-02 14:15:14 -0700

    PM-759: Optmization of the script

--- share/pegasus/sql/alter-pg-rc.py
+++ share/pegasus/sql/alter-pg-rc.py
@@ -10,17 +10,46 @@ try:
     con = psycopg2.connect(database, user);
     cur = con.cursor()
     
-    con.query("ALTER TABLE rc_lfn ADD COLUMN site VARCHAR(245)")
-    cur.execute("ALTER TABLE rc_lfn DROP INDEX sk_rc_lfn")
-    cur.execute("ALTER TABLE rc_lfn ADD CONSTRAINT sk_rc_lfn UNIQUE(lfn,pfn,site)")
+    print "Updating database schema..."
+    print "   Adding new column..."
+    cur.execute("ALTER TABLE rc_lfn ADD COLUMN site VARCHAR(245)")
     
-    cur.execute("SELECT id, value FROM rc_attr WHERE name = 'pool'")
-    rows = cur.fetchall()
-
-    for row in rows:
-        cur.execute("UPDATE rc_lfn SET site='%s' WHERE id=%d" % (row[1], row[0]))
-        cur.execute("DELETE FROM rc_attr WHERE id=%d AND name='pool' AND value='%s'" % (row[0],row[1]))        
+    print "   Removing index..."
+    cur.execute("ALTER TABLE rc_lfn DROP INDEX sk_rc_lfn")
+    
+    print "   Adding new constraint..."
+    cur.execute("ALTER TABLE rc_lfn ADD CONSTRAINT sk_rc_lfn UNIQUE(lfn,pfn,site)")
+    con.commit()
+    print "Data schema successfully updated."
+    print ""
+    
+    print "Migrating attribute data..."
+    cur = con.cursor()    
+    cur.execute("UPDATE rc_lfn l INNER JOIN rc_attr a ON (l.id=a.id AND a.name='pool') SET l.site=a.value")
+    con.commit()
+    print "Migration successfully completed."
+    print ""
         
+    print "Cleaning the database..."
+    cur = con.cursor()    
+    cur.execute("DELETE FROM rc_attr WHERE name='pool'")
+    con.commit()
+    print "Database successfully cleaned."
+    print ""    
+    
+    print "Validating the update process..."
+    cur = con.cursor()
+    cur.execute("SELECT COUNT(id) FROM rc_attr WHERE name='pool'")
+    attrs = int( cur.fetchone()[0] )
+    
+    if attrs > 0:
+        print "Error: attribute pool failed to be removed. There are still %d entries." % attrs
+        sys.exit(1)
+        
+    cur.execute("SELECT COUNT(id) FROM rc_lfn WHERE site IS NOT NULL")
+    updated = cur.fetchone()
+    print "Updated %d entries in the database." % updated
+
     con.commit()
 
 
