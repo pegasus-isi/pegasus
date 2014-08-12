ed4eff4792356a93dcb8d61f3fbcfd7da2ac250b
Gideon Juve <gideon@isi.edu>
2014-08-12 11:30:56 -0700

    Add package data from pegasus service to setup.py

--- lib/pegasus/python/setup.py
+++ lib/pegasus/python/setup.py
@@ -15,6 +15,19 @@ def readversion():
 def read(fname):
     return open(os.path.join(srcdir, fname)).read()
 
+def find_package_data(dirname):
+    def find_paths(dirname):
+        items = []
+        for fname in os.listdir(dirname):
+            path = os.path.join(dirname, fname)
+            if os.path.isdir(path):
+                items += find_paths(path)
+            elif not path.endswith(".py") and not path.endswith(".pyc"):
+                items.append(path)
+        return items
+    items = find_paths(dirname)
+    return [os.path.relpath(path, dirname) for path in items]
+
 setup(
     name = "pegasus-wms",
     version = readversion(),
@@ -35,7 +48,10 @@ setup(
         "Topic :: Utilities",
         "License :: OSI Approved :: Apache Software License",
     ],
-    packages = find_packages(exclude=["Pegasus.test"]),
+    packages = find_packages(exclude=["Pegasus.test","Pegasus.service.test"]),
+    package_data = {"Pegasus.service" : find_package_data("Pegasus/service") },
+    include_package_data = True,
+    zip_safe = False,
     test_suite = "Pegasus.test"
 )
 
