54bc4e97b53271897b301a626875abf20e039cb7
Karan Vahi <vahi@isi.edu>
2014-09-02 15:39:07 -0700

    PM-779 test case for job checkpoint files.

---
+++ test/core/032-black-chkpoint/README
@@ -0,0 +1,10 @@
+TEST DESCRIPTION
+- This test runs a blackdiamond workflow in nonsharedfs mode, to test
+the the planner support for job checkpoint files.
+
+
+PURPOSE
+- handling of checkpointing files.
+
+Associated JIRA Item
+- https://jira.isi.edu/browse/PM-779
---
+++ test/core/032-black-chkpoint/checkpoint_test.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+
+import os
+import sys
+import optparse
+
+checkpoint_file="test.checkpoint"
+max_value=4 # the max value on which the script exits with status 0
+
+def main():
+    """
+    A test executable for testing the job checkpointing support
+    in Pegasus Planner.
+    """
+
+     # Configure command line option parser
+    usage = '%s [options]' % sys.argv[0]
+    description = '%s [o]'    % sys.argv[0]
+
+    parser = optparse.OptionParser (usage=usage, description=description)
+
+    parser.add_option ("-o", "--output", action="store", type="str", dest="output_file",  help="output file")
+
+    #Parsing command-line options
+    (options, args) = parser.parse_args ()
+
+    if options.output_file is None:
+        parser.error( "Specify the -o option to specify the output file ")
+
+    last_value=0
+    if  os.path.isfile(checkpoint_file):
+        print "checkpoint  file found %s"  % checkpoint_file
+        print "reading checkpoint file %s" % checkpoint_file
+
+        #read in the last value from the checkpoint file
+        with open(checkpoint_file, "r") as CHECKPOINT_FILE:    
+            for my_line in CHECKPOINT_FILE:
+                #print my_line
+                contents = my_line.split()
+                last_value = int(contents[0])
+                
+    #increment the value and append the updated value
+    last_value = last_value + 1
+    with open(checkpoint_file,"a") as CHECKPOINT_FILE:
+        CHECKPOINT_FILE.write( "%d \n" % last_value  )
+        print "Written value %s to file %s " % ( last_value, checkpoint_file)
+    
+    if( last_value == max_value ):
+        #rename the test.checkpoint file to the output file
+        os.rename( checkpoint_file, options.output_file)
+        sys.exit( 0 )
+    else:
+        sys.exit( 1 )
+
+if __name__ == "__main__":
+    main()
+
+
