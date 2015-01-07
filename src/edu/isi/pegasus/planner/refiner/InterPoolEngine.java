481cc08383c1d37172b888183919b8a57681941d
Karan Vahi <vahi@isi.edu>
2015-01-07 11:04:40 -0800

    PM-810 data configuration is explicitly set per compute job

--- src/edu/isi/pegasus/planner/refiner/InterPoolEngine.java
+++ src/edu/isi/pegasus/planner/refiner/InterPoolEngine.java
@@ -304,11 +304,20 @@ public class InterPoolEngine extends Engine implements Refiner {
 
             mLogger.log("Job was mapped to " + job.jobName + " to site " + site,
                         LogManager.DEBUG_MESSAGE_LEVEL);
+            
+            
+            
             //incorporate the profiles and
             //do transformation selection
             //set the staging site for the job
             TransformationCatalogEntry entry = lookupTC(job);
             incorporateProfiles(job, entry );
+            
+            //PM-810 assign data configuration for the job if
+            //not already incorporated from profiles and properites
+            if( !job.vdsNS.containsKey( Pegasus.DATA_CONFIGURATION_KEY) ){
+                job.vdsNS.construct( Pegasus.DATA_CONFIGURATION_KEY, PegasusConfiguration.DEFAULT_DATA_CONFIGURATION_VALUE );
+            }
             job.setStagingSiteHandle( getStagingSite( job ) );
             handleExecutableFileTransfers(job, entry);
             
