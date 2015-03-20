533bc5d26a84c9c69502debb1665debf33e16ce3
Gideon Juve <gideon@isi.edu>
2015-03-19 20:52:50 -0700

    Move SABase and KeyInteger into pegasus_schema module

--- lib/pegasus/python/Pegasus/db/schema/pegasus_schema.py
+++ lib/pegasus/python/Pegasus/db/schema/pegasus_schema.py
@@ -10,8 +10,8 @@ from sqlalchemy import *
 from sqlalchemy import MetaData, orm
 from sqlalchemy.exc import *
 from sqlalchemy.orm import relation
+from sqlalchemy.dialects import postgresql, mysql, sqlite
 
-from Pegasus.db.schema import SABase, KeyInteger
 from Pegasus.db.modules.ensembles import Ensemble, EnsembleStates
 from Pegasus.db.modules.ensembles import EnsembleWorkflow, EnsembleWorkflowStates
 
@@ -29,6 +29,11 @@ table_keywords = {}
 table_keywords['mysql_charset'] = 'latin1'
 table_keywords['mysql_engine'] = 'InnoDB'
 
+KeyInteger = BigInteger()
+KeyInteger = KeyInteger.with_variant(postgresql.BIGINT(), 'postgresql')
+KeyInteger = KeyInteger.with_variant(mysql.BIGINT(), 'mysql')
+KeyInteger = KeyInteger.with_variant(sqlite.INTEGER(), 'sqlite')
+
 def initializeToPegasusDB(db):
     # This is only required if you want to query using the domain objects
     # instead of the session
@@ -36,6 +41,50 @@ def initializeToPegasusDB(db):
 
     # Create all the tables if they don't exist
     metadata.create_all(db)
+
+class SABase(object):
+    """
+    Base class for all the DB mapper objects.
+    """
+    def _commit(self, session, batch, merge=False):
+        if merge:
+            session.merge(self)
+        else:
+            session.add(self)
+        if batch:
+            return
+        session.flush()
+        session.commit()
+
+    def commit_to_db(self, session, batch=False):
+        """
+        Commit the DB object/row to the database.
+
+        @type   session: sqlalchemy.orm.scoping.ScopedSession object
+        @param  session: SQLAlch session to commit row to.
+        """ 
+        self._commit(session, batch)
+
+    def merge_to_db(self, session, batch=False):
+        """
+        Merge the DB object/row with an existing row in the database.
+
+        @type   session: sqlalchemy.orm.scoping.ScopedSession object
+        @param  session: SQLAlch session to merge row with.
+
+        Using this method pre-supposes that the developer has already
+        assigned any primary key information to the object before
+        calling.
+        """
+        self._commit(session, batch, merge=True)
+
+    def __repr__(self):
+        retval = '%s:\n' % self.__class__
+        for k,v in self.__dict__.items():
+            if k == '_sa_instance_state':
+                continue
+            retval += '  * %s : %s\n' % (k,v)
+        return retval
 
 # Empty classes that will be populated and mapped
 # to tables via the SQLAlch mapper.
