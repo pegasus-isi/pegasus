3495794eaeb23225a8f122f5425822f00a2cc8fd
Rafael Ferreira da Silva <rafsilva@isi.edu>
2015-04-08 21:28:33 -0700

    PM-776: replaced manual table creation by sqlalchemy metadata creation.

--- lib/pegasus/python/Pegasus/db/admin/versions/v2.py
+++ lib/pegasus/python/Pegasus/db/admin/versions/v2.py
@@ -6,6 +6,7 @@ import logging
 
 from Pegasus.db.admin.admin_loader import *
 from Pegasus.db.admin.versions.base_version import *
+from Pegasus.db.schema import *
 from sqlalchemy.exc import *
 
 log = logging.getLogger(__name__)
@@ -18,6 +19,21 @@ class Version(BaseVersion):
 
     def update(self, force=False):
         log.debug("Updating to version %s" % DB_VERSION)
+        try:
+            pg_ensemble.create(self.db.get_bind(), checkfirst=True)
+        except (OperationalError, ProgrammingError):
+            pass
+        except Exception, e:
+            self.db.rollback()
+            raise DBAdminError(e)
+        try:
+            pg_ensemble_workflow.create(self.db.get_bind(), checkfirst=True)
+        except (OperationalError, ProgrammingError):
+            pass
+        except Exception, e:
+            self.db.rollback()
+            raise DBAdminError(e)
+        
         try:
             self.db.execute("SELECT parent_wf_id FROM workflow")
             try:
@@ -78,42 +94,7 @@ class Version(BaseVersion):
         except Exception, e:
             self.db.rollback()
             raise DBAdminError(e)
-            
-        self.db.execute("CREATE TABLE IF NOT EXISTS ensemble ("
-            "id INTEGER PRIMARY KEY,"
-            "name VARCHAR(100) NOT NULL,"
-            "created DATETIME NOT NULL,"
-            "updated DATETIME NOT NULL,"
-            "state VARCHAR(6) NOT NULL,"
-            "max_running INTEGER NOT NULL,"
-            "max_planning INTEGER NOT NULL,"
-            "username VARCHAR(100) NOT NULL,"
-            "CHECK (state IN('ACTIVE', 'HELD', 'PAUSED'))"
-            ")")
-
-        self.db.execute("CREATE TABLE IF NOT EXISTS ensemble_workflow ("
-            "id INTEGER NOT NULL PRIMARY KEY,"
-            "name VARCHAR(100) NOT NULL,"
-            "basedir VARCHAR(512) NOT NULL,"
-            "created DATETIME NOT NULL,"
-            "updated DATETIME NOT NULL,"
-            "state VARCHAR(11) NOT NULL,"
-            "priority INTEGER NOT NULL,"
-            "wf_uuid VARCHAR(36),"
-            "submitdir VARCHAR(512),"
-            "ensemble_id INTEGER NOT NULL,"            
-            "CHECK (state IN('PLAN_FAILED', 'RUN_FAILED', 'FAILED', 'RUNNING', 'PLANNING', 'SUCCESSFUL', 'ABORTED', 'READY', 'QUEUED')),"
-            "FOREIGN KEY(ensemble_id) REFERENCES ensemble(id)"
-            ")")
-        try :
-            self.db.execute("CREATE INDEX UNIQUE_ENSEMBLE ON ensemble (username, name)")
-        except:
-            pass
-        try:
-            self.db.execute("CREATE INDEX UNIQUE_ENSEMBLE_WORKFLOW ON ensemble_workflow (ensemble_id, name)")
-        except:
-            pass
-            
+                    
         self.db.commit()           
 
         
