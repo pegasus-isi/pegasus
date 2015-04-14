5a71fdb24185ac6e89d06eba35c0736e4d2991b4
Gideon Juve <gideon@isi.edu>
2015-04-14 08:42:07 -0700

    Use LoggingCommand for pegasus-service

--- lib/pegasus/python/Pegasus/service/server.py
+++ lib/pegasus/python/Pegasus/service/server.py
@@ -3,7 +3,7 @@ import logging
 import random
 from OpenSSL import crypto, SSL
 
-from Pegasus.command import Command
+from Pegasus.command import LoggingCommand
 from Pegasus.service import app
 
 log = logging.getLogger(__name__)
@@ -40,38 +40,23 @@ def generate_self_signed_certificate(certfile, pkeyfile):
     open(certfile, "w").write(crypto.dump_certificate(crypto.FILETYPE_PEM, cert))
     open(pkeyfile, "w").write(crypto.dump_privatekey(crypto.FILETYPE_PEM, pkey))
 
-class ServerCommand(Command):
+class ServerCommand(LoggingCommand):
     usage = "%prog [options]"
     description = "Start Pegasus Service"
 
     def __init__(self):
-        Command.__init__(self)
+        LoggingCommand.__init__(self)
         self.parser.add_option("-H", "--host", dest="host", default=app.config["SERVER_HOST"],
                                help="Network interface on which to listen for requests")
         self.parser.add_option("-p", "--port", dest="port", type='int', default=app.config["SERVER_PORT"],
                                help="Request listener port")
         self.parser.add_option("-d", "--debug", action="store_true", dest="debug", default=None,
                                help="Enable debugging")
-        self.parser.add_option("-v", "--verbose", action="count", default=0, dest="verbose",
-                               help="Increase logging verbosity, repeatable")
 
     def run(self):
         if self.options.debug:
             app.config.update(DEBUG=True)
 
-        log_level = logging.DEBUG if self.options.debug else self.options.verbose
-
-        if log_level < 0:
-            log_level = logging.ERROR
-        elif log_level == 0:
-            log_level = logging.WARNING
-        elif log_level == 1:
-            log_level = logging.INFO
-        elif log_level > 1:
-            log_level = logging.DEBUG
-
-        logging.basicConfig(level=log_level)
-        logging.getLogger().setLevel(log_level)
 
         cert = app.config.get("CERTIFICATE", None)
         pkey = app.config.get("PRIVATE_KEY", None)
@@ -95,3 +80,4 @@ class ServerCommand(Command):
 
 def main():
     ServerCommand().main()
+
