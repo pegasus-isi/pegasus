a78a52eec0ca6c539554f3d2e7e1cbda614337ae
Rajiv Mayani <mayani@isi.edu>
2015-04-14 11:26:09 -0700

    Modify script to run dashboard cron.

--- contrib/dashboard-cron/dashboard.cron.sh
+++ contrib/dashboard-cron/dashboard.cron.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 pushd /localhome/bamboo/dashboard-cron > /dev/null
-DIR='pegasus-dashboard-cron'
+DIR='pegasus'
 
 PID_FILE=dashboard.pid
 LOG_FILE=dashboard.log
@@ -10,13 +10,13 @@ REV_FILE=dashboard.rev
 OLD_REV='r'
 
 if [ -e $REV_FILE ]; then
-	OLD_REV=`cat $REV_FILE`
+    OLD_REV=`cat $REV_FILE`
 fi
 
 if [[ ! -d $DIR ]]; then
     mkdir $DIR
-	pushd $DIR > /dev/null
-	git clone git://github.com/pegasus-isi/pegasus.git .
+    pushd $DIR > /dev/null
+    git clone git://github.com/pegasus-isi/pegasus.git .
 else
     pushd $DIR > /dev/null
     git pull
@@ -25,29 +25,28 @@ fi
 HEAD_REV=`git rev-list --reverse HEAD | tail -1`
 
 if [[ ! $OLD_REV = $HEAD_REV ]]; then
-	/ccg/software/ant/default/bin/ant clean dist
-	EC=$?
-	popd > /dev/null
+    /ccg/software/ant/default/bin/ant clean dist
+    EC=$?
+    popd > /dev/null
 
-	if [ $EC -eq 0 ]; then
-		echo $HEAD_REV > $REV_FILE
-	else
+    if [ $EC -eq 0 ]; then
+        echo $HEAD_REV > $REV_FILE
+    else
         popd > /dev/null
-		exit 0
-	fi
+        exit 0
+    fi
 
-	export PATH=`pwd`/`find $DIR/dist/ -mindepth 1 -maxdepth 1 -type d`/bin:$PATH
+    export PATH=`pwd`/`find $DIR/dist/ -mindepth 1 -maxdepth 1 -type d`/bin:$PATH
 
-	if [ -e $PID_FILE ]; then
-		kill `cat $PID_FILE`
-	fi
+    if [ -e $PID_FILE ]; then
+        kill `cat $PID_FILE`
+    fi
 
-	pegasus-dashboard -d &>> $LOG_FILE &
+    pegasus-service --host 0.0.0.0 --port 5000 -d &>> $LOG_FILE &
 
-	PID=$!
-	
-	echo $PID > $PID_FILE
+    PID=$!
+
+    echo $PID > $PID_FILE
 fi
 
 popd > /dev/null
-
