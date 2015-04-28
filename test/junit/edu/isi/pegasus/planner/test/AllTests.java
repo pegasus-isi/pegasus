e8603340705bab111bdd15b22d49dcf502237472
Karan Vahi <vahi@isi.edu>
2015-04-27 18:01:59 -0700

    PM-831 added the test case to test suite.

--- test/junit/edu/isi/pegasus/planner/test/AllTests.java
+++ test/junit/edu/isi/pegasus/planner/test/AllTests.java
@@ -26,6 +26,7 @@ import org.junit.runners.Suite.SuiteClasses;
     edu.isi.pegasus.planner.namespace.PegasusTest.class,
     edu.isi.pegasus.planner.catalog.replica.impl.RegexRCTest.class,
     edu.isi.pegasus.planner.catalog.replica.impl.JDBCRCTest.class,
+    edu.isi.pegasus.planner.catalog.transformation.impl.TextTest.class,
     edu.isi.pegasus.planner.cluster.RuntimeClusteringTest.class,
     edu.isi.pegasus.planner.code.generator.condor.style.GliteTest.class,
     edu.isi.pegasus.planner.transfer.mapper.FlatOutputMapperTest.class,
@@ -34,7 +35,7 @@ import org.junit.runners.Suite.SuiteClasses;
     edu.isi.pegasus.planner.transfer.mapper.FixedOutputMapperTest.class,
     edu.isi.pegasus.planner.refiner.DataReuseEngineTest.class,
     edu.isi.pegasus.common.util.GLiteEscapeTest.class,
-    edu.isi.pegasus.common.util.VariableExpanderTest.class
+    edu.isi.pegasus.common.util.VariableExpanderTest.class,
 })
 public class AllTests {
 }
