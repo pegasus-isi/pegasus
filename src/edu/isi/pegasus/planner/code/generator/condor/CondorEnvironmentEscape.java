b134b81abb285273b3bd61e2d566e774dff6eddb
Karan Vahi <vahi@isi.edu>
2015-05-28 11:19:37 -0700

    PM-934 added a new class CondorEnvironmentEscape that follows the new rules for escaping environment variables. The GLite class uses that now.

--- src/edu/isi/pegasus/planner/code/generator/condor/CondorEnvironmentEscape.java
+++ src/edu/isi/pegasus/planner/code/generator/condor/CondorEnvironmentEscape.java
@@ -15,141 +15,188 @@
  */
 
 
-package edu.isi.pegasus.common.util;
+package edu.isi.pegasus.planner.code.generator.condor;
+
+import edu.isi.pegasus.planner.namespace.ENV;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 /**
- * This class tries to define an interface to deal with quoting, escaping,
- * and the way back. The quoting algorithm is safe to only itself. Thus,
- *
+ * This class implements the rules defined by HTCondor for quoting environment
+ * values described here 
+ * http://research.cs.wisc.edu/htcondor/manual/v8.2/condor_submit.html 
+ * 
+ * The rules are described below
  * <pre>
- * unescape( escape( s ) ) === s
+ * The new syntax for specifying environment values:
+ * 
+ * Put double quote marks around the entire argument string. This distinguishes the new syntax from the old.
+ * The old syntax does not have double quote marks around it. 
+ * Any literal double quote marks within the string must be escaped by repeating the double quote mark.
+ * 1) Each environment entry has the form <name>=<value>
+ * 2) Use white space (space or tab characters) to separate environment entries.
+ * 3) To put any white space in an environment entry, surround the space and as
+ *    much of the surrounding entry as desired with single quote marks.
+ * 4) To insert a literal single quote mark, repeat the single quote mark anywhere 
+ *    inside of a section surrounded by single quote marks.
+ * Example:
+ *
+ *  environment = "one=1 two=""2"" three='spacey ''quoted'' value'"
+ * Produces the following environment entries:
+ *
+ * one=1
+ * two="2"
+ * three=spacey 'quoted' value
  * </pre>
  *
- * holds true, but
- *
- * <pre>
- * escape( unescape( s ) ) =?= s
- * </pre>
- *
- * does not necessarily hold.
- *
- * @author Gaurang Mehta
+ * 
  * @author Karan Vahi
- * @author Jens-S. Vöckler
- * @version $Revision$
+ * 
  */
-public class Escape
-{
-  /**
-   * Defines the character used to escape characters.
-   */
-  private char m_escape;
+public class CondorEnvironmentEscape {
 
-  /**
-   * Defines the set of characters that require escaping.
-   */
-  private String m_escapable;
+    /**
+     * Defines the set of characters that require escaping.
+     */
+    private char[] mEscapable = { '\'', '\"'};
+    
+    /**
+     * The characater to use if whitespace is detected
+     */
+    private static final char WHITESPACE_ENCLOSING_CHARACTER = '\'';
 
-  /**
-   * Defines the default quoting and escaping rules, escaping the
-   * apostrophe, double quote and backslash. The escape character
-   * is the backslash.
-   *
-   */
-  public Escape()
-  {
-    m_escapable = "\"'\\";
-    m_escape = '\\';
-  }
-
-  /**
-   * Constructs arbitrary escaping rules.
-   *
-   * @param escapable is the set of characters that require escaping
-   * @param escape is the escape character itself.
-   */
-  public Escape( String escapable, char escape )
-  {
-    m_escape = escape;
-    m_escapable = escapable;
-
-    // ensure that the escape character is part of the escapable char set
-    if ( escapable.indexOf(escape) == -1 ) m_escapable += m_escape;
-  }
-
-  /**
-   * Transforms a given string by escaping all characters inside the
-   * quotable characters set with the escape character. The escape
-   * character itself is also escaped.
-   *
-   * @param s is the string to escape.
-   * @return the quoted string
-   * @see #unescape( String )
-   */
-  public String escape( String s )
-  {
-    // sanity check
-    if ( s == null ) return null;
-
-    StringBuffer result = new StringBuffer( s.length() );
-    for ( int i=0; i<s.length(); ++i ) {
-      char ch = s.charAt(i);
-      if ( m_escapable.indexOf(ch) != -1 ) result.append( m_escape );
-      result.append(ch);
-    }
-    return result.toString();
-  }
-
-  /**
-   * Transforms a given string by unescaping all characters that
-   * are prefixed with the escape character.
-   *
-   * @param s is the string to remove escapes from.
-   * @return the quoted string
-   * @see #unescape( String )
-   */
-  public String unescape( String s )
-  {
-    // sanity check
-    if ( s == null ) return null;
-
-    StringBuffer result = new StringBuffer( s.length() );
-    int state = 0;
-    for ( int i=0; i<s.length(); ++i ) {
-      char ch = s.charAt(i);
-      if ( state == 0 ) {
-	// default state
-	if ( ch == m_escape ) state = 1;
-	else result.append(ch);
-      } else {
-	// "found escape" state
-	if ( m_escapable.indexOf(ch) == -1 ) result.append(m_escape);
-	result.append(ch);
-	state = 0;
-      }
+    /**
+     * Defines the default quoting and escaping rules, escaping the apostrophe,
+     * double quote and backslash. The escape character is the backslash.
+     *
+     */
+    public CondorEnvironmentEscape() {
     }
 
-    return result.toString();
-  }
-
-  /**
-   * Test program.
-   *
-   * @param args are command-line arguments
-   */
-  public static void main( String args[] )
-  {
-    Escape me = new Escape(); // defaults
-
-    for ( int i=0; i<args.length; ++i ) {
-      String e = me.escape(args[i]);
-      String u = me.unescape(args[i]);
-      System.out.println( "raw s  > " + args[i] );
-      System.out.println( "e(s)   > " + e );
-      System.out.println( "u(e(s))> " + me.unescape(e) );
-      System.out.println( "u(s)   > " + u );
-      System.out.println( "e(u(s))> " + me.escape(u) );
-      System.out.println();
+    /**
+     * Escapes all the profiles in the environment namespace per condor rules
+     * for escaping environment variables
+     * 
+     *  <pre>
+     * The new syntax for specifying environment values:
+     *
+     * Put double quote marks around the entire argument string. This distinguishes the new syntax from the old.
+     * The old syntax does not have double quote marks around it.
+     * Any literal double quote marks within the string must be escaped by repeating the double quote mark.
+     * 1) Each environment entry has the form <name>=<value>
+     * 2) Use white space (space or tab characters) to separate environment entries.
+     * 3) To put any white space in an environment entry, surround the space and as
+     *    much of the surrounding entry as desired with single quote marks.
+     * 4) To insert a literal single quote mark, repeat the single quote mark anywhere
+     *    inside of a section surrounded by single quote marks.
+     * Example:
+     *
+     *  environment = "one=1 two=""2"" three='spacey ''quoted'' value'"
+     * Produces the following environment entries:
+     *
+     * one=1
+     * two="2"
+     * three=spacey 'quoted' value
+     * </pre>
+     *
+     * @param env
+     * @return 
+     */
+    public String esacape( ENV env ){
+        StringBuilder result = new StringBuilder();
+        
+        //whole environment is enclosed in double quotes
+        result.append( "\"" );
+        for( Iterator it = env.getProfileKeyIterator(); it.hasNext(); ){
+            String key = (String) it.next();
+            result.append( key );
+            result.append(  "=" );
+            result.append( this.escape( (String)env.get(key)));
+            result.append( " " );
+        }
+        
+        //end enclosing double quotes
+        result.append( "\"" );
+        return result.toString();
     }
-  }
+
+    /**
+     * Transforms a given string by escaping all characters inside the quotable
+     * characters set with the escape character. The rules followed are described
+     * below
+     * <pre>
+     * 1) To put any white space in an environment entry, surround the space and as
+     *    much of the surrounding entry as desired with single quote marks.
+     * 2) To insert a literal single quote mark, repeat the single quote mark anywhere 
+     *    inside of a section surrounded by single quote marks.
+     * </pre>
+     * 
+     *
+     * @param s is the string to escape.
+     * @return the quoted string
+     */
+    public String escape(String s) {
+        // sanity check
+        if (s == null) {
+            return null;
+        }
+
+        //first check for existence of whitespace
+        boolean encloseInSingleQuote = false;
+        StringBuilder result = new StringBuilder(s.length());
+        if( s.contains( " " ) ){
+            //we have to enclose the whole string with single quote marks
+            encloseInSingleQuote = true;
+            result.append( WHITESPACE_ENCLOSING_CHARACTER );
+        }
+        
+        for (int i = 0; i < s.length(); ++i) {
+            char ch = s.charAt(i);
+            if ( isEscapable( ch ) ) {
+                //the escape characters are the ones 
+                //that are being escaped themselves! 
+                result.append( ch );
+            }
+            result.append(ch);
+        }
+        
+        if( encloseInSingleQuote ){
+            result.append( WHITESPACE_ENCLOSING_CHARACTER );
+        }
+
+        return result.toString();
+    }
+
+   
+
+    /**
+     * Test program.
+     *
+     * @param args are command-line arguments
+     */
+    public static void main(String args[]) {
+        CondorEnvironmentEscape me = new CondorEnvironmentEscape(); // defaults
+        Map<String,String> m = new LinkedHashMap();
+        m.put( "one", "1" );
+        m.put( "two", "\"2\"");
+        m.put( "three", "spacey 'quoted' value");
+        ENV env = new ENV( m );
+        
+        //should print out "one=1 two=""2"" three='spacey ''quoted'' value' "
+        String expected = "\"one=1 two=\"\"2\"\" three='spacey ''quoted'' value' \"";
+        String result  = me.esacape(env); 
+        System.out.println( "escaping successful " + result.equals(expected) );
+        System.out.println( result );
+    }
+
+    /**
+     * Returns whether the character is escapable or not
+     * 
+     * @return 
+     */
+    private boolean isEscapable(char ch) {
+       return( ch == mEscapable[0] || ch == mEscapable[1] ); 
+    }
+
 }
