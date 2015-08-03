5f80fff72f980f42668541393a11278fe8c7cd34
Karan Vahi <vahi@isi.edu>
2015-08-03 15:42:25 -0700

    PM-961 removed some commented code, and old comments that are not relevant

--- src/edu/isi/pegasus/planner/code/gridstart/Kickstart.java
+++ src/edu/isi/pegasus/planner/code/gridstart/Kickstart.java
@@ -573,20 +573,7 @@ public class Kickstart implements GridStart {
         //handle the -W option that asks kickstart to create and change
         //exectionSiteDirectory before launching an executable.
         if(job.vdsNS.getBooleanValue(Pegasus.CREATE_AND_CHANGE_DIR_KEY ) ){
-	    
-//            Commented to take account of submitting to condor pool
-//            directly or glide in nodes. However, does not work for
-//            standard universe jobs. Also made change in Kickstart
-//            to pick up only remote_initialdir Karan Nov 15,2005
-
-
-//           Removed for JIRA PM-543
-//                String directory = null;
-//                String key = getDirectoryKey( job );
-//                //we remove the key JIRA PM-80
-//                directory = (String)job.condorVariables.removeKey( key );
-
-            //pass the exectionSiteDirectory as an argument to kickstart
+	    //pass the exectionSiteDirectory as an argument to kickstart
             gridStartArgs.append(" -W ").append(directory).append(' ');
             
         }
@@ -594,35 +581,20 @@ public class Kickstart implements GridStart {
             //handle the -w option that asks kickstart to change
             //exectionSiteDirectory before launching an executable.
 
-
-//           Removed for JIRA PM-543
-//                String directory = null;
-//                String key = getDirectoryKey( job );\
-//                //we remove the key JIRA PM-80
-//                directory = (String)job.condorVariables.removeKey( key );
-
-                //pass the exectionSiteDirectory as an argument to kickstart
-                gridStartArgs.append(" -w ").append( directory ).append(' ');
+            gridStartArgs.append(" -w ").append( directory ).append(' ');
         }
         else{
             //set the directory key with the job
+            //for kickstart -w and -W it is not set
             if( requiresToSetDirectory( job ) ){
                 job.setDirectory( directory );
             }
         }
 
 
-            if(   job.vdsNS.getBooleanValue(Pegasus.TRANSFER_PROXY_KEY) ){
-
-//           Removed for JIRA PM-543
-//
-//                String key = getDirectoryKey( job );
-//                //just remove the remote_initialdir key
-//                //the constituentJob needs to be run in the exectionSiteDirectory
-//                //Condor or GRAM decides to run
-//                job.condorVariables.removeKey( key );
-                job.setDirectory( null );
-            }
+        if(   job.vdsNS.getBooleanValue(Pegasus.TRANSFER_PROXY_KEY) ){
+            job.setDirectory( null );
+        }
 
         //check if the constituentJob type indicates staging of executable
         //The -X functionality is handled by the setup jobs that
