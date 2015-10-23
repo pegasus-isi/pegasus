54a72c5eb688e249073d225f517712a1245ac207
Karan Vahi <vahi@isi.edu>
2015-10-22 18:57:22 -0700

    PM-995 updated the split example to remove the curl job

--- doc/tutorial/examples/split/generate_dax.py
+++ doc/tutorial/examples/split/generate_dax.py
@@ -10,22 +10,26 @@ if len(sys.argv) != 2:
         sys.exit(1)
 daxfile = sys.argv[1]
 
+# Create a abstract dag
 dax = ADAG("split")
 
 webpage = File("pegasus.html")
 
-curl = Job("curl")
-curl.addArguments("-o",webpage,"http://pegasus.isi.edu")
-curl.uses(webpage, link=Link.OUTPUT)
-dax.addJob(curl)
+# optional curl job to grab the input from Pegasus website
+#curl = Job("curl")
+#curl.addArguments("-o",webpage,"http://pegasus.isi.edu")
+#curl.uses(webpage, link=Link.OUTPUT)
+#dax.addJob(curl)
 
+# the split job that splits the webpage into smaller chunks
 split = Job("split")
 split.addArguments("-l","100","-a","1",webpage,"part.")
 split.uses(webpage, link=Link.INPUT)
 dax.addJob(split)
 
-dax.depends(split, curl)
+#dax.depends(split, curl)
 
+# we do a parmeter sweep on the first 4 chunks created
 for c in "abcd":
     part = File("part.%s" % c)
     split.uses(part, link=Link.OUTPUT)
@@ -38,7 +42,8 @@ for c in "abcd":
     wc.uses(part, link=Link.INPUT)
     wc.uses(count, link=Link.OUTPUT, transfer=True, register=False)
     dax.addJob(wc)
-
+    
+    #adding dependency
     dax.depends(wc, split)
 
 
