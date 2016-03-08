1080464b17c055628be80c22f63f5a7150d6cbc1
Rafael Ferreira da Silva <rafsilva@isi.edu>
2016-03-08 10:32:37 -0800

    PM-1036: added scripts for build

---
+++ lib/pegasus/r/Pegasus/setup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+DEST_DIR=$1
+LOG_FILE=build.log
+
+type R >/dev/null 2>&1 || { echo >&2 "R is not available. Skipping R compilation."; exit 0; }
+
+echo "Building R DAX API..."
+rm -rf $DEST_DIR/*.tar.gz
+
+R CMD build DAX &> $LOG_FILE
+
+if [ $? -ne 0 ]; then
+    cat $LOG_FILE >&2
+    exit 1
+fi
+
+mv *.tar.gz $DEST_DIR/
+rm -rf $LOG_FILE
+
+echo "R DAX API successfully built."
+
