33e70fcbd665a14087c2dd604a60cd8b3ac673ed
Rafael Ferreira da Silva <rafsilva@isi.edu>
2016-04-19 15:14:19 -0700

    PM-1043: fixed small bug and added documentation

--- src/edu/isi/pegasus/planner/refiner/cleanup/Constraint.java
+++ src/edu/isi/pegasus/planner/refiner/cleanup/Constraint.java
@@ -130,7 +130,8 @@ public class Constraint extends AbstractCleanupStrategy {
         availableSpacePerSite = new HashMap<String, Long>();
         maxAvailableSpacePerSite = new HashMap<String, Long>();
         deferStageins = (mProps.getProperty("pegasus.file.cleanup.constraint.deferstageins") != null);
-        // check if we want to keep the possibility of reading sizes from a CSV file
+        
+        // read file sizes from a CSV file
         String CSVName = System.getProperty("pegasus.file.cleanup.constraint.csv");
         if (CSVName != null) {
             try {
