53e741314c7213a3c273fe8ea4457e846638e0c4
Karan Vahi <vahi@isi.edu>
2016-07-29 11:37:46 -0700

    PM-1115 added the new functions for cyclic checks

--- src/edu/isi/pegasus/planner/classes/ADag.java
+++ src/edu/isi/pegasus/planner/classes/ADag.java
@@ -1071,7 +1071,23 @@ public class ADag extends Data implements Graph{
     }
 
     
-
+    /**
+     * Returns a boolean indicating whether a graph has cyclic edges or not.
+     * 
+     * @return boolean
+     */
+    public boolean hasCycles(){
+        return this.mGraphImplementor.hasCycles();
+    }
+    
+    /**
+     * Returns the detected cyclic edge if , hasCycles returns true
+     * 
+     * @return 
+     */
+    public NameValue getCyclicEdge(){
+        return this.mGraphImplementor.getCyclicEdge();
+    }
     
 
     
