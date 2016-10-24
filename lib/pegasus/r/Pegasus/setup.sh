042496fa9173db31963a8ea32f94931a27a59678
Mats Rynge <mats@rynge.net>
2016-10-24 12:59:43 -0700

    Make sure we don't skip R builds without explicit confirmation

--- lib/pegasus/r/Pegasus/setup.sh
+++ lib/pegasus/r/Pegasus/setup.sh
@@ -3,7 +3,15 @@
 DEST_DIR=$1
 LOG_FILE=build.log
 
-type R >/dev/null 2>&1 || { echo >&2 "R is not available. Skipping R compilation."; exit 0; }
+if ! type R >/dev/null 2>&1; then
+    if [ "x$PEGASUS_BUILD_R_MODULES" == "x0" ]; then
+        echo "R is not available, but user has overridden with PEGASUS_BUILD_R_MODULES=0. Skipping R compilation."
+        exit 0
+    else
+        echo "R is not available. Either install the R development packages, or disable this part of the build by setting PEGASUS_BUILD_R_MODULES=0 in your environment before executing ant."
+        exit 1
+    fi
+fi
 
 echo "Building R DAX API..."
 rm -rf $DEST_DIR/*.tar.gz
