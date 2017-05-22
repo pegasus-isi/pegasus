32be8159a9a9c07d66d5f19f223fb3bc4e13002c
Mats Rynge <rynge@isi.edu>
2017-05-22 12:39:49 -0700

    PM-1188: Fall back on sha256sum if openssl if not aavailable

--- src/tools/pegasus-kickstart/test/testintegrity.sh
+++ src/tools/pegasus-kickstart/test/testintegrity.sh
@@ -7,7 +7,6 @@ export PATH=$PWD/../../../../bin:$PATH
 
 if ! (cat test.out | grep 'checksum type="sha256" value="c8ea869cd618ff99b394f5ce2962689adad3e32fa6d2924938f3837fdee08fe6"') >/dev/null 2>&1; then
     echo "Missing/incorrect checksum in kickstart record!"
-    cat test.out test.err
     exit 1
 fi
 
