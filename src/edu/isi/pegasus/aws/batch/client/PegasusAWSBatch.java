698e9eff09357327540813379ac5f8c2aef49f87
Karan Vahi <vahi@isi.edu>
2017-11-21 16:14:51 -0800

    PM-1229 propagate the logging option to the Synch client.

--- src/edu/isi/pegasus/aws/batch/client/PegasusAWSBatch.java
+++ src/edu/isi/pegasus/aws/batch/client/PegasusAWSBatch.java
@@ -1,8 +1,19 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ *  Copyright 2007-2017 University Of Southern California
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
  */
+
 package edu.isi.pegasus.aws.batch.client;
 
 /**
@@ -152,10 +163,11 @@ public class PegasusAWSBatch {
      * @param options 
      */
     protected void executeCommand( OptionSet options ) {
-        
+        Level logLevel = Level.INFO;
         if( options.has( "log-level")){
-            mLogger.setLevel((Level) options.valueOf( "log-level"));
+            logLevel = (Level) options.valueOf( "log-level");
         }
+        mLogger.setLevel(logLevel);
         mLogger.debug( "Executing command");
         
         if( options.has( "help") ){
@@ -212,7 +224,7 @@ public class PegasusAWSBatch {
         
         Synch sc = new Synch();
         try {
-            sc.initialze( props, jsonMap );
+            sc.initialze( props, logLevel, jsonMap );
         } catch (IOException ex) {
             mLogger.error(ex, ex);
         }
--- src/edu/isi/pegasus/aws/batch/impl/Synch.java
+++ src/edu/isi/pegasus/aws/batch/impl/Synch.java
@@ -1,8 +1,19 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ *  Copyright 2007-2017 University Of Southern California
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
  */
+
 package edu.isi.pegasus.aws.batch.impl;
 
 
@@ -34,6 +45,7 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
+import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 import org.apache.log4j.Priority;
         
@@ -119,9 +131,18 @@ public class Synch {
         
     }
     
-    public void initialze( Properties properties, EnumMap<JSON_FILE_TYPE, File> jsonFileMap ) throws IOException{
+    /**
+     * Initialize the log.
+     * 
+     * @param properties properties with pegasus prefix stripped.
+     * @param level
+     * @param jsonFileMap
+     * @throws IOException
+     */
+    public void initialze( Properties properties, Level level, EnumMap<JSON_FILE_TYPE, File> jsonFileMap ) throws IOException{
         //"405596411149";
         mLogger = Logger.getLogger( Synch.class.getName() ); 
+        mLogger.setLevel(level);
         mAWSAccountID = getProperty( properties, Synch.AWS_PROPERTY_PREFIX , "account" );
         mAWSRegion = Region.of( getProperty( properties, Synch.AWS_PROPERTY_PREFIX, "region") );//"us-west-2" 
         mPrefix = getProperty( properties, Synch.AWS_BATCH_PROPERTY_PREFIX,  "prefix" );
@@ -855,7 +876,7 @@ public class Synch {
         props.setProperty( Synch.AWS_PROPERTY_PREFIX + ".account", "405596411149" );
         props.setProperty( Synch.AWS_BATCH_PROPERTY_PREFIX + ".prefix", "karan-batch-synch-test-1" );
         EnumMap<Synch.JSON_FILE_TYPE,File> jsonMap = new EnumMap<Synch.JSON_FILE_TYPE,File>( Synch.JSON_FILE_TYPE.class);
-        sc.initialze( props, jsonMap );
+        sc.initialze( props, Level.DEBUG, jsonMap );
         
         
         sc.monitor();
