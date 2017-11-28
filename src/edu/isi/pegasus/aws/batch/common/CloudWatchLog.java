e1b26c2d59a9a939d7b3f6d935daa3992c8f5003
Karan Vahi <vahi@isi.edu>
2017-11-28 15:32:57 -0800

    PM-1229 fixed formatting

--- src/edu/isi/pegasus/aws/batch/common/CloudWatchLog.java
+++ src/edu/isi/pegasus/aws/batch/common/CloudWatchLog.java
@@ -33,9 +33,12 @@ import software.amazon.awssdk.services.batch.model.DescribeJobsRequest;
 import software.amazon.awssdk.services.batch.model.DescribeJobsResponse;
 import software.amazon.awssdk.services.batch.model.JobDetail;
 import software.amazon.awssdk.services.cloudwatchlogs.CloudWatchLogsClient;
+
 import software.amazon.awssdk.services.cloudwatchlogs.model.GetLogEventsRequest;
 import software.amazon.awssdk.services.cloudwatchlogs.model.GetLogEventsResponse;
 import software.amazon.awssdk.services.cloudwatchlogs.model.OutputLogEvent;
+import software.amazon.awssdk.services.cloudwatchlogs.model.DeleteLogStreamRequest;
+import software.amazon.awssdk.services.cloudwatchlogs.model.DeleteLogStreamResponse;
 
 /**
  * A class to retrieve the cloud watch logs
@@ -45,142 +48,145 @@ import software.amazon.awssdk.services.cloudwatchlogs.model.OutputLogEvent;
 public class CloudWatchLog {
 
     private Logger mLogger;
-    
+
     private BatchClient mBatchClient;
-    
+
     private CloudWatchLogsClient mCWL;
-    
+
     private String mLogGroup;
-    
+
     /**
      * The default constructor
      */
-    public CloudWatchLog(){
-        
+    public CloudWatchLog() {
+
     }
-    
+
     /**
      * Initialize the log.
-     * 
+     *
      * @param awsRegion the aws region
-     * @param logLevel  the logging level
-     * @param logGroup  the cloud watch log group
+     * @param logLevel the logging level
+     * @param logGroup the cloud watch log group
      */
-    public void initialze( Region awsRegion, Level logLevel , String logGroup )  {
+    public void initialze(Region awsRegion, Level logLevel, String logGroup) {
         //"405596411149";
-        mLogger = Logger.getLogger( Synch.class.getName() ); 
+        mLogger = Logger.getLogger(Synch.class.getName());
         mLogger.setLevel(logLevel);
         mLogGroup = logGroup;
-        mBatchClient = BatchClient.builder().region( awsRegion ).build();
-        mCWL = CloudWatchLogsClient.builder().region( awsRegion ).build();
+        mBatchClient = BatchClient.builder().region(awsRegion).build();
+        mCWL = CloudWatchLogsClient.builder().region(awsRegion).build();
     }
-    
+
     /**
-    * Retrieves a cloud watch log for an AWS Job
-    * 
-    * @param awsJobID    the AWS job ID for the job
-    * 
-    *  @return the file to which it is retrieved
-    */
-   public File retrieve ( String awsJobID ){
-       DescribeJobsRequest jobsRequest = DescribeJobsRequest.builder().
-               jobs(awsJobID).
-               build();
-       DescribeJobsResponse jobsResponse = mBatchClient.describeJobs(jobsRequest);
-       for (JobDetail jobDetail : jobsResponse.jobs()) {
-           try {
-               Tuple<String, String> log = determineLog(jobDetail);
-               return this.retrieve(jobDetail.jobName(), log.getKey(), log.getValue());
-           } catch (Exception e) {
-               mLogger.error( "Error while retrieving cloud watch log for job " + awsJobID, e );
-           }
-       }
-       return null;
-   }
-   
-   /**
-    * Retrieves a cloud watch log for an AWS Job
-    * 
-    * @param jobName        the job name
-    * @param logGroup       the cloud watch log group
-    * @param streamName     the stream name
-    * 
-    * @return the file to which it is retrieved
-    */
-   public File retrieve( String jobName, String logGroup, String streamName ){
-        mLogger.info( "Retrieving log for " + jobName + " for log group " + logGroup + " with stream name " + streamName );
-        
+     * Retrieves a cloud watch log for an AWS Job
+     *
+     * @param awsJobID the AWS job ID for the job
+     *
+     * @return the file to which it is retrieved
+     */
+    public File retrieve(String awsJobID) {
+        DescribeJobsRequest jobsRequest = DescribeJobsRequest.builder().
+                jobs(awsJobID).
+                build();
+        DescribeJobsResponse jobsResponse = mBatchClient.describeJobs(jobsRequest);
+        for (JobDetail jobDetail : jobsResponse.jobs()) {
+            try {
+                Tuple<String, String> log = determineLog(jobDetail);
+                return this.retrieve(jobDetail.jobName(), log.getKey(), log.getValue());
+            } catch (Exception e) {
+                mLogger.error("Error while retrieving cloud watch log for job " + awsJobID, e);
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Retrieves a cloud watch log for an AWS Job
+     *
+     * @param jobName the job name
+     * @param logGroup the cloud watch log group
+     * @param streamName the stream name
+     *
+     * @return the file to which it is retrieved
+     */
+    public File retrieve(String jobName, String logGroup, String streamName) {
+        mLogger.info("Retrieving log for " + jobName + " for log group " + logGroup + " with stream name " + streamName);
+
         GetLogEventsRequest gle = GetLogEventsRequest.builder().
-                                                             logGroupName(logGroup).
-                                                             logStreamName( streamName ).
-                                                             startFromHead​( true ).
-                                                     build();
+                logGroupName(logGroup).
+                logStreamName(streamName).
+                startFromHead​(true).
+                build();
         boolean done = false;
         String previousToken = null;
         PrintWriter pw = null;
         File f = null;
-        try{
-            f = new File( jobName + ".out" );
-            pw = new PrintWriter( new BufferedWriter( new FileWriter(f)));
-            mLogger.debug( "Will write out log to " + f.getAbsolutePath() );
-            while(!done) {
-                 GetLogEventsResponse response = mCWL.getLogEvents(gle);
-                 for( OutputLogEvent event: response.events()){
-                     mLogger.debug(  "Retrieved event " +  event.message() );
-                     pw.println( event.message() );
-                 }
-                 String nextToken = response.nextForwardToken();
+        try {
+            f = new File(jobName + ".out");
+            pw = new PrintWriter(new BufferedWriter(new FileWriter(f)));
+            mLogger.debug("Will write out log to " + f.getAbsolutePath());
+            while (!done) {
+                GetLogEventsResponse response = mCWL.getLogEvents(gle);
+                for (OutputLogEvent event : response.events()) {
+                    mLogger.debug("Retrieved event " + event.message());
+                    pw.println(event.message());
+                }
+                String nextToken = response.nextForwardToken();
 
-                 if(  nextToken == null || nextToken.equals( previousToken) ) {
-                     //not clear if that is the right way to exit with token matching
-                     done = true;
-                 }
-                 else{
-                     gle = GetLogEventsRequest.builder().
-                                                     logGroupName( logGroup ).
-                                                     logStreamName( streamName ).
-                                                     startFromHead​( true ).
-                                                     nextToken( nextToken  ).
-                                             build();
-                 }
-                 previousToken = nextToken;
+                if (nextToken == null || nextToken.equals(previousToken)) {
+                    //not clear if that is the right way to exit with token matching
+                    done = true;
+                } else {
+                    gle = GetLogEventsRequest.builder().
+                            logGroupName(logGroup).
+                            logStreamName(streamName).
+                            startFromHead​(true).
+                            nextToken(nextToken).
+                            build();
+                }
+                previousToken = nextToken;
             }
             pw.flush();
-       }
-       catch(IOException ex){
-           mLogger.log(Priority.ERROR, ex);
-       }
-       finally{
-           if( pw != null ){
-               pw.close();
-           }
-       }
-       return f;                                                   
+        } catch (IOException ex) {
+            mLogger.log(Priority.ERROR, ex);
+        } finally {
+            if (pw != null) {
+                pw.close();
+            }
+        }
+        return f;
     }
-   
-   private Tuple<String,String> determineLog(JobDetail jobDetail) {
-       //go through the attemps and get last attempt
-       AttemptDetail  detail = null;
-       StringBuilder logStreamName = new StringBuilder();//karan-batch-synch-test-job-definition/default/e6b3eb37-46d3-4aa5-9208-e80eec481550
-       mLogger.debug( "determining cloud watch log ");
-       for( Iterator<AttemptDetail> it = jobDetail.attempts().iterator(); it.hasNext(); ){
-           detail = it.next();
-       }
-       if( detail != null ){
-           String taskARN = detail.container().taskArn();
-           String jobDefinition = jobDetail.jobDefinition();
-           
-           mLogger.debug( "log group: " + mLogGroup + " job defn: " + jobDefinition + " task arn: " + taskARN );
-           
-           String taskARNID = taskARN.substring( taskARN.lastIndexOf( "/" ) + 1 );
-           String jdBase = jobDefinition.substring(
-                                                    jobDefinition.indexOf( ":job-definition/" ) + ":job-definition/".length(),
-                                                    jobDefinition.lastIndexOf( ":"));
-           logStreamName.append( jdBase ).append( "/default/" ).append( taskARNID );
-       }
-       mLogger.info( "Log Stream name is " + logStreamName );
-       return new Tuple( mLogGroup, logStreamName.toString() );
-       
+
+    /**
+     * Returns a tuple and the log group name
+     *
+     * @param jobDetail
+     * @return
+     */
+    private Tuple<String, String> determineLog(JobDetail jobDetail) {
+        //go through the attemps and get last attempt
+        AttemptDetail detail = null;
+        StringBuilder logStreamName = new StringBuilder();//karan-batch-synch-test-job-definition/default/e6b3eb37-46d3-4aa5-9208-e80eec481550
+        mLogger.debug("determining cloud watch log ");
+        for (Iterator<AttemptDetail> it = jobDetail.attempts().iterator(); it.hasNext();) {
+            detail = it.next();
+        }
+        if (detail != null) {
+            String taskARN = detail.container().taskArn();
+            String jobDefinition = jobDetail.jobDefinition();
+
+            mLogger.debug("log group: " + mLogGroup + " job defn: " + jobDefinition + " task arn: " + taskARN);
+
+            String taskARNID = taskARN.substring(taskARN.lastIndexOf("/") + 1);
+            String jdBase = jobDefinition.substring(
+                    jobDefinition.indexOf(":job-definition/") + ":job-definition/".length(),
+                    jobDefinition.lastIndexOf(":"));
+            logStreamName.append(jdBase).append("/default/").append(taskARNID);
+        }
+        mLogger.info("Log Stream name is " + logStreamName);
+        return new Tuple(mLogGroup, logStreamName.toString());
+
     }
-    
+
 }
