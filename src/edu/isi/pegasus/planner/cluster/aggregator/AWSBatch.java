df40c2b5060d9e2e35bd9a6edc82bd2215111916
Karan Vahi <vahi@isi.edu>
2018-01-12 11:16:57 -0800

    PM-1231 we wants the logs to be merged into a single file for pegasus monitoring

--- src/edu/isi/pegasus/planner/cluster/aggregator/AWSBatch.java
+++ src/edu/isi/pegasus/planner/cluster/aggregator/AWSBatch.java
@@ -307,7 +307,11 @@ public class AWSBatch extends Abstract {
         args.append( "--log-level" ).append( " " ).append( "debug" ).append( " " );
         
         //we log to a file based on jobname
+        //this is where the stdout/stderr of pegasus-aws-batch client goes
         args.append( "--log-file" ).append( " " ).append( jobSubmitDir).append( File.separator).append( job.getID() + ".log" ).append( " " );
+        
+        //we wants the logs to be merged into a single file for pegasus monitoring
+        args.append( "--merge-logs" ).append( " " ).append( jobSubmitDir).append( File.separator).append( job.getID() ).append( " " );
         
         //the job name is the prefix for the time being
         args.append( "--prefix" ).append( " " ).append( job.getID() ).append( " " );
