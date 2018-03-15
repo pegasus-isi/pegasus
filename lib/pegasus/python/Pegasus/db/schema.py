83108f10ba45a2b91d419be792eccc070d2b70d7
Rajiv Mayani <mayani@isi.edu>
2018-03-14 17:36:36 -0700

    Fix bug in DB code  where exceptions are rethrown incorrectly

--- lib/pegasus/python/Pegasus/db/schema.py
+++ lib/pegasus/python/Pegasus/db/schema.py
@@ -76,16 +76,16 @@ def check_table_exists(engine, table):
     try:
         engine.execute(table.select().limit(1))
         return True
-    
+
     except OperationalError as e:
         if "no such table" in str(e).lower() or "unknown" in str(e).lower() \
           or "no such column" in str(e).lower():
             return False
-        raise OperationalError(e)
+        raise
     except ProgrammingError as e:
         if "doesn't exist" in str(e).lower():
             return False
-        raise ProgrammingError(e)
+        raise
 # --------------------------------------------------------------------
 
 
