955d89b8f0894496c3c68b83d42e1a9fcea0cb90
Karan Vahi <vahi@isi.edu>
2018-07-02 18:29:27 -0700

    PM-1280 made it consistent with other tutorial examples

--- share/pegasus/init/population/daxgen.py
+++ share/pegasus/init/population/daxgen.py
@@ -10,7 +10,18 @@ from Pegasus.DAX3 import *
 
 top_dir = os.getcwd()
 
+# The name of the DAX file is the first argument
+if len(sys.argv) != 2:
+        sys.stderr.write("Usage: %s DAXFILE\n" % (sys.argv[0]))
+        sys.exit(1)
+daxfile = sys.argv[1]
+USER = pwd.getpwuid(os.getuid())[0]
+
 dax = ADAG("kimetrica")
+
+# Add some workflow-level metadata
+dax.metadata("creator", "%s@%s" % (USER, os.uname()[1]))
+dax.metadata("created", time.ctime())
 
 # common library
 geospatial = File('geospatial.py')
@@ -105,6 +116,10 @@ for year in range(2017, 2020):
     animate.uses(png, link=Link.INPUT)
     dax.depends(parent=j3, child=animate)
 
-# Write the DAX to stdout
-dax.writeXML(sys.stdout)
+# write out the dax
+f = open(daxfile, "w")
+dax.writeXML(f)
+f.close()
+print "Generated dax %s" %daxfile
+
 
