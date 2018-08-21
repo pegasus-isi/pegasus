51bdf910b9992ef26b38e2ead3bdb0d40f84d02e
Karan Vahi <vahi@isi.edu>
2018-08-21 10:17:03 -0700

    PM-1293 for stage-in jobs we also need to check whether integrity checking is turned on or not.

--- src/edu/isi/pegasus/planner/refiner/TransferEngine.java
+++ src/edu/isi/pegasus/planner/refiner/TransferEngine.java
@@ -1413,7 +1413,7 @@ public class TransferEngine extends Engine {
             
             //PM-1250 if no checksum exists then set pegasus-transfer
             //to generate checksum. Later on a dial might be required here
-            if( !pf.hasRCCheckSum() ){
+            if( this.mDoIntegrityChecking && !pf.hasRCCheckSum() ){
                 ft.setChecksumComputedInWF( true );
                 pf.setChecksumComputedInWF( true );
             }
