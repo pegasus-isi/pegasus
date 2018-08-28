2868cd2fdfc6e3a08331c222753e83e48e218f56
Karan Vahi <vahi@isi.edu>
2018-08-28 16:15:04 -0700

    PM-1289 by default if an event filter is not specified , we only now send stampede.job_inst.tag and stampede.inv.end events

--- lib/pegasus/python/Pegasus/monitoring/event_output.py
+++ lib/pegasus/python/Pegasus/monitoring/event_output.py
@@ -298,7 +298,8 @@ class AMQPEventSink(EventSink):
 
         if events is None:
             # add pre-configured specific events
-            event_regexes.add(re.compile(STAMPEDE_NS + "xwf.*"))
+            event_regexes.add(re.compile(STAMPEDE_NS + "job_inst.tag"))
+            event_regexes.add(re.compile(STAMPEDE_NS + "job_inv.end"))
         else:
             for exp in events.split(","):
                 if exp == "*":
