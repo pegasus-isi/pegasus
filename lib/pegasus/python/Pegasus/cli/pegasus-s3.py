f96cf26f6702b303feef7741dc3a10214b615868
Mats Rynge <mats@rynge.net>
2019-02-08 15:42:15 -0800

    PM-1239: Moved to generated sh scripts instead of symlinks. The latter did not work due to HTCondor renaming executables on stage-in.

--- lib/pegasus/python/Pegasus/cli/pegasus-s3.py
+++ lib/pegasus/python/Pegasus/cli/pegasus-s3.py
@@ -3,13 +3,16 @@ import sys
 import os
 import subprocess
 
-bin_dir = os.path.normpath(os.path.join(os.path.dirname(sys.argv[0])))
-pegasus_config = os.path.abspath(os.path.join(bin_dir, "pegasus-config"))
-lib_dir = subprocess.Popen([pegasus_config,"--noeoln","--python"], stdout=subprocess.PIPE, shell=False).communicate()[0]
-lib_ext_dir = subprocess.Popen([pegasus_config,"--noeoln","--python-externals"], stdout=subprocess.PIPE, shell=False).communicate()[0]
+# only expand PYTHONPATH if it not already set
+if 'PEGASUS_PYTHONPATH_SET' not in os.environ:
+    bin_dir = os.path.normpath(os.path.join(os.path.dirname(sys.argv[0])))
+    pegasus_config = os.path.abspath(os.path.join(bin_dir, "pegasus-config"))
+    lib_dir = subprocess.Popen([pegasus_config,"--noeoln","--python"], stdout=subprocess.PIPE, shell=False).communicate()[0]
+    lib_ext_dir = subprocess.Popen([pegasus_config,"--noeoln","--python-externals"], stdout=subprocess.PIPE, shell=False).communicate()[0]
 
-sys.path.insert(0, lib_ext_dir)
-sys.path.insert(0, lib_dir)
+    sys.path.insert(0, lib_ext_dir)
+    sys.path.insert(0, lib_dir)
+
 
 from Pegasus import s3
 
