c35950e7c50a8ee3c994b88d812e9b6e33d8181b
Rafael Ferreira da Silva <rafsilva@isi.edu>
2019-10-23 13:48:08 -0700

    updating SQLAlchemy

--- lib/pegasus/python/Pegasus/db/admin/admin_loader.py
+++ lib/pegasus/python/Pegasus/db/admin/admin_loader.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 #
-#  Copyright 2017-2018 University Of Southern California
+#  Copyright 2017-2019 University Of Southern California
 #
 #  Licensed under the Apache License, Version 2.0 (the "License");
 #  you may not use this file except in compliance with the License.
