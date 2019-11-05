32ad10a65e233f313f8ec938fb23bf1e1494b10f
Mats Rynge <mats@rynge.net>
2019-11-05 14:39:42 -0800

    Allow for python in non-system locations

---
+++ lib/pegasus/python/Pegasus/cli/startup-validation.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+import sys
+
+if not sys.version_info >= (3, 5):
+    sys.stderr.write('Pegasus requires Python 3.5 or above\n')
+    sys.exit(1)
+
+try:
+    import yaml
+except:
+    sys.stderr.write('Pegasus requires the Python YAML module to be installed\n')
+    sys.exit(1)
+
+try:
+    import OpenSSL
+except:
+    sys.stderr.write('Pegasus requires the Python PyOpenSSL module to be installed\n')
+    sys.exit(1)
+
