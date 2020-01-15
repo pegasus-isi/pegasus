e30d300f47bb8a5921aa0642de70c71fbf348051
Ryan Tanaka <tanaka@isi.edu>
2020-01-14 16:33:11 -0800

    PM-1414 added tests to validate our schemas against the jsonschema draft 7 meta-schema

---
+++ lib/pegasus/python/Pegasus/test/dax4/test_schemas.py
@@ -0,0 +1,24 @@
+import pytest
+from jsonschema import Draft7Validator
+
+# Validating the actual format of the schemas against the Draft 7 meta-schema
+
+
+@pytest.mark.parametrize(
+    "schema_file",
+    [
+        ("common.json"),
+        ("dax-5.0.json"),
+        ("rc-5.0.json"),
+        ("tc-5.0.json"),
+        ("sc-5.0.json"),
+    ],
+)
+def test_schema(schema_file, convert_yaml_schemas_to_json, load_schema):
+    schema = load_schema(schema_file)
+    Draft7Validator.check_schema(schema)
+
+    # everything defined under $defs is not automatically validated unless we
+    # explicitly do so
+    for _, _def in schema["$defs"].items():
+        Draft7Validator.check_schema(_def)
