93be73fde023088bcb4eb865bb41e8dd3375490c
Ryan Tanaka <tanaka@isi.edu>
2020-02-14 12:08:00 -0800

    PM-1454 load/dump replica catalog unit tests

---
+++ lib/pegasus/python/Pegasus/test/test_replica_catalog.py
@@ -0,0 +1,61 @@
+import json
+from tempfile import TemporaryFile, NamedTemporaryFile
+
+import pytest
+
+import Pegasus
+from Pegasus.replica_catalog import *
+from Pegasus.replica_catalog import _to_rc
+from Pegasus.api.replica_catalog import ReplicaCatalog
+from Pegasus.api.writable import _CustomEncoder
+
+
+@pytest.fixture(scope="module")
+def rc_as_dict():
+    return  {
+            "pegasus": "5.0",
+            "replicas": [
+                {
+                    "lfn": "a",
+                    "pfn": "/a",
+                    "site": "local",
+                    "regex": False
+                }
+            ]
+         }
+
+def test_to_rc(rc_as_dict):
+
+    expected = ReplicaCatalog().add_replica("a", "/a", "local", regex=False)
+    result = _to_rc(rc_as_dict)
+
+    assert result.replicas == expected.replicas
+
+def test_load(mocker, rc_as_dict):
+    mocker.patch("Pegasus.yaml.load", return_value=rc_as_dict)
+    with TemporaryFile() as f:
+        rc = load(f)
+        Pegasus.yaml.load.assert_called_once_with(f)
+
+        assert len(rc.replicas) == 1
+        assert ("a", "/a", "local", False) in rc.replicas
+
+def test_loads(mocker, rc_as_dict):
+    mocker.patch("Pegasus.yaml.load", return_value=rc_as_dict)
+    rc = loads(json.dumps(rc_as_dict))
+
+    assert len(rc.replicas) == 1
+    assert ("a", "/a", "local", False) in rc.replicas
+
+def test_dump(mocker):
+    mocker.patch("Pegasus.api.writable.Writable.write")
+    rc = ReplicaCatalog()
+    with NamedTemporaryFile(mode="w") as f:
+        dump(rc, f)
+        Pegasus.api.writable.Writable.write.assert_called_once_with(f)
+
+def test_dumps(mocker):
+    mocker.patch("json.dumps")
+    rc = ReplicaCatalog().add_replica("a", "/a", "local", regex=False)
+    dumps(rc)
+    json.dumps.assert_called_once_with(rc, cls=_CustomEncoder)
