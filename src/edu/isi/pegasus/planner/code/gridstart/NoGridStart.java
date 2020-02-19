51aae39389e8e31c29d7e49c84d19a1174ec1c0e
Karan Vahi <vahi@isi.edu>
2020-02-19 15:30:05 -0800

    PM-1461 added support for gridstart launcher when we run jobs without kickstart. also added associated test cases

--- src/edu/isi/pegasus/planner/code/gridstart/NoGridStart.java
+++ src/edu/isi/pegasus/planner/code/gridstart/NoGridStart.java
@@ -351,6 +351,9 @@ public class NoGridStart implements GridStart {
                 generateListofFilenamesFile(job.getOutputFiles(), job, ".out.lof");
             }
         } /// end of mGenerateLOF
+        
+        //PM-1461 wrap job with launcher if specified
+        wrapJobWithGridStartLauncher(job);
 
         return true;
     }
@@ -405,6 +408,44 @@ public class NoGridStart implements GridStart {
         return path;
     }
 
+    /**
+     * Wrap invocation with a user specified GridStart launcher 
+     * 
+     * @param job 
+     */
+    protected void wrapJobWithGridStartLauncher(Job job) {
+        String launcher = job.vdsNS.getStringValue(Pegasus.GRIDSTART_LAUNCHER_KEY);
+        String launcherArguments = job.vdsNS.getStringValue(Pegasus.GRIDSTART_LAUNCHER_ARGUMENTS_KEY);
+        if (launcher == null){
+            return;
+        }
+        
+        String existingExecutable = job.getRemoteExecutable();
+        String existingArguments       = job.getArguments();
+        StringBuilder updatedArgs = new StringBuilder();
+        
+        if (launcherArguments != null ){
+            updatedArgs.append( launcherArguments ).append( " " );
+        }
+        
+        updatedArgs.append(existingExecutable);
+        
+        if (existingArguments != null ){
+            updatedArgs.append(" ");
+            updatedArgs.append(existingArguments);
+        }
+        
+        //launcher is the new executable for the job
+        job.setRemoteExecutable(launcher);
+        job.setArguments(updatedArgs.toString());
+        
+        mLogger.log("Wrapped job " + job.getID() + " with launcher " +
+                    job.getRemoteExecutable() + " " + job.getArguments(),
+                    LogManager.DEBUG_MESSAGE_LEVEL);
+        
+        return;
+    }
+    
     /**
      * Indicates whether the enabling mechanism can set the X bit on the executable on the remote
      * grid site, in addition to launching it on the remote grid stie
--- test/junit/edu/isi/pegasus/planner/code/GridStartTest.java
+++ test/junit/edu/isi/pegasus/planner/code/GridStartTest.java
@@ -47,14 +47,14 @@ import static org.junit.Assert.*;
  * @author Karan Vahi
  */
 public class GridStartTest {
-    
+
     private String DEFAULT_SITE_NAME = "condor_pool";
-    
+
     private static final String USER_JOB_EXECUTABLE = "/bin/echo";
     private static final String USER_JOB_ARGS = "user-job args";
     private static final String PEGASUS_KICKSTART_PATH = "/usr/bin/pegasus-kickstart";
     private static final Profile PEGASUS_HOME = new Profile("env", "PEGASUS_HOME", "/usr/");
-    
+
     private PegasusBag mBag;
 
     private PegasusProperties mProps;
@@ -68,40 +68,37 @@ public class GridStartTest {
     private static final String PROPERTIES_BASENAME = "properties";
     private Job mTestJob;
     private GridStartFactory mGSFactory;
-    
-    
+
     public GridStartTest() {
     }
-    
+
     @BeforeClass
     public static void setUpClass() {
     }
-    
+
     @AfterClass
     public static void tearDownClass() {
     }
-    
+
     @Before
     public void setUp() {
         mTestSetup = new DefaultTestSetup();
         mBag = new PegasusBag();
         mTestSetup.setInputDirectory(this.getClass());
-       
+
         mProps = PegasusProperties.nonSingletonInstance();
 
         mLogger = mTestSetup.loadLogger(mProps);
         mLogger.setLevel(LogManager.DEBUG_MESSAGE_LEVEL);
         mLogger.logEventStart("test.code.generator.GridStart", "setup", "0");
-        
+
         PlannerOptions options = new PlannerOptions();
         options.setBaseSubmitDirectory("/tmp");
-        
-        
+
         mBag.add(PegasusBag.PLANNER_OPTIONS, options);
         mBag.add(PegasusBag.PEGASUS_LOGMANAGER, mLogger);
         mBag.add(PegasusBag.PEGASUS_PROPERTIES, mProps);
-        
-       
+
         SiteStore store = new SiteStore();
         SiteCatalogEntry entry = new SiteCatalogEntry();
         entry.setSiteHandle(DEFAULT_SITE_NAME);
@@ -110,89 +107,120 @@ public class GridStartTest {
         entry.addProfile(PEGASUS_HOME);
         store.addEntry(entry);
         store.setForPlannerUse(mProps, options);
-        mBag.add(PegasusBag.SITE_STORE,store);
-        
-        
+        mBag.add(PegasusBag.SITE_STORE, store);
+
         mGSFactory = new GridStartFactory();
         mGSFactory.initialize(mBag, new ADag(), "/tmp/ps.log");
-        
+
         mTestJob = new Job();
         mTestJob.setName("test");
         mTestJob.setTransformation("", "test", "");
         mTestJob.setArguments(USER_JOB_ARGS);
         mTestJob.setRemoteExecutable(USER_JOB_EXECUTABLE);
         mTestJob.setSiteHandle(DEFAULT_SITE_NAME);
-        
-        mLogger.logEventCompletion(); 
+
+        mLogger.logEventCompletion();
     }
-    
-    
+
     @Test
-    public void testKickStartEnabledJob(){
-        
+    public void testKickStartEnabledJob() {
+
         mLogger.logEventStart(
                 "test.code.generator.GridStart", "kickstart", Integer.toString(mTestNumber++));
         Job j = (Job) mTestJob.clone();
-        
+
         GridStart ks = mGSFactory.loadGridStart(mTestJob, PROPERTIES_BASENAME);
-        
+
         assertTrue(ks.enable(j, true));
         assertEquals(PEGASUS_KICKSTART_PATH, j.getRemoteExecutable());
         assertEquals(" -n test -N null -R condor_pool  -L  -T  " + USER_JOB_EXECUTABLE + " " + USER_JOB_ARGS, j.getArguments());
         mLogger.logEventCompletion();
     }
-    
+
     @Test
-    public void testKickStartEnabledJobWithJSRun(){
-        
+    public void testKickStartEnabledJobWithJSRun() {
+
         mLogger.logEventStart(
                 "test.code.generator.GridStart", "js-run-kickstart", Integer.toString(mTestNumber++));
         Job j = (Job) mTestJob.clone();
-        
-        j.addProfile(new Profile("pegasus", Pegasus.GRIDSTART_LAUNCHER_KEY,"jsrun"));
-        
+
+        j.addProfile(new Profile("pegasus", Pegasus.GRIDSTART_LAUNCHER_KEY, "jsrun"));
+
         GridStart ks = mGSFactory.loadGridStart(j, PROPERTIES_BASENAME);
         assertTrue(ks.enable(j, true));
         assertEquals("jsrun", j.getRemoteExecutable());
         assertEquals(PEGASUS_KICKSTART_PATH + " -n test -N null -R condor_pool  -L  -T  " + USER_JOB_EXECUTABLE + " " + USER_JOB_ARGS, j.getArguments());
         mLogger.logEventCompletion();
     }
-    
+
     @Test
-    public void testKickStartEnabledJobWithJSRunArgs(){
-        
+    public void testKickStartEnabledJobWithJSRunArgs() {
+
         mLogger.logEventStart(
                 "test.code.generator.GridStart", "js-run-kickstart", Integer.toString(mTestNumber++));
         Job j = (Job) mTestJob.clone();
         String jsrunArgs = "-n 1 -a 1 -c 42 -g 0";
-        j.addProfile(new Profile("pegasus", Pegasus.GRIDSTART_LAUNCHER_KEY,"jsrun"));
-        j.addProfile(new Profile("pegasus", Pegasus.GRIDSTART_LAUNCHER_ARGUMENTS_KEY,jsrunArgs));
-        
+        j.addProfile(new Profile("pegasus", Pegasus.GRIDSTART_LAUNCHER_KEY, "jsrun"));
+        j.addProfile(new Profile("pegasus", Pegasus.GRIDSTART_LAUNCHER_ARGUMENTS_KEY, jsrunArgs));
+
         GridStart ks = mGSFactory.loadGridStart(j, PROPERTIES_BASENAME);
         assertTrue(ks.enable(j, true));
         assertEquals("jsrun", j.getRemoteExecutable());
         assertEquals(jsrunArgs + " " + PEGASUS_KICKSTART_PATH + " -n test -N null -R condor_pool  -L  -T  " + USER_JOB_EXECUTABLE + " " + USER_JOB_ARGS, j.getArguments());
         mLogger.logEventCompletion();
     }
-    
-    
+
     @Test
-    public void testNoKickStartEnabledJob(){
-        
+    public void testNoKickStartEnabledJob() {
+
         mLogger.logEventStart(
                 "test.code.generator.GridStart", "no-kickstart", Integer.toString(mTestNumber++));
         Job j = (Job) mTestJob.clone();
         j.addProfile(new Profile("pegasus", Pegasus.GRIDSTART_KEY, "None"));
         GridStart ks = mGSFactory.loadGridStart(j, PROPERTIES_BASENAME);
         assertTrue(ks.enable(j, true));
-        
+
         assertEquals(USER_JOB_EXECUTABLE, j.getRemoteExecutable());
         assertEquals(USER_JOB_ARGS, j.getArguments());
         mLogger.logEventCompletion();
     }
-    
-    
-    
+
+    @Test
+    public void testNoKickStartEnabledJobWithJSRun() {
+
+        mLogger.logEventStart(
+                "test.code.generator.GridStart", "js-run-no-kickstart", Integer.toString(mTestNumber++));
+        Job j = (Job) mTestJob.clone();
+        j.addProfile(new Profile("pegasus", Pegasus.GRIDSTART_KEY, "None"));
+
+        j.addProfile(new Profile("pegasus", Pegasus.GRIDSTART_LAUNCHER_KEY, "jsrun"));
+
+        GridStart ks = mGSFactory.loadGridStart(j, PROPERTIES_BASENAME);
+        assertTrue(ks.enable(j, true));
+        assertEquals("jsrun", j.getRemoteExecutable());
+        assertEquals(USER_JOB_EXECUTABLE + " " + USER_JOB_ARGS, j.getArguments());
+        mLogger.logEventCompletion();
+    }
+
+    @Test
+    public void testNoKickStartEnabledJobWithJSRunArgs() {
+
+        mLogger.logEventStart(
+                "test.code.generator.GridStart", "js-run-args-no-kickstart", Integer.toString(mTestNumber++));
+        Job j = (Job) mTestJob.clone();
+        String jsrunArgs = "-n 1 -a 1 -c 42 -g 0";
+        j.addProfile(new Profile("pegasus", Pegasus.GRIDSTART_KEY, "None"));
+
+        j.addProfile(new Profile("pegasus", Pegasus.GRIDSTART_LAUNCHER_KEY, "jsrun"));
+        j.addProfile(new Profile("pegasus", Pegasus.GRIDSTART_LAUNCHER_ARGUMENTS_KEY, jsrunArgs));
+
+        GridStart ks = mGSFactory.loadGridStart(j, PROPERTIES_BASENAME);
+        assertTrue(ks.enable(j, true));
+        assertEquals("jsrun", j.getRemoteExecutable());
+        assertEquals(jsrunArgs + " " + USER_JOB_EXECUTABLE + " " + USER_JOB_ARGS, j.getArguments());
+        mLogger.logEventCompletion();
+    }
+
     @After
     public void tearDown() {
     }
