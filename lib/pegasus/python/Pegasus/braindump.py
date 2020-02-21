10f685bc8fbf3a2aafdc4495d5106bd1869966ec
Rajiv Mayani <mayani@isi.edu>
2020-02-21 11:35:09 -0800

    PM-1451 Add docstring for Braindump class attributes.

--- lib/pegasus/python/Pegasus/braindump.py
+++ lib/pegasus/python/Pegasus/braindump.py
@@ -40,33 +40,90 @@ class Braindump:
         Use :mod:`dataclasses` instead of :mod:`attr`
     """
 
+    #: The username of the user that ran pegasus-plan
     user = attr.ib(type=str, default=None)  # type: str
+
+    #: The Distinguished Name in the proxy
     grid_dn = attr.ib(type=str, default=None)  # type: str
+
+    #: The hostname of the submit host
     submit_hostname = attr.ib(type=str, default=None)  # type: str
+
+    #: The workflow uuid of the root workflow
     root_wf_uuid = attr.ib(type=str, default=None)  # type: str
+
+    #: The workflow uuid of the current workflow i.e the one whose submit directory
+    #: the braindump file is.
     wf_uuid = attr.ib(type=str, default=None)  # type: str
+
+    #: The path to the dax file
     dax = attr.ib(type=str, default=None)  # type: str
+
+    #: The label attribute in the adag element of the dax
     dax_label = attr.ib(type=str, default=None)  # type: str
+
+    #: The index in the dax.
     dax_index = attr.ib(type=str, default=None)  # type: str
+
+    #: The version of the DAX schema that DAX referred to.
     dax_version = attr.ib(type=str, default=None)  # type: str
+
+    #: The workflow name constructed by pegasus when planning
     pegasus_wf_name = attr.ib(type=str, default=None)  # type: str
+
+    #: The timestamp when planning occured
     timestamp = attr.ib(type=str, default=None)  # type: str
+
+    #: The base submit directory
     basedir = attr.ib(type=str, default=None)  # type: str
+
+    #: The full path for the submit directory
     submit_dir = attr.ib(type=str, default=None)  # type: str
+
+    #: The planner used to construct the executable workflow. always pegasus
     planner = attr.ib(type=str, default=None)  # type: str
+
+    #: The versions of the planner
     planner_version = attr.ib(type=str, default=None)  # type: str
-    pegasus_build = attr.ib(type=str, default=None)  # type: str
+
+    #: The arguments with which the planner is invoked.
     planner_arguments = attr.ib(type=str, default=None)  # type: str
+
+    #: The build timestamp
+    pegasus_build = attr.ib(type=str, default=None)  # type: str
+
+    #: The path to the jobstate file
     jsd = attr.ib(type=str, default=None)  # type: str
+
+    #: The rundir in the numbering scheme for the submit directories
     rundir = attr.ib(type=str, default=None)  # type: str
+
+    #: The bin directory of the pegasus installation
     bindir = attr.ib(type=str, default=None)  # type: str
+
+    #: The vo group to which the user belongs to. Defaults to pegasus
     vogroup = attr.ib(type=str, default=None)  # type: str
+
+    #: Whether the workflow uses PMC
     uses_pmc = attr.ib(type=str, default=None)  # type: str
+
+    #: The full path to the properties file in the submit directory
     properties = attr.ib(type=str, default=None)  # type: str
+
+    #: The full path to condor common log in the submit directory
     condor_log = attr.ib(type=str, default=None)  # type: str
+
+    #: The basename of the dag file created
     dag = attr.ib(type=str, default=None)  # type: str
+
+    #: The type of executable workflow. Can be dag | shell
     type = attr.ib(type=str, default=None)  # type: str
+
+    #: The notify file that contains any notifications that need to be sent
+    #: for the workflow.
     notify = attr.ib(type=str, default=None)  # type: str
+
+    #: Set in PMC mode.
     script = attr.ib(type=str, default=None)  # type: str
 
 
