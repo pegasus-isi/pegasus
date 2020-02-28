f217ee9e6f189126ac2c5adc4de5c071d5f8c575
Mats Rynge <rynge@isi.edu>
2020-02-28 12:25:35 -0800

    PM-1472: added missing regions to pegasus-s3

--- lib/pegasus/python/Pegasus/s3.py
+++ lib/pegasus/python/Pegasus/s3.py
@@ -75,14 +75,21 @@ MB = 1024*KB
 GB = 1024*MB
 TB = 1024*GB
 
+# see https://docs.aws.amazon.com/general/latest/gr/s3.html
 LOCATIONS = {
     "s3.amazonaws.com": "",
+    "s3.us-east-1.amazonaws.com": "us-east-1",
+    "s3.us-east-2.amazonaws.com": "us-east-2",
     "s3-us-west-1.amazonaws.com": "us-west-1",
     "s3-us-west-2.amazonaws.com": "us-west-2",
+    "s3-ca-central-1.amazonaws.com": "ca-central-1",
     "s3-eu-west-1.amazonaws.com": "EU",
     "s3-ap-southeast-1.amazonaws.com": "ap-southeast-1",
     "s3-ap-southeast-2.amazonaws.com": "ap-southeast-2",
     "s3-ap-northeast-1.amazonaws.com": "ap-northeast-1",
+    "s3-ap-northeast-2.amazonaws.com": "ap-northeast-2",
+    "s3-ap-northeast-3.amazonaws.com": "ap-northeast-3",
+    "s3-ap-south-1.amazonaws.com": "ap-south-1",
     "s3-sa-east-1.amazonaws.com": "sa-east-1"
 }
 
