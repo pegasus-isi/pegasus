8ada9ee4b394119a73de8d85a9db2be9df547aae
Rajiv Mayani <mayani@isi.edu>
2020-03-05 19:09:59 -0800

    Remove check for pyOpenSSL as it is only needed in pegasus-service to use ssl certs.

--- lib/pegasus/python/Pegasus/cli/startup-validation.py
+++ lib/pegasus/python/Pegasus/cli/startup-validation.py
@@ -3,18 +3,11 @@
 import sys
 
 if not sys.version_info >= (3, 5):
-    sys.stderr.write('Pegasus requires Python 3.5 or above\n')
+    sys.stderr.write("Pegasus requires Python 3.5 or above\n")
     sys.exit(1)
 
 try:
     import yaml
 except:
-    sys.stderr.write('Pegasus requires the Python3 YAML module to be installed\n')
+    sys.stderr.write("Pegasus requires the Python3 YAML module to be installed\n")
     sys.exit(1)
-
-try:
-    import OpenSSL
-except:
-    sys.stderr.write('Pegasus requires the Python3 PyOpenSSL module to be installed\n')
-    sys.exit(1)
-
