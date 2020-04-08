b2b8400d40be462345446d309e6077b22c506e5e
Rajiv Mayani <mayani@isi.edu>
2020-04-07 19:17:53 -0700

    Upgrade SC3 to Yaml

--- test/core/020-pmc-only/plan.sh
+++ test/core/020-pmc-only/plan.sh
@@ -8,26 +8,28 @@ echo "Generating the dax..."
 export PYTHONPATH=`pegasus-config --python`
 python daxgen.py dax.xml
 
-cat > sites.xml <<END
-<?xml version="1.0" encoding="UTF-8"?>
-<sitecatalog xmlns="http://pegasus.isi.edu/schema/sitecatalog" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://pegasus.isi.edu/schema/sitecatalog http://pegasus.isi.edu/schema/sc-3.0.xsd" version="3.0">
-    <site handle="local" arch="x86_64" os="LINUX" osrelease="rhel" osversion="7">
-        <head-fs>
-            <scratch>
-                <shared>
-                    <file-server protocol="file" url="file://" mount-point="$TEST_DIR/work"/>
-                    <internal-mount-point mount-point="$TEST_DIR/work"/>
-                </shared>
-            </scratch>
-            <storage>
-                <shared>
-                    <file-server protocol="file" url="file://" mount-point="$TEST_DIR/outputs"/>
-                    <internal-mount-point mount-point="$TEST_DIR/outputs"/>
-                </shared>
-            </storage>
-        </head-fs>
-    </site>
-</sitecatalog>
+cat > sites.yml <<END
+pegasus: "5.0"
+sites:
+ -
+  name: "local"
+  arch: "x86_64"
+  os.type: "linux"
+  directories:
+   -
+    type: "sharedStorage"
+    path: "$TEST_DIR/outputs"
+    fileServers:
+     -
+      operation: "all"
+      url: "file://$TEST_DIR/outputs"
+   -
+    type: "sharedScratch"
+    path: "$TEST_DIR/work"
+    fileServers:
+     -
+      operation: "all"
+      url: "file://$TEST_DIR/work"
 END
 
 echo "Planning the workflow..."
