dccf93472d91ab853c360a27e1fb0baca7762fa7
Karan Vahi <vahi@isi.edu>
2020-04-17 16:14:36 -0700

    PM-1523 code format

--- src/edu/isi/pegasus/planner/client/RCConverter.java
+++ src/edu/isi/pegasus/planner/client/RCConverter.java
@@ -295,7 +295,8 @@ public class RCConverter extends Executable {
         for (String inputFile : inputFiles) {
             File input = new File(inputFile);
             if (!input.canRead()) {
-                throw new IOException("File not found or cannot be read." + input.getAbsolutePath());
+                throw new IOException(
+                        "File not found or cannot be read. " + input.getAbsolutePath());
             }
         }
         for (String inputFile : inputFiles) {
