333657dc8a58898a4d40675028e8f786d516f0ea
Mats Rynge <rynge@isi.edu>
2020-04-27 15:26:18 -0700

    Added test for bypass integrity checking. Fixed montage http_proxy settings

---
+++ test/core/043-integrity-bypass-staging/workflow-generator.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+import sys
+import os
+from pathlib import Path
+from Pegasus.api import *
+
+TOP_DIR = Path(Path.cwd())
+
+wf = Workflow('bypass-staging')
+
+# the workflow will be planned by not executed - src does not need to exist
+infile = File('input.txt')
+rc = (ReplicaCatalog()
+       .add_replica('local',
+                    infile, 
+                    'file://' + str(TOP_DIR / infile.lfn),
+                    checksum_type='sha256',
+                    checksum_value='66a42b4be204c824a7533d2c677ff7cc5c44526300ecd6b450602e06128063f9'))
+
+tc = (TransformationCatalog()
+       .add_transformations(Transformation('ls', site='condorpool', pfn='/bin/ls', is_stageable=False)))
+
+job = (Job('ls')
+        .add_args('-l')
+        .add_inputs(infile))
+wf.add_jobs(job)
+
+wf.add_transformation_catalog(tc)
+wf.add_replica_catalog(rc)
+wf.write('workflow.yml')
+
