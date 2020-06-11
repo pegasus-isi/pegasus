d40b70ddd138914c749efa5c7d8461f4e039988e
Karan Vahi <vahi@isi.edu>
2020-06-11 15:11:05 -0700

    PM-1608 load an additional output mapper if --output-map option is specified

--- src/edu/isi/pegasus/planner/refiner/TransferEngine.java
+++ src/edu/isi/pegasus/planner/refiner/TransferEngine.java
@@ -32,6 +32,7 @@ import edu.isi.pegasus.planner.classes.NameValue;
 import edu.isi.pegasus.planner.classes.PegasusBag;
 import edu.isi.pegasus.planner.classes.PegasusFile;
 import edu.isi.pegasus.planner.classes.PlannerCache;
+import edu.isi.pegasus.planner.classes.PlannerOptions;
 import edu.isi.pegasus.planner.classes.ReplicaLocation;
 import edu.isi.pegasus.planner.common.PegasusConfiguration;
 import edu.isi.pegasus.planner.common.PegasusProperties;
@@ -128,8 +129,19 @@ public class TransferEngine extends Engine {
     /** A Replica Catalog, that tracks all the GET URL's for the files on the staging sites. */
     private ReplicaCatalog mWorkflowCache;
 
-    /** Handle to an OutputMapper that tells where to place the files on the output site. */
+    /**
+     * Handle to an OutputMapper that tells where to place the files on the output sites. this
+     * output mapper is the one configured by the user via properties
+     */
     private OutputMapper mOutputMapper;
+
+    /**
+     * Handle to an OutputMapper that tells where to place the files on the scratch file system for
+     * compute jobs in the parent workflow (the workflow in which there is a DAX job that triggers
+     * the execution of the workflow currently planned as part of the pre script) This output mapper
+     * is the one set on the command line by Pegasus in case of hierarchal workflows.
+     */
+    private OutputMapper mParentScratchOutputMapper;
 
     /**
      * Handle to an Staging Mapper that tells where to place the files on the shared scratch space
@@ -214,6 +226,9 @@ public class TransferEngine extends Engine {
         mOutputSites = (Set<String>) mPOptions.getOutputSites();
         mOutputMapper = OutputMapperFactory.loadInstance(reducedDag, bag);
 
+        // PM-1608 load a mapper if required
+        mParentScratchOutputMapper = getParentWFScratchMapper(reducedDag, bag);
+
         mWorkflowCache = this.initializeWorkflowCacheFile(reducedDag);
 
         // log some configuration messages
@@ -232,6 +247,13 @@ public class TransferEngine extends Engine {
         mLogger.log(
                 "Output Mapper loaded is              [" + mOutputMapper.description() + "]",
                 LogManager.CONFIG_MESSAGE_LEVEL);
+        if (mParentScratchOutputMapper != null) {
+            mLogger.log(
+                    "Output Scratch Mapper loaded is      ["
+                            + mParentScratchOutputMapper.description()
+                            + "]",
+                    LogManager.CONFIG_MESSAGE_LEVEL);
+        }
     }
 
     /**
@@ -2094,5 +2116,39 @@ public class TransferEngine extends Engine {
                     .append(localSiteFileRCEs);
         }
         throw new RuntimeException(error.toString());
+    }
+
+    /**
+     * Returns an output mapper responsible for transferring some outputs to the scratch file system
+     * of the enclosing parent workflow.
+     *
+     * @param dag
+     * @param bag
+     * @return
+     */
+    private OutputMapper getParentWFScratchMapper(ADag dag, PegasusBag bag) {
+        PlannerOptions options = bag.getPlannerOptions();
+        String mapFile = options.getOutputMap();
+        if (mapFile == null) {
+            return null;
+        }
+
+        // PM-1608 always load a replica based output mapper corresponding to this file
+        PegasusProperties props = PegasusProperties.nonSingletonInstance();
+        String key = OutputMapperFactory.PROPERTY_KEY;
+        props.setProperty(key, "Replica");
+        // output map file is a file based rc
+        key += ".replica";
+        props.setProperty(key, "File");
+        // set path to output mapper
+        key += ".file";
+        props.setProperty(key, mapFile);
+
+        PegasusBag b = new PegasusBag();
+        b.add(PegasusBag.PLANNER_OPTIONS, options);
+        b.add(PegasusBag.PEGASUS_LOGMANAGER, bag.getLogger());
+        b.add(PegasusBag.PEGASUS_PROPERTIES, props);
+
+        return OutputMapperFactory.loadInstance(dag, b);
     }
 }
