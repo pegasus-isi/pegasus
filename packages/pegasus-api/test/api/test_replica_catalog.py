cb8d5607e4ac004f8d4ccb8f637f0c3ede51e149
Ryan Tanaka <ryanyt@hawaii.edu>
2020-06-18 21:16:55 -0700

    PM-1546 updated rc unit tests

--- packages/pegasus-api/test/api/test_replica_catalog.py
+++ packages/pegasus-api/test/api/test_replica_catalog.py
@@ -2,6 +2,8 @@ import json
 import re
 from pathlib import Path
 from tempfile import NamedTemporaryFile
+from collections import OrderedDict
+
 
 import pytest
 from jsonschema import validate
@@ -11,12 +13,13 @@ import yaml
 from Pegasus.api.errors import DuplicateError
 from Pegasus.api.replica_catalog import (
     PEGASUS_VERSION,
+    _PFN,
     File,
     ReplicaCatalog,
     _ReplicaCatalogEntry,
 )
 from Pegasus.api.writable import _CustomEncoder
-
+from conftest import _tojson
 
 class TestFile:
     @pytest.mark.parametrize("lfn,size", [("a", None), ("例", 2048)])
@@ -32,7 +35,10 @@ class TestFile:
 
     @pytest.mark.parametrize(
         "lfn,size,expected",
-        [("f1", None, {"lfn": "f1"}), ("f2", 2048, {"lfn": "f2", "size": 2048})],
+        [
+            ("f1", None, {"lfn": "f1"}),
+            ("f2", 2048, {"lfn": "f2", "size": 2048, "metadata": {"size": 2048}}),
+        ],
     )
     def test_tojson_no_metadata(self, lfn, size, expected):
         assert File(lfn, size).__json__() == expected
@@ -44,7 +50,11 @@ class TestFile:
 
     def test_tojson_with_metdata(self, convert_yaml_schemas_to_json, load_schema):
         result = File("lfn", size=2048).add_metadata(key="value").__json__()
-        expected = {"lfn": "lfn", "metadata": {"key": "value"}, "size": 2048}
+        expected = {
+            "lfn": "lfn",
+            "metadata": {"key": "value", "size": 2048},
+            "size": 2048,
+        }
 
         file_schema = load_schema("rc-5.0.json")["$defs"]["file"]
         validate(instance=result, schema=file_schema)
@@ -54,95 +64,69 @@ class TestFile:
 
 class Test_ReplicaCatalogEntry:
     @pytest.mark.parametrize(
-        "site, lfn, pfn, regex, checksum_type, checksum_value",
+        "lfn, pfns, checksum, metadata, regex",
         [
-            ("local", "f.a", "/f.a", False, None, None),
-            ("local", "f.a", "/f.a", True, None, None),
-            ("local", "f.a", "/f.a", False, "sha256", "abc123"),
-        ],
-    )
-    def test_valid_entry(self, site, lfn, pfn, regex, checksum_type, checksum_value):
-        assert _ReplicaCatalogEntry(
-            site, lfn, pfn, regex, checksum_type, checksum_value
-        )
-
-    def test_regex_and_checksum_in_entry(self):
-        with pytest.raises(ValueError) as e:
-            _ReplicaCatalogEntry(
-                "local",
+            ("f.a", {_PFN("local", "/f.a")}, None, None, False),
+            ("f.a", {_PFN("local", "/f.a")}, {"sha245": "abc123"}, None, False),
+            (
                 "f.a",
-                "/f.a",
-                regex=True,
-                checksum_type="sha256",
-                checksum_value="abc123",
-            )
-
-        assert "Checksum values cannot be used with a regex" in str(e)
-
-    def test_invalid_checksum(self):
-        with pytest.raises(ValueError) as e:
-            _ReplicaCatalogEntry(
-                "local", "f.a", "/f.a", checksum_type="sha256", checksum_value=None
-            )
-
-        assert "Checksum usage in replica catalog requires" in str(e)
-
-    @pytest.mark.parametrize(
-        "a,b",
-        [
-            (
-                _ReplicaCatalogEntry("local", "f.a", "/f.a"),
-                _ReplicaCatalogEntry("local", "f.a", "/f.a"),
+                {_PFN("local", "/f.a")},
+                {"sha245": "abc123"},
+                {"owner": "ryan"},
+                False,
             ),
-            (
-                _ReplicaCatalogEntry("local", "f.a", "/f.a", True),
-                _ReplicaCatalogEntry("local", "f.a", "/f.a", True),
-            ),
-            (
-                _ReplicaCatalogEntry(
-                    "local",
-                    "f.a",
-                    "/f.a",
-                    checksum_type="sha256",
-                    checksum_value="abc123",
-                ),
-                _ReplicaCatalogEntry(
-                    "local",
-                    "f.a",
-                    "/f.a",
-                    checksum_type="sha256",
-                    checksum_value="abc123",
-                ),
-            ),
+            ("f.a", {_PFN("local", "/f.a")}, None, None, True),
+            ("f.a", {_PFN("local", "/f.a")}, None, {"owner", "ryan"}, True),
         ],
     )
-    def test_eq(self, a, b):
-        assert a == b
+    def test_valid_entry(self, lfn, pfns, checksum, metadata, regex):
+        assert _ReplicaCatalogEntry(
+            lfn, pfns, checksum=checksum, metadata=metadata, regex=regex
+        )
 
     @pytest.mark.parametrize(
         "result, expected",
         [
             (
-                _ReplicaCatalogEntry("local", "f.a", "/f.a").__json__(),
-                {"site": "local", "lfn": "f.a", "pfn": "/f.a",},
+                _tojson(_ReplicaCatalogEntry("f.a", {_PFN("local", "/f.a")})),
+                {"lfn": "f.a", "pfns": [{"site": "local", "pfn": "/f.a"}]},
             ),
             (
-                _ReplicaCatalogEntry("local", "f.a", "/f.a", regex=True).__json__(),
-                {"site": "local", "lfn": "f.a", "pfn": "/f.a", "regex": True},
-            ),
-            (
-                _ReplicaCatalogEntry(
-                    "local",
-                    "f.a",
-                    "/f.a",
-                    checksum_type="sha256",
-                    checksum_value="abc123",
-                ).__json__(),
+                _tojson(
+                    _ReplicaCatalogEntry(
+                        "f.a", {_PFN("local", "/f.a")}, checksum={"sha256": "abc123"}
+                    )
+                ),
                 {
-                    "site": "local",
                     "lfn": "f.a",
-                    "pfn": "/f.a",
-                    "checksum": {"type": "sha256", "value": "abc123"},
+                    "pfns": [{"site": "local", "pfn": "/f.a"}],
+                    "checksum": {"sha256": "abc123"},
+                },
+            ),
+            (
+                _tojson(
+                    _ReplicaCatalogEntry(
+                        "f.a",
+                        {_PFN("local", "/f.a")},
+                        checksum={"sha256": "abc123"},
+                        metadata={"owner": "ryan"},
+                    )
+                ),
+                {
+                    "lfn": "f.a",
+                    "pfns": [{"site": "local", "pfn": "/f.a"}],
+                    "checksum": {"sha256": "abc123"},
+                    "metadata": {"owner": "ryan"},
+                },
+            ),
+            (
+                _tojson(
+                    _ReplicaCatalogEntry("f.a", {_PFN("local", "/f.a")}, regex=True)
+                ),
+                {
+                    "lfn": "f.a",
+                    "pfns": [{"site": "local", "pfn": "/f.a"}],
+                    "regex": True,
                 },
             ),
         ],
@@ -152,87 +136,178 @@ class Test_ReplicaCatalogEntry:
 
 
 class TestReplicaCatalog:
-    @pytest.mark.parametrize(
-        "replica", [("lfn", "pfn", "site", True), (File("lfn"), "pfn", "site", True)]
-    )
-    def test_add_replica(self, replica: tuple):
+    def test_add_replica_str_as_lfn(self):
         rc = ReplicaCatalog()
-        rc.add_replica(*replica)
-        assert len(rc.replicas) == 1
-
-    def test_add_duplicate_replica(self):
-        rc = ReplicaCatalog()
-        with pytest.raises(DuplicateError) as e:
-            rc.add_replica("site", "lfn", "pfn", True)
-            rc.add_replica("site", File("lfn"), "pfn", True)
-
-        assert (
-            "entry: ReplicaEntry(site=site, lfn=lfn, pfn=pfn, regex=True, checksum_type=None, checksum_value=None)"
-            in str(e)
-        )
-
-    def test_add_invalid_replica(self):
-        rc = ReplicaCatalog()
-        with pytest.raises(TypeError) as e:
-            rc.add_replica("site", set(), "pfn")
-
-        assert "invalid lfn: {lfn}".format(lfn=set()) in str(e)
-
-    def test_tojson(self, convert_yaml_schemas_to_json, load_schema):
-        rc = ReplicaCatalog()
-        rc.add_replica("site1", "lfn1", "pfn1")
-        rc.add_replica("site2", "lfn2", "pfn2", True)
-        rc.add_replica(
-            "site3", "lfn3", "pfn3", checksum_type="sha256", checksum_value="abc123"
-        )
-
-        expected = {
-            "pegasus": PEGASUS_VERSION,
-            "replicas": [
-                {"lfn": "lfn1", "pfn": "pfn1", "site": "site1"},
-                {"lfn": "lfn2", "pfn": "pfn2", "site": "site2", "regex": True},
-                {
-                    "lfn": "lfn3",
-                    "pfn": "pfn3",
-                    "site": "site3",
-                    "checksum": {"type": "sha256", "value": "abc123"},
-                },
-            ],
+        rc.add_replica("local", "f.a", "/f.a")
+        assert _tojson(rc.entries[("f.a", False)]) == {
+            "lfn": "f.a",
+            "pfns": [{"site": "local", "pfn": "/f.a"}],
         }
-        expected["replicas"] = sorted(expected["replicas"], key=lambda d: d["lfn"])
 
-        result = json.loads(json.dumps(rc, cls=_CustomEncoder))
-        result["replicas"] = sorted(result["replicas"], key=lambda d: d["lfn"])
+    '''
+    def test_add_replica_file_as_lfn(self):
+        rc = ReplicaCatalog()
+        f = File("f.a", size=1024).add_metadata(creator="ryan")
+        rc.add_replica("local", f, "/f.a")
 
-        rc_schema = load_schema("rc-5.0.json")
-        validate(instance=result, schema=rc_schema)
+        assert _tojson(rc.entries[("f.a", False)]) == {
+            "lfn": "f.a",
+            "pfns": [{"site": "local", "pfn": "/f.a"}],
+            "metadata": {"size": 1024, "creator": "ryan"},
+        }
+    '''
 
-        assert result == expected
+    def test_add_multiple_replicas(self):
+        rc = ReplicaCatalog()
+        rc.add_replica("local", "f.a", "/f.a")
+        rc.add_replica("local", "f.b", "/f.b")
+        assert _tojson(rc.entries[("f.a", False)]) == {
+            "lfn": "f.a",
+            "pfns": [{"site": "local", "pfn": "/f.a"}],
+        }
+
+        assert _tojson(rc.entries[("f.b", False)]) == {
+            "lfn": "f.b",
+            "pfns": [{"site": "local", "pfn": "/f.b"}],
+        }
+
+        assert len(rc.entries) == 2
+
+    def test_add_replica_multiple_pfns(self):
+        rc = ReplicaCatalog()
+        rc.add_replica("local", "f.a", "/f.a")
+        rc.add_replica("condorpool", "f.a", "/f.a")
+        rc.add_replica("condorpool", "f.a", "/f.a")
+
+        assert len(rc.entries) == 1
+        assert rc.entries[("f.a", False)].pfns == {
+            _PFN("local", "/f.a"),
+            _PFN("condorpool", "/f.a"),
+        }
+
+        assert rc.entries[("f.a", False)].metadata == {}
+
+    def test_add_replica_multiple_pfns_checksums_and_metadata(self):
+        rc = ReplicaCatalog()
+        rc.add_replica(
+            "local",
+            "f.a",
+            "/f.a",
+            checksum={"sha256": "abc"},
+            metadata={"creator": "ryan"},
+        )
+
+        rc.add_replica(
+            "condorpool", "f.a", "/f.a", metadata={"size": 1024},
+        )
+
+        f_a_entry = rc.entries[("f.a", False)]
+        assert f_a_entry.metadata == {"size": 1024, "creator": "ryan"}
+        assert f_a_entry.checksum == {"sha256": "abc"}
+
+    def test_add_replica_with_invalid_checksum(self):
+        rc = ReplicaCatalog()
+        with pytest.raises(ValueError) as e:
+            rc.add_replica("local", "f.a", "/f.a", checksum={"md5": "123"})
+
+        assert "Invalid checksum: md5" in str(e)
+
+    def test_add_regex_replica(self):
+        rc = ReplicaCatalog()
+        rc.add_regex_replica("local", "*.txt", "/path")
+
+        assert _tojson(rc.entries[("*.txt", True)]) == {
+            "lfn": "*.txt",
+            "pfns": [{"site": "local", "pfn": "/path"}],
+            "regex": True
+        }
+
+    def test_add_duplicate_regex_replica(self):
+        rc = ReplicaCatalog()
+        rc.add_regex_replica("local", "*.txt", "/path")
+
+        with pytest.raises(DuplicateError) as e:
+            rc.add_regex_replica("local", "*.txt", "/path")
+        
+        assert "Pattern: *.txt already exists" in str(e)
+
+    def test_add_regex_replica_with_metadata(self):
+        rc = ReplicaCatalog()
+        rc.add_regex_replica("local", "*.txt", "/path", metadata={"creator": "ryan"})
+
+        assert _tojson(rc.entries[("*.txt", True)]) == {
+            "lfn": "*.txt",
+            "pfns": [{"site": "local", "pfn": "/path"}],
+            "metadata": {"creator": "ryan"},
+            "regex": True
+        }
+
+    def test_tojson(self):
+        rc = ReplicaCatalog()
+        rc.add_replica("local", "f.a", "/f.a", checksum={"sha256": "123"}, metadata={"size": 1024})
+        rc.add_regex_replica("local", "*.txt", "/path", metadata={"creator": "ryan"})
+
+        assert _tojson(rc) == {
+                'pegasus': '5.0', 
+                'replicas': [
+                    {
+                        'lfn': 'f.a', 
+                        'pfns': [{'site': 'local', 'pfn': '/f.a'}], 
+                        'checksum': {'sha256': '123'}, 
+                        'metadata': {'size': 1024}
+                    }, 
+                    {
+                        'lfn': '*.txt', 
+                        'pfns': [{'site': 'local', 'pfn': '/path'}], 
+                        'metadata': {'creator': 'ryan'}, 
+                        'regex': True
+                    }, 
+                ]
+            }
 
     @pytest.mark.parametrize(
         "_format, loader", [("json", json.load), ("yml", yaml.safe_load)]
     )
     def test_write(self, _format, loader):
         rc = ReplicaCatalog()
-        rc.add_replica("site1", "lfn1", "pfn1", True).add_replica(
-            "site2", "lfn2", "pfn2", True
-        )
+        f_a = File("f.a", size=1024).add_metadata(creator="ryan")
+        rc.add_replica("local", f_a, "/f.a", checksum={"sha256": "123"}, metadata={"extra": "metadata"})
+        rc.add_replica("condorpool", f_a, "/f.a")
+        rc.add_replica("local", "f.b", "/f.b")
+        rc.add_regex_replica("local", "*.txt", "/path", metadata={"creator": "ryan"})
 
         expected = {
-            "pegasus": PEGASUS_VERSION,
-            "replicas": [
-                {"lfn": "lfn1", "pfn": "pfn1", "site": "site1", "regex": True},
-                {"lfn": "lfn2", "pfn": "pfn2", "site": "site2", "regex": True},
-            ],
+            'pegasus': '5.0', 
+            'replicas': [
+                {
+                    'lfn': 'f.a', 
+                    'pfns': [{'site': 'local', 'pfn': '/f.a'}, {'site': 'condorpool', 'pfn': '/f.a'}], 
+                    'checksum': {'sha256': '123'}, 
+                    'metadata': {'extra': 'metadata', 'size': 1024, 'creator': 'ryan'}
+                }, 
+                {
+                    'lfn': 'f.b', 
+                    'pfns': [{'site': 'local', 'pfn': '/f.b'}], 
+                    'metadata': {'size': 1024, 'creator': 'ryan'}
+                }, 
+                {
+                    'lfn': '*.txt', 
+                    'pfns': [{'site': 'local', 'pfn': '/path'}], 
+                    'metadata': {'creator': 'ryan'}, 'regex': True}
+            ]
         }
-        expected["replicas"] = sorted(expected["replicas"], key=lambda d: d["lfn"])
+        expected["replicas"][0]["pfns"] = sorted( 
+            expected["replicas"][0]["pfns"], key=lambda pfn: pfn["site"]
+        )
 
         with NamedTemporaryFile(mode="r+") as f:
             rc.write(f, _format=_format)
             f.seek(0)
             result = loader(f)
 
-        result["replicas"] = sorted(result["replicas"], key=lambda d: d["lfn"])
+        result["replicas"][0]["pfns"] = sorted( 
+            result["replicas"][0]["pfns"], key=lambda pfn: pfn["site"]
+        )
 
         assert result == expected
 
@@ -264,3 +339,4 @@ class TestReplicaCatalog:
         """
         p = re.compile(r"pegasus: '5.0'[\w\W]+replicas:[\w\W]+")
         assert p.match(result) is not None
+
