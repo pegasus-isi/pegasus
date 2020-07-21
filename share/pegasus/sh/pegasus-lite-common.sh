17c02a710d5a4518fa5a0b69ae1c188b4cb70a2e
Mats Rynge <mats@rynge.net>
2020-07-21 11:57:53 -0700

    Improved resiliance of plite in case PATH is not set

--- share/pegasus/sh/pegasus-lite-common.sh
+++ share/pegasus/sh/pegasus-lite-common.sh
@@ -424,15 +424,15 @@ pegasus_lite_init()
         # PM-1134 - provide some details on where we are running
         # PM-1144 - do not use HOSTNAME from env, as it might have come form getenv=true
         out="Executing on"
-        my_hostname=`hostname -f 2>&1`
+        my_hostname=`hostname -f 2>/dev/null || /bin/true`
         if [ "x$my_hostname" != "x" ]; then
             out="$out host $my_hostname"
 
             # also IP if we can figure it out
-            my_ip=`(host $my_hostname | grep "has address" | sed 's/.* has address //') 2>/dev/null`
+            my_ip=`(host $my_hostname | grep "has address" | sed 's/.* has address //') 2>/dev/null || /bin/true`
             if [ "x$my_ip" = "x" ]; then
                 # can also try hostname -I
-                my_ip=`(hostname -I | sed 's/ .*//') 2>/dev/null`
+                my_ip=`(hostname -I | sed 's/ .*//') 2>/dev/null || /bin/true`
             fi
             if [ "x$my_ip" != "x" ]; then
                 out="$out IP=$my_ip"
