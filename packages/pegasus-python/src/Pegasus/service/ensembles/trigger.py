ec304442939800b49d2e7c59451520437d64c006
Ryan Tanaka <ryanyt@hawaii.edu>
2020-08-03 13:00:56 -0700

    PM-1607 refactored TriggerManager into main thread and trigger dispatcher thread; improved logging

--- packages/pegasus-python/src/Pegasus/service/ensembles/trigger.py
+++ packages/pegasus-python/src/Pegasus/service/ensembles/trigger.py
@@ -5,6 +5,7 @@ import math
 import pickle
 import queue
 import subprocess
+import time
 from enum import Enum
 from glob import glob
 from multiprocessing import Process
@@ -13,7 +14,24 @@ from pathlib import Path
 from threading import Event, Thread
 from typing import List, Optional
 
+# --- setup dir for trigger related files --------------------------------------
+trigger_dir = Path().home() / ".pegasus/triggers"
+trigger_dir.mkdir(parents=True, exist_ok=True)
 
+# --- logging conf -------------------------------------------------------------
+logger = logging.getLogger("trigger")
+
+# setup file handler
+log_file = str(trigger_dir / "trigger_manager.log")
+fh = logging.FileHandler(log_file)
+
+# setup log format
+formatter = logging.Formatter("%(asctime)s - %(name)18s - %(levelname)6s - %(message)s")
+fh.setFormatter(formatter)
+
+logger.addHandler(fh)
+
+# --- messages -----------------------------------------------------------------
 class _TriggerManagerMessageType(Enum):
     """Message types to be handled by TriggerManager"""
 
@@ -41,6 +59,7 @@ class TriggerManagerMessage:
         )
 
 
+# --- manager ------------------------------------------------------------------
 class TriggerManager(Process):
     """
     Manager to be spawned by the pegasus-em server process. It will listen for 
@@ -51,74 +70,102 @@ class TriggerManager(Process):
     def __init__(self):
         Process.__init__(self, daemon=True)
 
-        # TODO: add file handler so that logs can be written to ~/.pegasus/ensembles/triggers
-        self._log = logging.getLogger("TriggerManager")
+        self.log = logging.getLogger("trigger.manager")
 
-        # key="<ensemble>::<trigger_name>", value=trigger_thread
-        self._running_triggers = dict()
+        # messages to be consumed by worker
+        self.mailbox = queue.Queue()
 
-        # triggers that can be removed from self._running_triggers
-        self._to_del = queue.Queue()
-
-        # make state visible to pegasus-em client via file
-        trigger_dir = Path().home() / ".pegasus/triggers"
-        trigger_dir.mkdir(parents=True, exist_ok=True)
-        self._running_triggers_file = trigger_dir / "running.p"
-
-        self._log.debug(
-            "writing trigger state file to : {}".format(
-                self._running_triggers_file.resolve()
-            )
-        )
-        with self._running_triggers_file.open("wb") as f:
-            pickle.dump(set(), f)
+        self.dispatcher = _TriggerDispatcher(self.mailbox)
 
     def run(self):
         # TODO: make configurable
         address = ("localhost", 3000)
-        self._log.info("starting at: {}".format(address))
+        self.log.info("starting and will listen on: {}".format(address))
+
+        # start up trigger manager worker
+        self.dispatcher.start()
+
         # TODO: change authkey
         with Listener(address, authkey=b"123") as listener:
-            self._log.debug("listening for new connection")
             while True:
-                # TODO: cleanup any threads that are no longer in running state
+                self.log.debug("listening for new connection")
                 with listener.accept() as conn:
-                    self._log.debug(
+                    self.log.debug(
                         "connection accepted from {}".format(listener.last_accepted)
                     )
 
+                    # handle message received from pegasus-em client
                     msg = conn.recv()
-                    self._log.debug("received message: {}".format(msg))
+                    self.log.debug("received message: {}".format(msg))
+                    self.mailbox.put(msg)
 
-                    if msg._type == TriggerManagerMessage.STOP_TRIGGER:
-                        self._stop_trigger_handler(**msg.kwargs)
-                    elif (
-                        msg._type
-                        == TriggerManagerMessage.START_PATTERN_INTERVAL_TRIGGER
-                    ):
-                        self._start_pattern_interval_trigger_handler(**msg.kwargs)
-                    elif msg._type == TriggerManagerMessage.STATUS:
-                        self._status_handler(**msg.kwargs)
-                    else:
-                        self._log.error("invalid message: {}".format(msg))
 
-    def _stop_trigger_handler(self, ensemble: str, trigger_name: str):
+class _TriggerDispatcher(Thread):
+    """Work thread of the TriggerManager"""
+
+    def __init__(self, mailbox: queue.Queue):
+        Thread.__init__(self, daemon=True)
+
+        self.log = logging.getLogger("trigger.dispatcher")
+
+        # make state visible to pegasus-em client via file
+        self.running_triggers_file = trigger_dir / "running.p"
+        with self.running_triggers_file.open("wb") as f:
+            pickle.dump(set(), f)
+
+        # work passed down by the TriggerManager
+        self.mailbox = mailbox
+
+        # key="<ensemble>::<trigger_name>", value=trigger_thread
+        self.running = dict()
+
+        # triggers that can be removed from self.running
+        self.checkout = queue.Queue()
+
+    def run(self):
+        self.log.info("starting")
+
+        work = {
+            TriggerManagerMessage.START_PATTERN_INTERVAL_TRIGGER: self.start_pattern_interval_trigger_handler,
+            TriggerManagerMessage.STOP_TRIGGER: self.stop_trigger_handler,
+            TriggerManagerMessage.STATUS: self.status_handler,
+        }
+
+        while True:
+            # handle accounting: remove references to any threads that have stopped
+            has_changed = False
+            while not self.checkout.empty():
+                del self.running[self.checkout.get()]
+                has_changed = True
+
+            if has_changed:
+                self.update_state_file()
+
+            # handle work
+            try:
+                msg = self.mailbox.get(timeout=1)
+                self.log.debug("received message: {}".format(msg))
+                work[msg._type](**msg.kwargs)
+            except queue.Empty:
+                pass
+
+    def stop_trigger_handler(self, ensemble: str, trigger_name: str):
         """Handler for a STOP_TRIGGER message"""
         trigger = "::".join([ensemble, trigger_name])
         try:
-            self._running_triggers[trigger].shutdown()
-            del self._running_triggers[trigger]
-            self._save_running_threads()
+            self.running[trigger].shutdown()
+            del self.running[trigger]
+            self.update_state_file()
 
-            self._log.info("stopped trigger {}".format(trigger))
+            self.log.info("stopped trigger {}".format(trigger))
 
         # case: trigger doesn't exist; should not happen as pegsus em client would
         # have thrown an exception after seeing that this trigger is not in running
         # triggers file
         except KeyError:
-            self._log.error("TriggerManager does not contain: {}".format(trigger))
+            self.log.error("TriggerManager does not contain: {}".format(trigger))
 
-    def _start_pattern_interval_trigger_handler(
+    def start_pattern_interval_trigger_handler(
         self,
         ensemble: str,
         trigger_name: str,
@@ -130,7 +177,8 @@ class TriggerManager(Process):
     ):
         """Handler for a START_PATTERN_INTERVAL_TRIGGER message"""
 
-        t = PatternIntervalTrigger(
+        t = _PatternIntervalTrigger(
+            checkout=self.checkout,
             ensemble=ensemble,
             trigger_name=trigger_name,
             workflow_name_prefix=workflow_name_prefix,
@@ -140,48 +188,50 @@ class TriggerManager(Process):
             additional_args=additional_args,
         )
 
-        if t.name not in self._running_triggers:
-            self._running_triggers[t.name] = t
-            self._save_running_threads()
+        if t.name not in self.running:
+            self.running[t.name] = t
+            self.update_state_file()
             t.start()
 
         # case: trigger already exists, however this should never happen as pegasus em
         # client would have thrown an error after seeing it in the running
         # triggers file
         else:
-            self._log.error(
+            self.log.error(
                 "Cannot overwrite existing trigger: {}::{}".format(
                     ensemble, trigger_name
                 )
             )
 
-    def _status_handler(self, ensemble: str, name: Optional[str] = None):
+    def status_handler(self, ensemble: str, name: Optional[str] = None):
         """Handler for a STATUS message"""
 
         raise NotImplementedError("trigger status not yet implemented")
 
-    def _save_running_threads(self):
+    def update_state_file(self):
         """
         Overwrite ~/.pegasus/triggers/running.p with the current set of
         running triggers.
         """
-        running_triggers = {t for t in self._running_triggers}
-        self._log.debug(
-            "writing {} to {}".format(running_triggers, self._running_triggers_file)
+        running_triggers = {t for t in self.running}
+        self.log.debug(
+            "writing {} to {}".format(running_triggers, self.running_triggers_file)
         )
 
-        with self._running_triggers_file.open("wb") as f:
+        with self.running_triggers_file.open("wb") as f:
             fcntl.flock(f, fcntl.LOCK_EX)
             pickle.dump(running_triggers, f)
             fcntl.flock(f, fcntl.LOCK_UN)
 
 
-class PatternIntervalTrigger(Thread):
+# --- trigger(s) ---------------------------------------------------------------
+class _PatternIntervalTrigger(Thread):
     """Time interval and file pattern based workflow trigger."""
 
     def __init__(
         self,
         *,
+        checkout: queue.Queue,
         ensemble: str,
         trigger_name: str,
         workflow_name_prefix: str,
@@ -191,9 +241,10 @@ class PatternIntervalTrigger(Thread):
         additional_args: Optional[str] = None
     ):
         Thread.__init__(self, name="::".join([ensemble, trigger_name]), daemon=True)
-        # TODO: add file handler so that logs can be written to ~/.pegasus/ensembles/triggers
-        self._log = logging.getLogger("PatternIntervalTrigger")
-        self._stop_event = Event()
+
+        self.log = logging.getLogger("trigger.trigger")
+        self.stop_event = Event()
+        self.checkout = checkout
 
         self.ensemble = ensemble
         self.trigger_name = trigger_name
@@ -207,15 +258,14 @@ class PatternIntervalTrigger(Thread):
         # TODO: add some accounting like list of workflows started, total time running, etc.
 
     def __str__(self):
-        return "<PatternIntervalTrigger {}::{}>".format(
-            self.ensemble, self.trigger_name
-        )
+        return "<PatternIntervalTrigger {}>".format(self.name)
 
     def run(self):
-        self._log.info("{} starting".format(self))
+        self.log.info("{} starting".format(self.name))
 
-        time_now = datetime.datetime.now().timestamp()
-        while not self._stop_event.isSet():
+        while not self.stop_event.isSet():
+            time_now = datetime.datetime.now().timestamp()
+
             # get paths of all files that match pattern with mod date s.t.
             # self._last_ran <= mod date < time_now
             input_files = []
@@ -227,8 +277,10 @@ class PatternIntervalTrigger(Thread):
 
             # early termination condition
             if len(input_files) == 0:
-                self._log.info(
-                    "{} encountered no new input files, shutting down".format(self)
+                self.log.info(
+                    "{} encountered no new input files at {}, shutting down".format(
+                        self.name, datetime.datetime.fromtimestamp(time_now).isoformat()
+                    )
                 )
                 break
 
@@ -249,15 +301,24 @@ class PatternIntervalTrigger(Thread):
             if self.additional_args:
                 cmd.extend(self.additional_args.split())
 
-            self._log.debug("executing command: {}".format(" ".join(cmd)))
+            self.log.debug(
+                "{} executing command: {} for interval {}".format(
+                    self.name,
+                    " ".join(cmd),
+                    "[{} - {})".format(
+                        datetime.datetime.fromtimestamp(self.last_ran).isoformat(),
+                        datetime.datetime.fromtimestamp(time_now).isoformat(),
+                    ),
+                )
+            )
 
             # invoke pegasus-em submit
             cp = subprocess.run(cmd)
 
             if cp.returncode != 0:
-                self._log.error(
-                    "Encountered error submitting workflow with: {}, shutting down".format(
-                        cmd
+                self.log.error(
+                    "{} encountered error submitting workflow with: {}, shutting down".format(
+                        self.name, cmd
                     )
                 )
                 break
@@ -268,14 +329,15 @@ class PatternIntervalTrigger(Thread):
             # sleep for interval
             time.sleep(self.interval)
 
-        self._log.info("{} done".format(self.name))
-
-        # TODO: add myself to the q to be cleaned up
+        # advertise that my work is complete
+        self.checkout.put(self.name)
+        self.log.info("{} done".format(self.name))
 
     def shutdown(self):
         """Gracefully shutdown this thread."""
 
-        self._log.info("shutting down {}".format(self.trigger_name))
-        self._stop_event.set()
+        self.log.info("{} shutting down".format(self.name))
+        self.stop_event.set()
 
-        # TODO: add myself to the q to be cleaned  up
+        # advertise that my work is complete
+        self.checkout.put(self.name)
