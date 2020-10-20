89988340ae214a6c299fc01f52944ef3077a30ed
George Papadimitriou <georgpap@isi.edu>
2020-10-20 15:14:44 -0700

    pegasus-init stuff

--- packages/pegasus-python/src/Pegasus/cli/pegasus-init.py
+++ packages/pegasus-python/src/Pegasus/cli/pegasus-init.py
@@ -5,6 +5,8 @@ from pathlib import Path
 
 from Pegasus.init import main
 
-pegasus_share_dir = (Path(os.environ["PEGASUS_HOME"]) / "share" / "pegasus").resolve()
+#pegasus_share_dir = (Path(os.environ["PEGASUS_HOME"]) / "share" / "pegasus").resolve()
 
-main(str(pegasus_share_dir))
+#main(str(pegasus_share_dir))
+
+main()
