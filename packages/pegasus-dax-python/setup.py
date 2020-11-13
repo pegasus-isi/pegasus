2d7346fdc3d31335381f2cffa65a537155690ed8
Rajiv Mayani <mayani@isi.edu>
2020-11-12 17:18:40 -0800

    Add py39 env into testing config. Update descriptions in setup.py

--- packages/pegasus-dax-python/setup.py
+++ packages/pegasus-dax-python/setup.py
@@ -58,6 +58,7 @@ setup(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "Topic :: Scientific/Engineering",
         "Topic :: Utilities",
         "License :: OSI Approved :: Apache Software License",
