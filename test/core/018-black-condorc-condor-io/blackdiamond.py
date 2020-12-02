a5c592dbe82b01a84e283e51922c71788718a727
Ryan Tanaka <ryanyt@hawaii.edu>
2020-12-01 16:01:09 -0800

    PM-1710 added missing props.write(); cleaned up wf section

--- test/core/018-black-condorc-condor-io/blackdiamond.py
+++ test/core/018-black-condorc-condor-io/blackdiamond.py
@@ -22,6 +22,7 @@ props["pegasus.dir.storage.deep"] = "false"
 props["pegasus.condor.logs.symlink"] = "false"
 props["pegasus.data.configuration"] = "condorio"
 props["pegasus.transfer.worker.package"] = "true"
+props.write()
 
 # --- Sites --------------------------------------------------------------------
 sc = SiteCatalog()
@@ -118,25 +119,37 @@ fc1 = File("f.c1")
 fc2 = File("f.c2")
 fd = File("f.d")
 
+wf = Workflow("diamond")
+
+preprocess_job = Job(preprocess)\
+                    .add_args("-a", "preprocess", "-T", "10", "-i", fa, "-o", fb1, fb2)\
+                    .add_inputs(fa)\
+                    .add_outputs(fb1, fb2)
+
+findrange_1_job = Job(findrange)\
+                    .add_args("-a", "findrange", "-T", "5", "-i", fb1, "-o", fc1)\
+                    .add_inputs(fb1)\
+                    .add_outputs(fc1)
+
+findrange_2_job = Job(findrange)\
+                    .add_args("-a", "findrange", "-T", "5", "-i", fb2, "-o", fc2)\
+                    .add_inputs(fb2)\
+                    .add_outputs(fc2)
+
+analyze_job = Job(analyze)\
+                .add_args("-a", "analyze", "-T", "10", "-i", fc1, fc2, "-o", fd)\
+                .add_inputs(fc1, fc2)\
+                .add_outputs(fd)
+
+wf.add_jobs(
+    preprocess_job,
+    findrange_1_job,
+    findrange_2_job,
+    analyze_job
+)
+
 try:
-    Workflow("diamond").add_jobs(
-        Job(preprocess)
-        .add_args("-a", "preprocess", "-T", "10", "-i", fa, "-o", fb1, fb2)
-        .add_inputs(fa)
-        .add_outputs(fb1, fb2),
-        Job(findrange)
-        .add_args("-a", "findrange", "-T", "5", "-i", fb1, "-o", fc1)
-        .add_inputs(fb1)
-        .add_outputs(fc1),
-        Job(findrange)
-        .add_args("-a", "findrange", "-T", "5", "-i", fb2, "-o", fc2)
-        .add_inputs(fb2)
-        .add_outputs(fc2),
-        Job(analyze)
-        .add_args("-a", "analyze", "-T", "10", "-i", fc1, fc2, "-o", fd)
-        .add_inputs(fc1, fc2)
-        .add_outputs(fd),
-    ).plan(
+    wf.plan(
         dir="work/submit",
         verbose=3,
         sites=["isi-condorc"],
