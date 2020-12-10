5b4b6715b56d0549f8bd4f52769afd4bed0e1955
Ryan Tanaka <ryanyt@hawaii.edu>
2020-12-10 08:59:01 -0800

    PM-1717 initial conversion of 028-dynamic-hierarchy to 5.0 python api

---
+++ test/core/028-dynamic-hierarchy/generate_inner_diamond_workflow.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python3
+from Pegasus.api import *
+
+rc = ReplicaCatalog()
+rc.add_replica(site="local", lfn="f.a", pfn=Path(args.submit_node_working_dir) / "f.a")
+rc.write()
+
+# --- Transformations ----------------------------------------------------------
+preprocess = Transformation(
+                name="preprocess",
+                site="local",
+                pfn="/usr/bin/pegasus-keg",
+                is_stageable=True,
+                arch=Arch.X86_64,
+                os_type=OS.LINUX,
+                os_release="rhel",
+                os_version="7"
+            )
+
+findrange = Transformation(
+                name="findrange",
+                site="local",
+                pfn="/usr/bin/pegasus-keg",
+                is_stageable=True,
+                arch=Arch.X86_64,
+                os_type=OS.LINUX,
+                os_release="rhel",
+                os_version="7"
+            )
+
+analyze = Transformation(
+                name="analyze",
+                site="local",
+                pfn="/usr/bin/pegasus-keg",
+                is_stageable=True,
+                arch=Arch.X86_64,
+                os_type=OS.LINUX,
+                os_release="rhel",
+                os_version="7"
+            )
+
+tc = TransformationCatalog()
+tc.add_transformations(preprocess, findrange, analyze)
+tc.write()
+
+# --- Workflow -----------------------------------------------------------------
+fa = File("f.a")
+fb1 = File("f.b1")
+fb2 = File("f.b2")
+fc1 = File("f.c1")
+fc2 = File("f.c2")
+fd = File("f.d")
+
+wf = Workflow("diamond")
+
+preprocess_job = Job("preprocess")\
+                    .add_args("-a", "preprocess", "-T", "10", "-i", fa, "-o", fb1, fb2)\
+                    .add_inputs(fa)\
+                    .add_outputs(fb1, fb2)
+
+findrange_1_job = Job("findrange")\
+                    .add_args("-a", "findrange", "-T", "5", "-i", fb1, "-o", fc1)\
+                    .add_inputs(fb1)\
+                    .add_outputs(fc1)
+
+findrange_2_job = Job("findrange")\
+                    .add_args("-a", "findrange", "-T", "5", "-i", fb2, "-o", fc2)\
+                    .add_inputs(fb2)\
+                    .add_outputs(fc2)
+
+analyze_job = Job("analyze")\
+                .add_args("-a", "analyze", "-T", "10", "-i", fc1, fc2, "-o", fd)\
+                .add_inputs(fc1, fc2)\
+                .add_outputs(fd)
+
+wf.add_jobs(
+    preprocess_job,
+    findrange_1_job,
+    findrange_2_job,
+    analyze_job
+)
+
+wf.write(file="inner_diamond_worklow.yml")
