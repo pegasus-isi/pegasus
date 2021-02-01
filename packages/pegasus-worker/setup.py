3b92f77c72effe25d9660d646086ba4484fdd928
Duncan Macleod <duncan.macleod@ligo.org>
2021-02-01 17:15:36 +0000

    pegasus-worker: use implicit namespace packages
    infrastructure code copied from pegasus-common

--- packages/pegasus-worker/setup.py
+++ packages/pegasus-worker/setup.py
@@ -1,7 +1,7 @@
 import os
 import subprocess
 
-from setuptools import find_packages, setup
+from setuptools import setup
 
 src_dir = os.path.dirname(__file__)
 home_dir = os.path.abspath(os.path.join(src_dir, "../.."))
@@ -32,6 +32,21 @@ def read_version():
 #
 def read(fname):
     return open(os.path.join(src_dir, fname)).read()
+
+
+# TODO: Someday remove this method and replace with setuptools.find_namespace_packages
+def find_namespace_packages(where):
+    pkgs = []
+    for root, dirs, _ in os.walk(where):
+        root = root[len(where) + 1 :]
+        for pkg in dirs:
+            if pkg == where or pkg.endswith(".egg-info") or pkg == "__pycache__":
+                continue
+
+            pkgs.append(os.path.join(root, pkg).replace("/", "."))
+
+    return pkgs
+
 
 
 setup(
@@ -66,7 +81,7 @@ setup(
         "License :: OSI Approved :: Apache Software License",
     ],
     package_dir={"": "src"},
-    packages=find_packages(where="src"),
+    packages=find_namespace_packages(where="src"),
     include_package_data=True,
     zip_safe=False,
     install_requires=install_requires,
