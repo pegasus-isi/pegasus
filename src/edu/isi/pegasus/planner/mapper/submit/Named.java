8c8b7d59fd158a3b364424e91058b9209e442ac5
Karan Vahi <vahi@isi.edu>
2021-04-28 16:38:11 -0700

    PM-1751 new Named submit directory mapper implementation

---
+++ src/edu/isi/pegasus/planner/mapper/submit/Named.java
@@ -0,0 +1,127 @@
+/**
+ * Copyright 2007-2016 University Of Southern California
+ *
+ * <p>Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of the License at
+ *
+ * <p>http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * <p>Unless required by applicable law or agreed to in writing, software distributed under the
+ * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package edu.isi.pegasus.planner.mapper.submit;
+
+import edu.isi.pegasus.common.logging.LogManager;
+import edu.isi.pegasus.planner.classes.Job;
+import edu.isi.pegasus.planner.classes.PegasusBag;
+import edu.isi.pegasus.planner.classes.PlannerOptions;
+import edu.isi.pegasus.planner.mapper.SubmitMapper;
+import edu.isi.pegasus.planner.namespace.Pegasus;
+import java.io.File;
+import java.util.Properties;
+
+/**
+ * A Named implementation for the Submit Directory Mapper that allows users to specify the relative
+ * directory into which the submit files for the compute jobs are written out to by the Planner
+ * during Code Generation. The relative directory is specified by associated a Pegasus profile key
+ * named {@link Pegasus#RELATIVE_SUBMIT_DIR_KEY}
+ *
+ * <p>For all auxiliary jobs that are added by the planner this mapper uses the associated
+ * transformation name for the determining the relative submit directory.
+ *
+ * @see Pegasus#RELATIVE_SUBMIT_DIR_KEY
+ * @author Karan Vahi
+ */
+public class Named implements SubmitMapper {
+
+    /** Short description. */
+    private static final String DESCRIPTION = "Relative Submit Directory Mapper";
+
+    /** The root of the directory tree under which other directories are created */
+    private File mBaseDir;
+
+    /** Handle to the logger */
+    private LogManager mLogger;
+
+    private File mBaseSubmitDirectory;
+
+    /** Default constructor. */
+    public Named() {}
+
+    /**
+     * Initializes the submit mapper
+     *
+     * @param bag the bag of Pegasus objects
+     * @param properties properties that can be used to control the behavior of the mapper
+     * @param base the base directory relative to which all job directories are created
+     */
+    public void initialize(PegasusBag bag, Properties properties, File base) {
+        mBaseDir = base;
+        mLogger = bag.getLogger();
+        PlannerOptions options = bag.getPlannerOptions();
+        mBaseSubmitDirectory = new File(options.getSubmitDirectory());
+    }
+
+    /**
+     * Returns the relative submit directory for the job
+     *
+     * @param job
+     * @return the relative submit directory for the job
+     */
+    public File getRelativeDir(Job job) {
+        String relative = this.determineRelativeDirectory(job);
+        return new File(relative);
+    }
+
+    /**
+     * Returns the full path to the submit directory to be used for the job
+     *
+     * @param job
+     * @return
+     */
+    public File getDir(Job job) {
+        return new File(this.mBaseSubmitDirectory, this.determineRelativeDirectory(job));
+    }
+
+    /**
+     * Returns a short description of the mapper.
+     *
+     * @return String
+     */
+    public String description() {
+        return Named.DESCRIPTION;
+    }
+
+    /**
+     * Returns the relative directory to use for the job.
+     *
+     * @param job the job
+     * @return
+     */
+    protected String determineRelativeDirectory(Job job) throws RuntimeException {
+        String relative = null;
+
+        switch (job.getJobType()) {
+                // for compute jobs look for the profile
+            case Job.COMPUTE_JOB:
+                relative = job.vdsNS.getStringValue(Pegasus.RELATIVE_SUBMIT_DIR_KEY);
+                break;
+
+                // all other jobs use the associated transformation name
+            default:
+                relative = job.getTXName();
+                break;
+        }
+
+        if (relative == null) {
+            throw new RuntimeException(
+                    "Pegasus Profile Key "
+                            + Pegasus.RELATIVE_SUBMIT_DIR_KEY
+                            + " not specified. Unable to determine relative directory for job "
+                            + job);
+        }
+        return relative;
+    }
+}
