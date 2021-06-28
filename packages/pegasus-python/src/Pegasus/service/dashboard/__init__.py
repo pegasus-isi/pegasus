a3510fa705d0917f75460108aed7a7fb0c9a6204
Mats Rynge <mats@rynge.net>
2021-06-28 15:06:56 -0700

    Allow for url prefix in the dashboard

--- packages/pegasus-python/src/Pegasus/service/dashboard/__init__.py
+++ packages/pegasus-python/src/Pegasus/service/dashboard/__init__.py
@@ -1,5 +1,9 @@
 from flask import Blueprint
+import os
 
-blueprint = Blueprint("dashboard", __name__)
+blueprint = Blueprint("dashboard", __name__,
+                      static_folder="../static",
+                      static_url_path="/static")
+
 
 from Pegasus.service.dashboard import views  # noqa: E402,F401 isort:skip
