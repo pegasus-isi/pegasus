be06bf60ef3ae269a66f8dbbdbacd2ea22faf747
Karan Vahi <vahi@isi.edu>
2021-07-14 17:58:15 -0700

    PM-1783, PM-1784 added some unit tests to test bypass staging

--- test/junit/edu/isi/pegasus/planner/test/AllTests.java
+++ test/junit/edu/isi/pegasus/planner/test/AllTests.java
@@ -69,6 +69,7 @@ import org.junit.runners.Suite;
     edu.isi.pegasus.planner.parser.dax.DAXParser3Test.class,
     edu.isi.pegasus.planner.parser.dax.DAXParser5Test.class,
     edu.isi.pegasus.planner.dax.ADAGTest.class,
-    edu.isi.pegasus.planner.transfer.generator.StageOutTest.class
+    edu.isi.pegasus.planner.transfer.generator.StageOutTest.class,
+    edu.isi.pegasus.planner.transfer.generator.StageInTest.class
 })
 public class AllTests {}
