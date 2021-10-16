a81b026a6dcd6f3bbfa758b42edb1a23790fbbcd
Rajiv Mayani <mayani@isi.edu>
2021-10-16 00:05:23 -0700

    PM-1673 Add unit tests

--- packages/pegasus-api/test/api/test_properties.py
+++ packages/pegasus-api/test/api/test_properties.py
@@ -1,6 +1,7 @@
 import os
 from configparser import DEFAULTSECT
-from tempfile import TemporaryFile
+from pathlib import Path
+from tempfile import NamedTemporaryFile
 
 import pytest
 
@@ -12,73 +13,141 @@ def props():
     return Properties()
 
 
-class TestProperties:
-    def test_ls(self, capsys, props):
-        try:
-            Properties.ls("pegasus.pmc")
-            captured = capsys.readouterr().out
-            assert (
-                captured
-                == "pegasus.pmc_priority\npegasus.pmc_request_cpus\npegasus.pmc_request_memory\npegasus.pmc_task_arguments\n"
-            )
+def test_ls(capsys, props):
+    try:
+        Properties.ls("pegasus.pmc")
+        captured = capsys.readouterr().out
+        assert (
+            captured
+            == """pegasus.pmc_priority
+pegasus.pmc_request_cpus
+pegasus.pmc_request_memory
+pegasus.pmc_task_arguments
+"""
+        )
 
-            Properties.ls()
-            Properties.ls("nothing")
-            props.ls()
-        except:
-            pytest.raises("should not have failed")
+        Properties.ls()
+        Properties.ls("nothing")
+        props.ls()
+    except Exception:
+        pytest.raises("should not have failed")
 
-    def test_get_item(self, props, mocker):
-        props["a"] = "b"
-        assert props["a"] == "b"
 
-    def test_del_item(self, props):
-        props["a"] = "b"
-        del props["a"]
+@pytest.mark.parametrize(
+    "k, v",
+    [
+        ("pegasus.mode", "development"),
+        ("pegasus.catalog.replica.db.user", "alpha"),
+        ("pegasus.catalog.replica.db.useSSL", True),
+        ("pegasus.ppn", 100),
+        ("pegasus.catalog.site.file", Path("dir/regular.txt")),
+        ("pegasus.catalog.site.file", Path("dir/with space.txt")),
+    ],
+)
+def test_set_item(k, v, props):
+    props[k] = v
+    assert props[k] == str(v)
 
-        assert "a" not in props._conf[DEFAULTSECT]
 
-    def test_write_str_filename(self, props):
-        filename = "props"
-        props["a"] = "b"
-        props["c"] = "d"
-        props.write(filename)
+@pytest.mark.parametrize(
+    "k, v",
+    [
+        ("invalid", "development"),
+        ("Pegasus.mode", "development"),
+        ("pegasus.catalog.replica.db.", "alpha"),
+    ],
+)
+def test_set_item_fail(k, v, props):
+    props[k] = v
+    # TODO
+    assert 1
 
-        with open(filename) as f:
-            assert f.read() == "a = b\nc = d\n\n"
 
-        os.remove(filename)
+@pytest.mark.parametrize(
+    "k, v",
+    [
+        ("pegasus.mode", "development"),
+        ("pegasus.catalog.replica.db.user", "alpha"),
+        ("pegasus.catalog.replica.db.useSSL", True),
+        ("pegasus.ppn", 100),
+        ("pegasus.catalog.site.file", Path("dir/regular.txt")),
+        ("pegasus.catalog.site.file", Path("dir/with space.txt")),
+    ],
+)
+def test_get_item(k, v, props):
+    props[k] = v
+    assert props[k] == str(v)
 
-    def test_write_str_filename_ensure_key_case_preserved(self, props):
-        filename = "props"
-        props["a"] = "b"
-        props["C"] = "d"
-        props.write(filename)
 
-        with open(filename) as f:
-            assert f.read() == "a = b\nC = d\n\n"
+@pytest.mark.parametrize(
+    "k, v",
+    [
+        ("pegasus.mode", "development"),
+        ("pegasus.catalog.replica.db.user", "alpha"),
+        ("pegasus.catalog.replica.db.useSSL", True),
+    ],
+)
+def test_del_item(k, v, props):
+    props[k] = v
+    del props[k]
 
-        os.remove(filename)
+    assert k not in props._conf[DEFAULTSECT]
 
-    def test_write_file(self, props):
-        with TemporaryFile(mode="w+") as f:
-            props["a"] = "b"
-            props.write(f)
-            f.seek(0)
-            assert f.read() == "a = b\n\n"
 
-    def test_write_invalid_file(self, props):
-        with pytest.raises(TypeError) as e:
-            props.write(123)
+def test_write_str_filename(props):
+    with NamedTemporaryFile(mode="w+") as f:
+        props["pegasus.mode"] = "development"
+        props["globus.queue"] = "main"
+        props.write(f.name)
 
-        assert "invalid file: 123" in str(e)
+        f.seek(0)
+        assert (
+            f.read()
+            == """pegasus.mode = development
+globus.queue = main
 
-    def test_write_default_file(self, props):
-        props["a"] = "b"
-        props.write()
+"""
+        )
 
-        EXPECTED_DEFAULT_FILE = "pegasus.properties"
-        with open(EXPECTED_DEFAULT_FILE) as f:
-            assert f.read() == "a = b\n\n"
 
-        os.remove(EXPECTED_DEFAULT_FILE)
+def test_write_str_filename_ensure_key_case_preserved(props):
+    with NamedTemporaryFile(mode="w+") as f:
+        props["env.PEGASUS_HOME"] = "HOME"
+        props["env.pegasus_home"] = "home"
+        props.write(f)
+
+        f.seek(0)
+        assert (
+            f.read()
+            == """env.PEGASUS_HOME = HOME
+env.pegasus_home = home
+
+"""
+        )
+
+
+def test_write_file(props):
+    with NamedTemporaryFile(mode="w+") as f:
+        props["pegasus.mode"] = "development"
+        props.write(f)
+
+        f.seek(0)
+        assert f.read() == "pegasus.mode = development\n\n"
+
+
+def test_write_invalid_file(props):
+    with pytest.raises(TypeError) as e:
+        props.write(123)
+
+    assert "invalid file: 123" in str(e)
+
+
+def test_write_default_file(props):
+    props["pegasus.mode"] = "development"
+    props.write()
+
+    EXPECTED_DEFAULT_FILE = "pegasus.properties"
+    with open(EXPECTED_DEFAULT_FILE) as f:
+        assert f.read() == "pegasus.mode = development\n\n"
+
+    os.remove(EXPECTED_DEFAULT_FILE)
