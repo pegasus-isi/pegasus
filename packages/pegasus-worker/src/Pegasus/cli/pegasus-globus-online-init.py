f6be8b54cff7c1cef797b96a0dfe5647a1c77461
George Papadimitriou <georgpap@isi.edu>
2022-03-14 13:11:56 -0700

    PM-1830 simple changes for consents and globus sdk v3 compatibitliy

--- packages/pegasus-worker/src/Pegasus/cli/pegasus-globus-online-init.py
+++ packages/pegasus-worker/src/Pegasus/cli/pegasus-globus-online-init.py
@@ -2,9 +2,11 @@
 from __future__ import print_function
 
 import os
-from optparse import OptionParser
+import json
+from argparse import ArgumentParser
 
 import globus_sdk
+from globus_sdk.scopes import TransferScopes, GCSCollectionScopeBuilder
 
 try:
     from configparser import ConfigParser
@@ -14,25 +16,38 @@ except Exception:
 client_id = "d7382f5a-4ea3-4b69-b094-99c392fc820d"
 config_file = os.path.expanduser("~/.pegasus/globus.conf")
 
-parser = OptionParser(description="Initialize Globus OAuth Tokens")
-parser.add_option(
+parser = ArgumentParser(description="Initialize Globus OAuth Tokens - SDK Version {}".format(globus_sdk.__version__))
+parser.add_argument(
+    "-p",
     "--permanent",
     action="store_true",
-    dest="permanent",
     help="request a refreshable token",
 )
+parser.add_argument(
+    "-e",
+    "--endpoints",
+    nargs="*",
+    default=[],
+    help="list of endpoint uuids to acquire data_consents",
+)
 
-(options, args) = parser.parse_args()
+args = parser.parse_args()
 
 config = ConfigParser()
 config.add_section("oauth")
 config.set("oauth", "client_id", client_id)
 
+pegasus_scopes = [TransferScopes.all]
+for c in args.endpoints:
+    sb = GCSCollectionScopeBuilder(c, known_scopes=[TransferScopes.all])
+    pegasus_scopes.append("{0}[*{1}]".format(TransferScopes.all, sb.data_access))
+    pegasus_scopes.append(sb.data_access)
+
 client = globus_sdk.NativeAppAuthClient(client_id)
-if options.permanent:
-    client.oauth2_start_flow(refresh_tokens=True)
+if args.permanent:
+    client.oauth2_start_flow(refresh_tokens=True, requested_scopes=pegasus_scopes)
 else:
-    client.oauth2_start_flow()
+    client.oauth2_start_flow(requested_scopes=pegasus_scopes)
 
 authorize_url = client.oauth2_get_authorize_url()
 print("Please go to this URL and login: {0}".format(authorize_url))
@@ -43,11 +58,12 @@ token_response = client.oauth2_exchange_code_for_tokens(auth_code)
 
 transfer_data = token_response.by_resource_server["transfer.api.globus.org"]
 
-# get tokens as strings and their expiration timestamp
-transfer_at = transfer_data["access_token"]
-transfer_rt = transfer_data["refresh_token"]
-transfer_at_exp = transfer_data["expires_at_seconds"]
+print(json.dumps(token_response.by_resource_server, indent=2))
 
+# get tokens as strings and their expiration timestamp
+transfer_at = str(transfer_data["access_token"])
+transfer_rt = str(transfer_data["refresh_token"])
+transfer_at_exp = transfer_data["expires_at_seconds"]
 
 config.set("oauth", "transfer_at", transfer_at)  # transfer access token
 config.set("oauth", "transfer_rt", transfer_rt)  # transfer refresh token
