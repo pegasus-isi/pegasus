3d856e595d0bd07dfd608cdabefc97351f036a5a
Karan Vahi <vahi@isi.edu>
2023-01-13 16:12:14 -0800

    PM-1894 we now allow users to override and enable strict worker package checking explicity in the container script that launches the job in the container.

--- src/edu/isi/pegasus/planner/code/gridstart/container/impl/Docker.java
+++ src/edu/isi/pegasus/planner/code/gridstart/container/impl/Docker.java
@@ -340,9 +340,7 @@ public class Docker extends Abstract {
         sb.append("pegasus_lite_version_minor=$pegasus_lite_version_minor").append("\n");
         sb.append("pegasus_lite_version_patch=$pegasus_lite_version_patch").append("\n");
 
-        // explicitly set the strict check to false, as we want to pick up pegasus version
-        // in the container if specified
-        sb.append("pegasus_lite_enforce_strict_wp_check=false").append("\n");
+        sb.append("pegasus_lite_enforce_strict_wp_check=$pegasus_lite_enforce_strict_wp_check").append("\n");
 
         sb.append(
                         "pegasus_lite_version_allow_wp_auto_download=$pegasus_lite_version_allow_wp_auto_download")
