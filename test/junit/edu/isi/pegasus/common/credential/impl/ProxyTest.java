4d20e69215e4b241877c2650d553c9662e23dec1
Rajiv Mayani <mayani@isi.edu>
2023-06-26 20:59:28 -0700

    test: add empty unit test files for all Java source files

---
+++ test/junit/edu/isi/pegasus/common/credential/impl/ProxyTest.java
@@ -0,0 +1,46 @@
+/**
+ * Copyright 2007-2013 University Of Southern California
+ *
+ * <p>Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of the License at
+ *
+ * <p>http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * <p>Unless required by applicable law or agreed to in writing, software distributed under the
+ * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package edu.isi.pegasus.common.credential.impl;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @author Rajiv Mayani
+ */
+public class ProxyTest {
+    @BeforeClass
+    public static void setUpClass() {}
+
+    @AfterClass
+    public static void tearDownClass() {}
+
+    @Before
+    public void setUp() {}
+
+    @After
+    public void tearDown() {}
+
+    /*
+    @Test
+    public void testSomeMethod() {
+        assertEquals(1, 1);
+    }
+    */
+}
