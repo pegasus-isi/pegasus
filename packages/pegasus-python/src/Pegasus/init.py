4a4ae24bd2efca2c45c72485c3d1b9aa28bbf16a
Karan Vahi <vahi@isi.edu>
2025-09-16 14:31:09 -0500

    For #2125 distinguish between directories for the submit host and the remote slurm cluster that is remote to the submit node.

--- packages/pegasus-python/src/Pegasus/init.py
+++ packages/pegasus-python/src/Pegasus/init.py
@@ -83,6 +83,9 @@ def console_select_site(wf_dir):
     pegasus_home = ""
     login_host = ""
     transfer_endpoint = ""
+    shared_scratch = ""
+    storage_dir = ""
+    remote_shared_scratch = ""
     share_filesystem_with_submit_node = False
 
     # default base directories for scratch and storage space
@@ -155,23 +158,10 @@ def console_select_site(wf_dir):
             kwargs["default"] = default_shared_scratch
             kwargs["show_default"] = True
 
-        shared_scratch = click.prompt(**kwargs)
-    else:
-        shared_scratch = default_shared_scratch
+        remote_shared_scratch = click.prompt(**kwargs)
 
-    if site in Sites.SitesRequireStorage:
-        kwargs = dict(
-            text="What's the storage space on the cluster that you want to use",
-            type=click.Path(),
-        )
-
-        if share_filesystem_with_submit_node:
-            kwargs["default"] = default_shared_storage
-            kwargs["show_default"] = True
-
-        storage_dir = click.prompt(**kwargs)
-    else:
-        storage_dir = default_shared_storage
+    shared_scratch = default_shared_scratch
+    storage_dir = default_shared_storage
 
     return (
         site,
@@ -182,6 +172,7 @@ def console_select_site(wf_dir):
         storage_dir,
         login_host,
         transfer_endpoint,
+        remote_shared_scratch,
     )
 
 
@@ -329,6 +320,7 @@ def create_workflow(
     storage_dir,
     login_host,
     transfer_endpoint,
+    remote_shared_scratch,
 ):
     commands = []
     pegasushub_config = read_pegasushub_config(wf_dir, workflow)
@@ -369,6 +361,7 @@ Please refer to Pegasus Documentation https://pegasus.isi.edu/documentation/refe
         pegasus_home=pegasus_home,
         login_host=login_host,
         transfer_endpoint=transfer_endpoint,
+        remote_shared_scratch_parent_dir=remote_shared_scratch,
     )
 
     pre_scripts = [x for x in pegasushub_config["scripts"] if x.startswith("pre-")]
@@ -518,6 +511,7 @@ def main(directory, workflow_gallery):
         storage_dir,
         login_host,
         transfer_endpoint,
+        remote_shared_scratch,
     ) = console_select_site(directory)
     workflows_available = read_workflows(workflow_gallery, site)
 
@@ -541,6 +535,7 @@ def main(directory, workflow_gallery):
         storage_dir,
         login_host,
         transfer_endpoint,
+        remote_shared_scratch,
     )
 
     return
