564b1de693ed322575775a8e816ab3793a3e4f3b
Mats Rynge <mats@rynge.net>
2025-10-21 15:53:14 -0700

    Tests cleanup

--- test/core/020-pmc-only/plan.sh
+++ test/core/020-pmc-only/plan.sh
@@ -5,7 +5,7 @@ set -e
 TEST_DIR=$PWD
 
 echo "Generating the dax..."
-python3 daxgen.py dax.xml
+PYTHONPATH=$(pegasus-config --python) python3 daxgen.py dax.xml
 
 cat > sites.yml <<END
 pegasus: "5.0"
