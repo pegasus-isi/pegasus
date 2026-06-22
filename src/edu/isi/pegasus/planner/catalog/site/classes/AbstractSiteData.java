209471a395fc2d0484e48c72a52e04fe471067ca
Rajiv Mayani <mayani@isi.edu>
2026-06-22 14:46:17 -0700

    style: code format

--- src/edu/isi/pegasus/planner/catalog/site/classes/AbstractSiteData.java
+++ src/edu/isi/pegasus/planner/catalog/site/classes/AbstractSiteData.java
@@ -22,6 +22,7 @@ import com.fasterxml.jackson.databind.MapperFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 import com.fasterxml.jackson.dataformat.yaml.YAMLGenerator;
+
 import java.io.IOException;
 import java.io.StringWriter;
 import java.io.Writer;
