5efd8d36c9408d2399ae92120ba601aab313ae75
Rajiv Mayani <mayani@isi.edu>
2026-06-22 15:58:05 -0700

    chore: don't stream condor._q stdout, so CLI tools don't show JSON blocks in the console output

--- packages/pegasus-common/src/Pegasus/client/condor.py
+++ packages/pegasus-common/src/Pegasus/client/condor.py
@@ -66,7 +66,7 @@ def _handle_stream(
             break
 
 
-def _exec(cmd, stream_stdout=True, stream_stderr=False):
+def _exec(cmd, stream_stdout: bool = True, stream_stderr: bool = False):
     """Execute a command and capture its output via threaded stream handlers.
 
     :param cmd: command and arguments to execute
@@ -121,5 +121,5 @@ def _q(cmd):
     """
     if not cmd:
         raise ValueError("cmd is required")
-    rv = _exec(cmd=cmd)
+    rv = _exec(cmd=cmd, stream_stdout=False)
     return rv.json
